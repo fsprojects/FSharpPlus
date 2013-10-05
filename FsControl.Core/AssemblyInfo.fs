b5d5210658a0dcb2c7eebcac4ad407041b2fd78e
gmpl <gustavo@perezleon.com.ar>
2013-10-05 12:40:41 +0200

    + AssemblyInfo.fs

---
+++ FsControl.Core/AssemblyInfo.fs
@@ -0,0 +1,18 @@
+﻿module FsControl.AssemblyInfo
+
+open System.Reflection
+
+[<assembly: AssemblyTitle("FsControl")>]
+[<assembly: AssemblyDescription("")>]
+
+[<assembly: AssemblyConfiguration("")>]
+[<assembly: AssemblyCompany("")>]
+[<assembly: AssemblyProduct("")>]
+[<assembly: AssemblyCopyright("Copyright © 2012")>]
+[<assembly: AssemblyTrademark("")>]
+[<assembly: AssemblyCulture("")>]
+
+[<assembly: AssemblyVersion("1.0.0.0")>]
+[<assembly: AssemblyFileVersion("1.0.0.0")>]
+[<assembly: AssemblyInformationalVersion("1.0.0")>]
+do()
