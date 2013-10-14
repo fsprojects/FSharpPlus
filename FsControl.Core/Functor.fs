5833826ee3fd79058245944d619e1773eed8dafb
gmpl <gustavo@perezleon.com.ar>
2013-10-14 19:16:48 +0200

    Choice Functor fixed.

--- FsControl.Core/Functor.fs
+++ FsControl.Core/Functor.fs
@@ -22,6 +22,6 @@ module Functor =
             Array4D.init (x.GetLength 0) (x.GetLength 1) (x.GetLength 2) (x.GetLength 3) (fun a b c d -> f x.[a,b,c,d])
         static member instance (Fmap, x:Async<_>   , _) = fun f -> DefaultImpl.FmapFromMonad f x
         static member instance (Fmap, x:Nullable<_>, _) = fun f -> if x.HasValue then Nullable(f x.Value) else Nullable()
-        static member instance (Fmap, x:Choice<_,_>, _) = fun f -> match x with Choice2Of2 x -> Choice2Of2(f x) | x -> x
+        static member instance (Fmap, x:Choice<_,_>, _) = fun f -> match x with Choice1Of2 x -> Choice1Of2(f x) | Choice2Of2 x -> Choice2Of2 x
 
     let inline internal fmap   f x = Inline.instance (Fmap, x) f
