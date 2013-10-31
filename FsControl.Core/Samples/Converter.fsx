530e1864643daee7eb44c1ca019c63b4d469f87a
gmpl <gustavo@perezleon.com.ar>
2013-10-31 19:05:50 +0100

    + Sample code for Converter.

---
+++ FsControl.Core/Samples/Converter.fsx
@@ -0,0 +1,12 @@
+﻿#r @"..\bin\Release\FsControl.Core.dll"
+
+open System
+open FsControl.Core.Abstractions.Converter
+
+let inline internal fromBytesWithOffset (startIndex:int) (value:byte[]) = Inline.instance FromBytes (value, startIndex)
+let inline internal fromBytes                            (value:byte[]) = Inline.instance FromBytes (value, 0         )
+let inline internal toBytes value :byte[] = Inline.instance (ToBytes, value) ()
+let inline internal parse  (value:string) = Inline.instance Parse value
+
+let r120 = parse "10" + fromBytes [|10uy;0uy;0uy;0uy;|]                + 100
+let r110 = parse "10" + fromBytes [|10uy;0uy;0uy;0uy;0uy;0uy;0uy;0uy|] + 100.
