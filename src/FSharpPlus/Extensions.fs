333a8c0f0050aee2b4aa752a6104ddd136e60693
Gustavo <gustavo@perezleon.com.ar>
2013-11-05 15:41:19 +0100

    + Seq.splitBy
    Like Seq.groupBy but without sorting, more in the spirit of the Haskell version of group function.

--- src/FSharpPlus/Extensions.fs
+++ src/FSharpPlus/Extensions.fs
@@ -14,6 +14,23 @@ module Extensions =
 
     [<AutoOpen>]
     module Seq =
+        
+        let splitBy keyMapper (source:_ seq) = seq {
+            use e = source.GetEnumerator()
+            if (e.MoveNext()) then
+                let groupKey = ref (keyMapper e.Current)
+                let values   = ref (new ResizeArray<_>())
+                (!values).Add(e.Current)
+                while (e.MoveNext()) do
+                    let key = keyMapper e.Current
+                    if !groupKey = key then (!values).Add(e.Current)
+                    else
+                        yield (!groupKey, !values :> seq<_>)
+                        groupKey := key
+                        values   := new ResizeArray<_>()
+                        (!values).Add(e.Current)
+                yield (!groupKey, !values :> seq<_>)}
+
         // http://codebetter.com/matthewpodwysocki/2009/05/06/functionally-implementing-intersperse/
         let intersperse sep list =
             seq {
