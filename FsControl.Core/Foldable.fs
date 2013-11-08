245a942b61c33fdf83e5546eb070cf0ae2b5c92e
Gustavo <gustavo@perezleon.com.ar>
2013-11-08 10:50:43 +0100

    + Default methods for Foldable.
    . FoldMap derived from Foldr.
    . Foldl derived from FoldMap.

--- FsControl.Core/Foldable.fs
+++ FsControl.Core/Foldable.fs
@@ -7,6 +7,7 @@ open Dual
 open Endo
 
 module Foldable =
+
     type Foldr = Foldr with
         static member instance (Foldr, x:option<_>, _) = fun (f,z) -> match x with Some t -> f t z | _ -> z
         static member instance (Foldr, x:List<_>  , _) = fun (f,z) -> List.foldBack          f x z
@@ -15,11 +16,18 @@ module Foldable =
     type DefaultImpl =
         static member inline FoldMapFromFoldr f x = Inline.instance (Foldr, x) (mappend << f, mempty())
    
-    type FoldMap = FoldMap with
-        static member inline instance (FoldMap, x:option<_>, _) = fun f -> DefaultImpl.FoldMapFromFoldr f x
-        static member inline instance (FoldMap, x:List<_>  , _) = fun f -> DefaultImpl.FoldMapFromFoldr f x
-        static member inline instance (FoldMap, x:Set<_>   , _) = fun f -> DefaultImpl.FoldMapFromFoldr f x
-        static member inline instance (FoldMap, x:array<_> , _) = fun f -> Array.foldBack (mappend << f) x (mempty())
+    
+    type FoldMapDefault() =
+        static member inline instance (_:FoldMapDefault, x:#obj, _) = fun f -> DefaultImpl.FoldMapFromFoldr f x
+
+    type FoldMap() =
+        inherit FoldMapDefault()
+        static member inline instance (_:FoldMap, x:option<_>, _) = fun f -> DefaultImpl.FoldMapFromFoldr f x
+        static member inline instance (_:FoldMap, x:List<_>  , _) = fun f -> DefaultImpl.FoldMapFromFoldr f x
+        static member inline instance (_:FoldMap, x:Set<_>   , _) = fun f -> DefaultImpl.FoldMapFromFoldr f x
+        static member inline instance (_:FoldMap, x:array<_> , _) = fun f -> Array.foldBack (mappend << f) x (mempty())
+
+    let FoldMap = FoldMap()
 
     type DefaultImpl with
         static member inline FoldrFromFoldMap f z x = 
@@ -30,13 +38,21 @@ module Foldable =
             let inline foldMap f x = Inline.instance (FoldMap, x) f
             appEndo (getDual (foldMap (Dual << Endo << flip f) t)) z
 
+
     type Foldr with
         static member inline instance (Foldr, x:array<_>, _) = fun (f,z) -> DefaultImpl.FoldrFromFoldMap f z x
 
-    type Foldl = Foldl with
-        static member instance (Foldl, x:option<_>, _) = fun (f,z) -> match x with Some t ->       f z t | _ -> z
-        static member instance (Foldl, x:List<_>  , _) = fun (f,z) -> List.fold                    f z x
-        static member instance (Foldl, x:Set<_>   , _) = fun (f,z) -> Set.fold                     f z x
-        static member instance (Foldl, x:array<_> , _) = fun (f,z) -> DefaultImpl.FoldlFromFoldMap f z x
-
     let inline internal foldr (f: 'a -> 'b -> 'b) (z:'b) x :'b = Inline.instance (Foldr, x) (f,z)
+
+
+    type FoldlDefault() =
+        static member inline instance (_:FoldlDefault, x:#obj , _) = fun (f,z) -> DefaultImpl.FoldlFromFoldMap f z x
+
+    type Foldl() =
+        inherit FoldlDefault()
+        static member instance (_:Foldl, x:option<_>, _) = fun (f,z) -> match x with Some t ->       f z t | _ -> z
+        static member instance (_:Foldl, x:List<_>  , _) = fun (f,z) -> List.fold                    f z x
+        static member instance (_:Foldl, x:Set<_>   , _) = fun (f,z) -> Set.fold                     f z x
+        static member instance (_:Foldl, x:array<_> , _) = fun (f,z) -> DefaultImpl.FoldlFromFoldMap f z x
+
+    let Foldl = Foldl()
