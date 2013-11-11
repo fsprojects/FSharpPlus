9ad842ebeacd445d21646fed3a3eac2fb4f9a393
gmpl <gustavo.perezleon@gmail.com>
2013-11-12 00:36:29 +0100

    + Generic Linq.

--- FSharpPlus/Samples/Collections.fsx
+++ FSharpPlus/Samples/Collections.fsx
@@ -1,7 +1,9 @@
 #r @"..\..\packages\FsControl.1.0.5\lib\net40\FsControl.Core.dll"
 #load @"..\Operators.fs"
+#load @"..\Builders.fs"
 #load @"..\Extensions.fs"
 #load @"..\NonEmptyList.fs"
+
 open FSharpPlus
 
 let inline print x = async { System.Console.WriteLine(toString x) }
@@ -13,3 +15,23 @@ async {
     let nel = { NonEmptyList.Head = 1; Tail = [2;3;4] } ++ { NonEmptyList.Head = 5; Tail = [6] }
     do! print nel
 } |> Async.RunSynchronously
+
+
+let arr1 = [|"1";"2";"3"|]
+let arr2 = [|10;20;30;40|]
+
+let arr = linq {
+    for e1 in arr1 do
+    for e2 in arr2 do
+    where  (parse e1 + e2 < 23)
+    select (e1,e2)   }
+
+
+let lst1 = ["1";"2";"3"]
+let lst2 = [10;20;30;40]
+
+let lst = linq {
+    for e1 in lst1 do
+    for e2 in lst2 do
+    where  (parse e1 + e2 < 23)
+    select (e1,e2)   }
