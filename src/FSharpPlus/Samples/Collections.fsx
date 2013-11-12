4eb6c1d798417d5ab4326ef6687590fe9602cdbe
gmpl <gustavo.perezleon@gmail.com>
2013-11-12 22:23:09 +0100

    + Generic Linq sample code.

--- src/FSharpPlus/Samples/Collections.fsx
+++ src/FSharpPlus/Samples/Collections.fsx
@@ -17,21 +17,13 @@ async {
 } |> Async.RunSynchronously
 
 
-let arr1 = [|"1";"2";"3"|]
-let arr2 = [|10;20;30;40|]
-
-let arr = linq {
-    for e1 in arr1 do
-    for e2 in arr2 do
+let inline f x1 x2 = linq {
+    for e1 in x1 do
+    for e2 in x2 do
     where  (parse e1 + e2 < 23)
     select (e1,e2)   }
 
+let (arr1, arr2) = [|"1";"2";"3"|], [|10;20;30;40|]
+let (lst1, lst2) = [ "1";"2";"3" ], [ 10;20;30;40 ]
 
-let lst1 = ["1";"2";"3"]
-let lst2 = [10;20;30;40]
-
-let lst = linq {
-    for e1 in lst1 do
-    for e2 in lst2 do
-    where  (parse e1 + e2 < 23)
-    select (e1,e2)   }
+let (arr, lst) = f arr1 arr2, f lst1 lst2
