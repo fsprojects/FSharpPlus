d2f5f3315d958837605f70c48c8a235fc6d564e6
Gustavo <gustavo@perezleon.com.ar>
2013-11-15 14:18:40 +0100

    Sample code updated.

--- FSharpPlus/Samples/Collections.fsx
+++ FSharpPlus/Samples/Collections.fsx
@@ -17,13 +17,16 @@ async {
 } |> Async.RunSynchronously
 
 
-let inline f x1 x2 = linq {
+let inline myQuery x1 x2 = linq {
     for e1 in x1 do
     for e2 in x2 do
-    where  (parse e1 + e2 < 23)
-    select (e1,e2)   }
+    where   (parse e1 + e2 < 23)
+    groupBy (parse e1 + e2 %  2) into g
+    sortBy  (-(fst g))
+    select  (toString (fst g), snd g )}
 
 let (arr1, arr2) = [|"1";"2";"3"|], [|10;20;30;40|]
 let (lst1, lst2) = [ "1";"2";"3" ], [ 10;20;30;40 ]
 
-let (arr, lst) = f arr1 arr2, f lst1 lst2
+let arr = myQuery arr1 arr2
+let lst = myQuery lst1 lst2
