6d663a31e1f7c3c5eb218b788f16784079022e3d
gmpl <gustavo.perezleon@gmail.com>
2013-11-16 19:32:08 +0100

    Version 0.0.1

---
+++ FSharpPlus/AssemblyInfo.fs
@@ -0,0 +1,18 @@
+﻿module FSharpPlus.AssemblyInfo
+
+open System.Reflection
+
+[<assembly: AssemblyTitle("FSharpPlus")>]
+[<assembly: AssemblyDescription("")>]
+
+[<assembly: AssemblyConfiguration("")>]
+[<assembly: AssemblyCompany("")>]
+[<assembly: AssemblyProduct("")>]
+[<assembly: AssemblyCopyright("Copyright © 2013")>]
+[<assembly: AssemblyTrademark("")>]
+[<assembly: AssemblyCulture("")>]
+
+[<assembly: AssemblyVersion("0.0.0.0")>]
+[<assembly: AssemblyFileVersion("0.0.0.0")>]
+[<assembly: AssemblyInformationalVersion("0.0.1")>]
+do()
