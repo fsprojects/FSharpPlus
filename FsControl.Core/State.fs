c99819ea9ac929c17ceaa44defc653469ce7f7dd
Gustavo <gustavo@perezleon.com.ar>
2014-04-20 15:39:19 +0200

    Replace Default <*> implementation for State.
    Replaced with a specific implementation.
    It speeds up a bit.
    Here are some benchmarks with zipIndex from https://github.com/gmpl/FsControl/issues/22
    
    --> Timing now on (Specific implementation)
    
    >  zipIndex [1..10000] ;;
    Real: 00:00:00.044, CPU: 00:00:00.046, GC gen0: 1, gen1: 0, gen2: 0
    Real: 00:00:00.024, CPU: 00:00:00.031, GC gen0: 1, gen1: 0, gen2: 0
    Real: 00:00:00.021, CPU: 00:00:00.015, GC gen0: 1, gen1: 0, gen2: 0
    
    --> Timing now on (Default implementation)
    
    >  zipIndex [1..10000] ;;
    Real: 00:00:00.057, CPU: 00:00:00.062, GC gen0: 1, gen1: 0, gen2: 0
    Real: 00:00:00.037, CPU: 00:00:00.031, GC gen0: 1, gen1: 0, gen2: 0
    Real: 00:00:00.037, CPU: 00:00:00.031, GC gen0: 1, gen1: 0, gen2: 0

--- FsControl.Core/State.fs
+++ FsControl.Core/State.fs
@@ -11,11 +11,15 @@ module State =
     let map  f (State m)  = State(f << m) :State<'s,_>
     let eval (State sa) (s:'s) = fst(sa s)
     let exec (State sa) (s:'s) = snd(sa s)
-    let get() = State (fun s -> (s , s))       :State<'s,_>
-    let put x = State (fun _ -> ((), x))       :State<'s,_>
+    let get() = State (fun s -> (s , s))  :State<'s,_>
+    let put x = State (fun _ -> ((), x))  :State<'s,_>
 
 type State<'S,'A> with
     static member instance (_:Functor.Map   ,   State m,              _) = fun f -> State(fun s -> let (a, s') = m s in (f a, s'))         :State<'s,_>
     static member instance (Applicative.Pure, _:State<'s,'a>           ) = fun a -> State(fun s -> (a, s))                                 :State<'s,'a>
     static member instance (Monad.Bind  ,   State m, _:State<'s,'b>) = fun k -> State(fun s -> let (a, s') = m s in State.run(k a) s')     :State<'s,'b>
-    static member instance (_:Applicative.Apply, f:State<'s,_>, x:State<'s,'a>, _:State<'s,'b>) = fun () -> DefaultImpl.ApplyFromMonad f x :State<'s,'b>
+    static member instance (_:Applicative.Apply, f:State<'s,_>, x:State<'s,'a>, _:State<'s,'b>) = fun () -> 
+        State(fun st -> 
+            let (f, st1) = State.run f st
+            let (x, st2) = State.run x st1
+            (f x, st2))   :State<'s,'b>
