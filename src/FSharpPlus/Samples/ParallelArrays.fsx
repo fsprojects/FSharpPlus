4c2ea4f17332ed53831de9f8f502ac215eabab8e
gmpl <gustavo.perezleon@gmail.com>
2014-06-16 14:42:38 +0200

    + Module ApplicativeMath.

--- src/FSharpPlus/Samples/ParallelArrays.fsx
+++ src/FSharpPlus/Samples/ParallelArrays.fsx
@@ -8,6 +8,9 @@ let arr1 = [| 1..100000|]
 let arr2 = [|10..100000|]
 
 let arr1_plus_arr2  = (+) <!> parray arr1 <*> parray arr2
+
+open ApplicativeMath
+
 let arr1_plus_arr2' = parray arr1 |+| parray arr2
 let arrCombined     = 10 *| parray arr1 |+| parray arr2 |- 5
 let asMonoid        = Const "Hello " </mappend/> parray [|"City"; "World"; "Sun"|]
