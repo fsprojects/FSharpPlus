6cb047fc71ee20bcc89422acbd2f8db1c196b880
gmpl <gustavo.perezleon@gmail.com>
2014-06-26 21:42:15 +0200

    Version 0.0.4

--- FSharpPlus/AssemblyInfo.fs
+++ FSharpPlus/AssemblyInfo.fs
@@ -14,5 +14,5 @@ open System.Reflection
 
 [<assembly: AssemblyVersion("0.0.0.0")>]
 [<assembly: AssemblyFileVersion("0.0.0.0")>]
-[<assembly: AssemblyInformationalVersion("0.0.3")>]
+[<assembly: AssemblyInformationalVersion("0.0.4")>]
 do()
