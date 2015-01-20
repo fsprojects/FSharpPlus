e054186f47e0ebefcee8283920ab0a7aa23de68f
gmpl <gustavo.perezleon@gmail.com>
2015-01-20 17:07:03 +0100

    Fix map and apply for ContT.

--- FsControl.Core/ContT.fs
+++ FsControl.Core/ContT.fs
@@ -13,15 +13,15 @@ type ContT<'Mr,'A> = ContT of  (('A -> 'Mr) -> 'Mr)
 [<RequireQualifiedAccess>]
 module ContT =
     let run   (ContT x) = x
-    let inline map f (ContT m) = ContT(fmap (Cont.map f) m)
-    let bind f (ContT m) = ContT(fun c -> m (fun a -> run (f a) c)) :ContT<'mr,'b>
-    let inline apply (ContT f) (ContT x) = ContT(fmap (<*>) f <*> x) :ContT<'mr,'a>
+    let map f (ContT m) = ContT(fun k -> m (k << f))
+    let bind f (ContT m) = ContT(fun k -> m (fun a -> run (f a) k)) :ContT<'mr,'b>
+    let apply (ContT f) (ContT x) = ContT (fun k -> f (fun f' -> x (k << f')))  :ContT<'mr,'a>
 
 type ContT<'Mr,'A> with
     static member instance (_:Functor.Map, x, _) = fun f -> ContT.map f x
 
     static member instance (Applicative.Pure, _:ContT<'mr,'a>           ) = fun a -> ContT((|>) a) :ContT<'mr,'a>
-    static member inline instance (_:Applicative.Apply, f, x, _:ContT<'mr,'b>) = fun () -> ContT.apply f x :ContT<'mr,'b>
+    static member instance (_:Applicative.Apply, f, x, _:ContT<'mr,'b>) = fun () -> ContT.apply f x :ContT<'mr,'b>
     static member instance (Monad.Bind  , x, _:ContT<'mr,'b>) = fun f -> ContT.bind f x :ContT<'mr,'b>
 
     static member inline instance (MonadTrans.Lift  , _:ContT<'mr,'a>) = fun (m:'ma) -> ContT((>>=) m) : ContT<'mr,'a>    
