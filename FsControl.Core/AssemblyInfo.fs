e424196313a01bd555db649fce839f2fdc348584
gmpl <gustavo.perezleon@gmail.com>
2015-01-30 11:43:39 +0100

    Version 1.0.10

--- FsControl.Core/AssemblyInfo.fs
+++ FsControl.Core/AssemblyInfo.fs
@@ -14,5 +14,5 @@ open System.Reflection
 
 [<assembly: AssemblyVersion("1.0.0.0")>]
 [<assembly: AssemblyFileVersion("1.0.0.0")>]
-[<assembly: AssemblyInformationalVersion("1.0.9")>]
+[<assembly: AssemblyInformationalVersion("1.0.10")>]
 do()
