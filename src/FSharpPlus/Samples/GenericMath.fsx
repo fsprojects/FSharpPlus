1e315e0d1378780006b772865bde1d65c883ea7e
gmpl <gustavo.perezleon@gmail.com>
2015-01-30 19:45:31 +0100

    + GenericMath functions and sample file.

---
+++ src/FSharpPlus/Samples/GenericMath.fsx
@@ -0,0 +1,16 @@
+﻿#r @"..\..\build\FsControl.Core.dll"
+#r @"..\..\build\FSharpPlus.dll"
+
+open FSharpPlus
+open GenericMath
+
+let inline areaOfCircle radio =
+    let pi = 
+        314159265358979323846264338I 
+            </ratio/> 
+        100000000000000000000000000I
+    fromRational pi * radio * radio
+
+let area1 = areaOfCircle 5.
+let area2 = areaOfCircle 5.0f
+let area3 = areaOfCircle 5.0M
