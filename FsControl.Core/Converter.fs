8f68ac32db15aece7ffb1096903f4f9aad024cb0
mausch <mauricioscheffer@gmail.com>
2015-05-24 21:29:25 +0100

    + Uri toString

--- FsControl.Core/Converter.fs
+++ FsControl.Core/Converter.fs
@@ -132,6 +132,7 @@ type ToString() =
     static member ToString (x:int64         , _:ToString) = fun (k:CultureInfo) -> x.ToString k
     static member ToString (x:float32       , _:ToString) = fun (k:CultureInfo) -> x.ToString k
     static member ToString (x:string        , _:ToString) = fun (_:CultureInfo) -> if x = null then "null" else x
+    static member ToString (x:Uri           , _:ToString) = fun (_:CultureInfo) -> if x = null then "null" else x.ToString()
     static member ToString (x:uint16        , _:ToString) = fun (k:CultureInfo) -> x.ToString k
     static member ToString (x:uint32        , _:ToString) = fun (k:CultureInfo) -> x.ToString k
     static member ToString (x:uint64        , _:ToString) = fun (k:CultureInfo) -> x.ToString k
