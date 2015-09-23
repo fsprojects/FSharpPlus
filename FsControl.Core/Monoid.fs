aa65250bc899e62dda4a949b9be72f714328aafb
gmpl <gustavo.perezleon@gmail.com>
2015-09-24 01:36:58 +0200

    + Wrappers.fs

--- FsControl.Core/Monoid.fs
+++ FsControl.Core/Monoid.fs
@@ -173,29 +173,3 @@ type Mconcat with
 type Mconcat with
     [<Extension>]static member inline Mconcat (x:seq< ^R>, [<Optional>]output:^R, _:Default1) = ((^R) : (static member Mconcat: 'R seq -> ^R) x)
                  static member inline Mconcat (x:seq< ^R>, _:^t when ^t: null and ^t: struct, _:Default1) = fun () -> id
-
-
-
-
-namespace FsControl.Core.Types
-open FsControl.Core.Prelude
-open FsControl.Core.TypeMethods
-
-type Dual<'a> = Dual of 'a with
-    static member inline Mempty  (_:Dual<'m>, _:Mempty) = Dual (Mempty.Invoke()) :Dual<'m>
-    static member inline Mappend (  Dual x  ,   Dual y) = Dual (Mappend.Invoke y x)
-module Dual = let inline  internal getDual (Dual x) = x
-
-type Endo<'a> = Endo of ('a -> 'a) with
-    static member        Mempty  (_:Endo<'m>, _:Mempty) = Endo id  :Endo<'m>
-    static member        Mappend (  Endo f  ,   Endo g) = Endo (f << g)
-module Endo = let inline  internal appEndo (Endo f) = f
-
-
-type All = All of bool with
-    static member Mempty  (_:All, _:Mempty) = All true
-    static member Mappend (  All x, All y ) = All (x && y)
-
-type Any = Any of bool with
-    static member Mempty  (_:Any, _:Mempty) = Any false
-    static member Mappend (  Any x, Any y ) = Any (x || y)
---
+++ FsControl.Core/Wrappers.fs
@@ -0,0 +1,22 @@
+﻿namespace FsControl.Core.Types
+open FsControl.Core.Prelude
+open FsControl.Core.TypeMethods
+
+type Dual<'a> = Dual of 'a with
+    static member inline Mempty  (_:Dual<'m>, _:Mempty) = Dual (Mempty.Invoke()) :Dual<'m>
+    static member inline Mappend (  Dual x  ,   Dual y) = Dual (Mappend.Invoke y x)
+module Dual = let inline  internal getDual (Dual x) = x
+
+type Endo<'a> = Endo of ('a -> 'a) with
+    static member        Mempty  (_:Endo<'m>, _:Mempty) = Endo id  :Endo<'m>
+    static member        Mappend (  Endo f  ,   Endo g) = Endo (f << g)
+module Endo = let inline  internal appEndo (Endo f) = f
+
+
+type All = All of bool with
+    static member Mempty  (_:All, _:Mempty) = All true
+    static member Mappend (  All x, All y ) = All (x && y)
+
+type Any = Any of bool with
+    static member Mempty  (_:Any, _:Mempty) = Any false
+    static member Mappend (  Any x, Any y ) = Any (x || y)
