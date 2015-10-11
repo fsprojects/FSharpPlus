39fcc4982bc05a60092b4b26ff069495caa47b44
gmpl <gustavo.perezleon@gmail.com>
2015-10-11 16:10:17 +0200

    + Examples.

--- src/FSharpPlus/Samples/Conversions.fsx
+++ src/FSharpPlus/Samples/Conversions.fsx
@@ -4,7 +4,11 @@
 open FSharpPlus
 
 let r23asBytes = parse "12" + 11 |> toBytes |> toList
-let r10:float = explicit 10
+let ipAddress:System.Net.IPAddress = [192;168;0;1] |>> string |> intersperse "." |> mconcat |> parse
+let r10:float  = explicit 10
+let r11:System.Nullable<_> = implicit 11
 
 open GenericMath
-let r11 = toBigInt (5G + 6G)
+let r12:float = fromBigInt 10I + 2G
+let r13       = toBigInt (5G + 8G)
+let r14:float = parse "11" + 3G
