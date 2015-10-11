7ec51998dfb494cc5a6583a63a5152effa739f35
gmpl <gustavo.perezleon@gmail.com>
2015-10-11 16:10:17 +0200

    + Examples.

--- FSharpPlus/Samples/Applicatives.fsx
+++ FSharpPlus/Samples/Applicatives.fsx
@@ -6,3 +6,7 @@ open FSharpPlus
 let lst5n6  = map ((+) 4) [ 1;2 ]
 let arr5n6  = map ((+) 4) [|1;2|]
 let lst5n6' = (+) <!> [4] <*> [1;2]
+let opt120  = (+) <!> Some 20 <*> tryParse "100"
+
+open FSharpPlus.Builders
+let opt121  = iI (+) (Some 21) (tryParse "100") Ii
