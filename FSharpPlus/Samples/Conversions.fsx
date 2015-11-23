917d7121576598e2da399c180aa8bd7311f3f6fe
gmpl <gustavo.perezleon@gmail.com>
2015-11-23 19:41:05 +0100

    + Parse active recognizer.

--- FSharpPlus/Samples/Conversions.fsx
+++ FSharpPlus/Samples/Conversions.fsx
@@ -5,6 +5,12 @@ open FSharpPlus
 
 let r23asBytes = parse "12" + 11 |> toBytes |> toList
 let ipAddress:System.Net.IPAddress = [192;168;0;1] |>> string |> intersperse "." |> concat |> parse
+
+let ipAndPort : (System.Net.IPAddress * int) option = 
+    match "192.168.0.1:8001".Split ':' with 
+    | [|Parse ip; Parse port|] -> Some (ip, port)
+    | _ -> None
+
 let r10:float  = explicit 10
 let r11:System.Nullable<_> = implicit 11
 
