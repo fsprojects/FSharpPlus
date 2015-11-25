9a6e441de5bf2d6f5fa4a82299414721b0c6a826
gmpl <gustavo.perezleon@gmail.com>
2015-11-25 14:20:01 +0100

    Merge each Monad with its Transformer in the same file.

--- src/FSharpPlus/Seq.fs
+++ src/FSharpPlus/Seq.fs
@@ -38,3 +38,45 @@ module Seq =
     let replicate count initial = System.Linq.Enumerable.Repeat(initial, count)
 
     let inline replicateM count (initial:'``Applicative<'T>``) = sequence (replicate count initial)
+
+open FsControl
+
+type SeqT<'``monad<seq<'t>>``> = SeqT of '``monad<seq<'t>>``
+
+[<RequireQualifiedAccess>]
+module SeqT =
+    let run (SeqT m) = m
+
+    let inline internal sequence ms =
+        let k m m' = m >>= fun (x:'a) -> m' >>= fun (xs:seq<'a>) -> (result :seq<'a> -> 'M) (seq {yield x; yield! xs})
+        Seq.foldBack k ms ((result :seq<'a> -> 'M) Seq.empty)
+
+    let inline internal mapM f as' = sequence (Seq.map f as')
+
+    let inline map  (f:'T->'U) (SeqT m : SeqT<'``Monad<seq<'T>``>)                                            = SeqT <| map (Seq.map f: (seq<_>->_)) m      : SeqT<'``Monad<seq<'U>``>
+    let inline bind (f:'T-> SeqT<'``Monad<seq<'U>``>) (SeqT m : SeqT<'``Monad<seq<'T>``>)               = SeqT (m >>= (mapM:_->seq<_>->_) (run << f) >>= ((Seq.concat:seq<seq<_>>->_) >> result)) 
+    let inline apply (SeqT f : SeqT<'``Monad<seq<('T -> 'U)>``>) (SeqT x : SeqT<'``Monad<seq<'T>``>) = SeqT (Map.Invoke (Seq.apply:seq<_->_>->seq<_>->seq<_>) f <*> x)          : SeqT<'``Monad<seq<'U>``>       
+
+type SeqT with
+    static member inline Map    (x : SeqT<'``Monad<seq<'T>``>, f : 'T->'U , impl:Map)                                                       =  SeqT.map f x                                                        : SeqT<'``Monad<seq<'U>``>
+    static member inline Return (output : SeqT<'``Monad<seq<'T>``>, impl:Return)                                                            = SeqT << result << Seq.singleton                                      : 'T -> SeqT<'``Monad<seq<'T>``>
+    static member inline Apply  (f : SeqT<'``Monad<seq<('T -> 'U)>``>, x : SeqT<'``Monad<seq<'T>``>, output:SeqT<'r>, impl:Apply ) = SeqT.apply f x                                                       : SeqT<'``Monad<seq<'U>``>
+    static member inline Bind   (x  : SeqT<'``Monad<seq<'T>``>, f: 'T -> SeqT<'``Monad<seq<'U>``>)                                    = SeqT.bind f x
+
+    static member inline MZero (output: SeqT<'``MonadPlus<seq<'T>``>, impl:MZero)                                                           = SeqT <| result Seq.empty                                             : SeqT<'``MonadPlus<seq<'T>``>
+    static member inline MPlus (SeqT x, SeqT y, impl:MPlus)                                                                                 = SeqT <| (x >>= (fun a -> y >>= (fun b ->  result ((Seq.append:seq<_>->seq<_>->_) a b)))) : SeqT<'``MonadPlus<seq<'T>``>
+
+    static member inline Lift (x:'``Monad<'T>``) = x |> (Map.FromMonad Seq.singleton ) |> SeqT    :  SeqT<'``Monad<seq<'T>>``>
+    
+    static member inline LiftAsync (x : Async<'T>) = lift (liftAsync x)
+    
+    static member inline ThrowError (x:'E) = x |> throw |> lift
+    static member inline CatchError (m:SeqT<'``MonadError<'E1,'T>``>   , h:'E1 -> SeqT<'``MonadError<'E2,'T>``>)    = SeqT    ((fun v h -> catch v h) (SeqT.run    m) (SeqT.run    << h)) : SeqT<'``MonadError<'E2,'T>``>
+    
+    static member inline CallCC (f:(('T -> SeqT<'``MonadCont<'R,seq<'U>>``>) -> _)) = SeqT (callCC <| fun c -> SeqT.run (f (SeqT  << c << Seq.singleton ))) : SeqT<'``MonadCont<'R, seq<'T>>``>
+    
+    static member inline get_Get()  = lift get                                          : '``SeqT<'MonadState<'S,'S>>``
+    static member inline Put (x:'T) = x |> put |> lift                                  : '``SeqT<'MonadState<unit,'S>>``
+    
+    static member inline get_Ask() = lift ask                                           : '``SeqT<'MonadReader<'R,seq<'R>>>``
+    static member inline Local (SeqT (m:'``MonadReader<'R2,'T>``), f:'R1->'R2) = SeqT (local f m)
--- src/FSharpPlus/State.fs
+++ src/FSharpPlus/State.fs
@@ -30,3 +30,33 @@ type State with
     static member (<*>) (f, x:State<'S,'T>) = State.apply f x : State<'S,'U>
     static member get_Get() = State.get                       : State<'S,'S>
     static member Put x     = State.put x                     : State<'S,unit>
+
+
+type StateT<'s,'``monad<'t * 's>``> = StateT of ('s -> '``monad<'t * 's>``)
+
+[<RequireQualifiedAccess>]
+module StateT =
+    let run (StateT x) = x : 'S -> '``Monad<'T * 'S>``
+    let inline map  (f:'T->'U) (StateT (m:_->'``Monad<'T * 'S>``)) = StateT (m >> Map.Invoke (fun (a, s') -> (f a, s')))     : StateT<'S,'``Monad<'U * 'S>``>
+    let inline apply  (StateT f: StateT<'S,'``Monad<('T -> 'U) * 'S>``>) (StateT a: StateT<'S,'``Monad<'T * 'S>``>) = StateT (fun s -> f s >>= fun (g, t) -> Map.Invoke (fun (z, u) -> (g z, u)) (a t)) : StateT<'S,'``Monad<'U * 'S>``>
+    let inline bind (f:'T->StateT<'S,'``Monad<'U * 'S>``>) (StateT m: StateT<'S,'``Monad<'T * 'S>``>) = StateT <| fun s -> m s >>= (fun (a, s') -> run (f a) s')
+
+type StateT with
+    static member inline Map    (x: StateT<'S,'``Monad<'T * 'S>``>, f:'T->'U     , impl:Map   ) = StateT.map f x                                : StateT<'S,'``Monad<'U * 'S>``>
+    static member inline Return (output: StateT<'S,'``Monad<'T * 'S>``>, impl:Return) = fun (a:'T) -> StateT (fun s -> result (a, s))           : StateT<'S,'``Monad<'T * 'S>``>
+    static member inline Apply  (f: StateT<'S,'``Monad<('T -> 'U) * 'S>``>, x: StateT<'S,'``Monad<'T * 'S>``>, output: StateT<'S,'``Monad<'U * 'S>``>, impl:Apply ) = StateT.apply f x : StateT<'S,'``Monad<'U * 'S>``>
+    static member inline Bind   (x:StateT<'S,'``Monad<'T * 'S>``>, f :'T->StateT<'S,'``Monad<'U * 'S>``>) = StateT.bind f x
+
+    static member inline MZero (output:StateT<'S,'``MonadPlus<'T * 'S>``>, impl:MZero) = StateT (fun _ -> getMZero()) : StateT<'S,'``MonadPlus<'T * 'S>``>
+    static member inline MPlus (  StateT m, StateT n, impl:MPlus) = StateT (fun s -> m s <|> n s   )                  : StateT<'S,'``MonadPlus<'T * 'S>``>
+
+    static member inline Lift (m:'``Monad<'T>``) : StateT<'S,'``Monad<'T * 'S>``> = StateT <| fun s -> m >>= fun a -> result (a, s)
+
+    static member inline LiftAsync (x: Async<'T>) = lift (liftAsync x)
+    
+    static member inline get_Get()  = StateT (fun s -> result (s , s))  : StateT<'S, '``Monad<'S * 'S>``>
+    static member inline Put (x:'S) = StateT (fun _ -> result ((), x))  : StateT<'S, '``Monad<unit * 'S>``>
+
+    static member inline ThrowError (x:'E) = x |> throw |> lift
+    static member inline CatchError (m:StateT<'S,'``MonadError<'E1,'T * 'S>``>, h:'E1 -> _) = 
+        StateT (fun s -> catch (StateT.run m s) (fun e -> StateT.run (h e) s)) : StateT<'S,'``MonadError<'E2, 'T * 'S>``>
