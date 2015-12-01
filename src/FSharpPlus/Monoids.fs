4226c7bcc4972558f3af15b4491e8a2881faba3b
gmpl <gustavo.perezleon@gmail.com>
2015-12-01 09:57:18 +0100

    + Identity as a Monad. Moved into a single file. + Comments and type annotations.

--- src/FSharpPlus/Monoids.fs
+++ src/FSharpPlus/Monoids.fs
@@ -2,50 +2,67 @@
 
 open FSharpPlus.Operators
 
-type Dual<'T> = Dual of 'T with
-    static member inline get_Empty() = Dual (getEmpty())
-    static member inline Append (Dual x, Dual y) = Dual (append y x)
-[<RequireQualifiedAccess>]module Dual = let run (Dual x) = x
+/// The dual of a monoid, obtained by swapping the arguments of append.
+type Dual<'t> = Dual of 't with
+    static member inline get_Empty() = Dual (getEmpty())                : Dual<'T>
+    static member inline Append (Dual x, Dual y) = Dual (append y x)    : Dual<'T>
 
-type Endo<'T> = Endo of ('T -> 'T) with
-    static member get_Empty() = Endo id
-    static member Append (Endo f, Endo g) = Endo (f << g)
-[<RequireQualifiedAccess>]module Endo = let run (Endo f) = f
+[<RequireQualifiedAccess>]
+module Dual = let run (Dual x) = x          : 'T
+
+/// The monoid of endomorphisms under composition.
+type Endo<'t> = Endo of ('t -> 't) with
+    static member get_Empty() = Endo id                                 : Endo<'T>
+    static member Append (Endo f, Endo g) = Endo (f << g)               : Endo<'T>
+
+[<RequireQualifiedAccess>]
+module Endo = let run (Endo f) = f          : 'T -> 'T
 
 
+/// Boolean monoid under conjunction.
 type All = All of bool with
     static member Empty = All true
     static member Append (All x, All y) = All (x && y)
 
+/// Boolean monoid under disjunction.
 type Any = Any of bool with
     static member Empty = Any false
     static member Append (Any x, Any y) = Any (x || y)
 
 
-type Identity<'t> = Identity of 't with
-    static member Return (_:Identity<'T>  , _:FsControl.Return) = fun x -> Identity x :Identity<'T>
-    static member Apply (Identity (f:'T->'U)     , Identity (x: 'T)     , output:Identity<'U> , impl:FsControl.Apply) = Identity (f x)      : Identity<'U>
-    static member Map (Identity x         , f:'T->'U) = Identity (f x)
-[<RequireQualifiedAccess>]module Identity = let run (Identity x) = x
-
 type Const<'t,'u> = Const of 't with
+
+    // Monoid
     static member inline get_Empty() = Const (getEmpty()) : Const<'T,'U>
     static member inline Append (Const x : Const<'T,'U>, Const y : Const<'T,'U>) = Const (append x y) : Const<'T,'U>
+
+    // Functor
+    static member Map (Const x : Const<_,'T>, f:'T->'U) = Const x : Const<'C,'U>
+
+    // Applicative
     static member inline Return (_:'U) = Const (getEmpty()) : Const<'T,'U>
-    static member inline Apply (Const f : Const<'C,'T->'U>, Const x : Const<'C,'T>, output:Const<'C,'U>, impl:FsControl.Apply) = Const (append f x) : Const<'C,'U>
-    static member        Map (Const x : Const<_,'T>, f:'T->'U) = Const x : Const<'C,'U>
-    static member        Contramap (Const x : Const<'C,'T>, _:'U->'T) = Const x  :Const<'C,'U>
-    static member        First  (Const x : Const<'T,'V>, f:'T->'U) = Const (f x) : Const<'U,'V>
-    static member        Second (Const x : Const<'T,'V>, _:'V->'W) = Const x : Const<'T,'W>
-[<RequireQualifiedAccess>]module Const = let run (Const t) = t
+    static member inline (<*>) (Const f : Const<'C,'T->'U>, Const x : Const<'C,'T>) = Const (append f x) : Const<'C,'U>
+
+    // Contravariant
+    static member Contramap (Const x : Const<'C,'T>, _:'U->'T) = Const x        : Const<'C,'U>
+
+    // Bifunctor
+    static member First     (Const x : Const<'T,'V>, f:'T->'U) = Const (f x)    : Const<'U,'V>
+    static member Second    (Const x : Const<'T,'V>, _:'V->'W) = Const x        : Const<'T,'W>
+
+[<RequireQualifiedAccess>]
+module Const =
+    let run (Const t) = t
 
 
+/// Option<'T> monoid returning the leftmost non-None value.
 type First<'t> = First of Option<'t> with
-    static member Empty  (_:First<'T>, _:FsControl.Empty   ) = First None :First<'T>
-    static member Append (x:First<'T>, y:First<'T>) = match x, y with First None, r -> r | l, _ -> l
-    static member run (First a) = a
+    static member get_Empty() = First None                                          : First<'T>
+    static member Append (x, y) = match x, y with First None, r -> r | l, _ -> l    : First<'T>
+    static member run (First a) = a                                                 : 'T option
 
+/// Option<'T> monoid returning the rightmost non-None value.
 type Last<'t> = Last of Option<'t> with
-    static member Empty = Last None
-    static member Append (x:Last<'T>, y:Last<'T>) = match x, y with l, Last None -> l | _, r -> r
-[<RequireQualifiedAccess>]module Last = let run (Last a) = a
+    static member get_Empty() = Last None                                           : Last<'T>
+    static member Append (x, y) = match x, y with l, Last None -> l | _, r -> r     : Last<'T>
+    static member run (Last a) = a                                                  : 'T option
