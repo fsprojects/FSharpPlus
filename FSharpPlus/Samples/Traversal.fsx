e9b82a0dbcdd3a6019efb3d6f16759f9baeabb64
gmpl <gustavo.perezleon@gmail.com>
2016-01-28 19:12:17 +0100

    Use forward pipe in traversal.fsx samples to improve type inference.

--- FSharpPlus/Samples/Traversal.fsx
+++ FSharpPlus/Samples/Traversal.fsx
@@ -5,21 +5,21 @@
 open FSharpPlus
 open FSharpPlus.Lens
 
-let x = set  (_all "") ("Nothing") [|"Something"; ""; "Something Else"; "";|]
+let x = [|"Something"; ""; "Something Else"; "";|] |> set (_all "") ("Nothing")
 // val it : string [] = c
 
 // we can preview it
-let r2 = preview (_all "Something") [|"Something"; "Nothing"; "Something Else"; "Nothing"|]
+let r2 = [|"Something"; "Nothing"; "Something Else"; "Nothing"|] |> preview (_all "Something")
 // val it : Option<string> = Some "Something"
 
 // view all elements in a list
-let r3 = toListOf (_all "Something") [|"Something"; "Nothing"; "Something Else"; "Nothing"|]
+let r3 = [|"Something"; "Nothing"; "Something Else"; "Nothing"|] |> toListOf (_all "Something")
 // val it : string list = ["Something"]
 
 // also view it, since string is a monoid
-let r4 = view  (_all "Something") [|"Something"; "Nothing"; "Something Else"; "Nothing"|]
+let r4 = [|"Something"; "Nothing"; "Something Else"; "Nothing"|] |> view  (_all "Something")
 // val it : string = "Something"
 
 // Lens composed with a Traversal -> Traversal
-let r5 = preview  (_all ((),"Something") << _2) [((), "Something"); ((),""); ((), "Something Else"); ((),"")]
+let r5 = [((), "Something"); ((),""); ((), "Something Else"); ((),"")] |> preview  (_all ((),"Something") << _2)
 // val r5 : Option<string> = Some "Something"
