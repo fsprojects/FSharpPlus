090265608a6597e7dce623d0d7ec9a7edff80d8c
dogwith1eye <dogwith1eye@gmail.com>
2016-12-26 21:58:11 -0600

    Initial scaffold

---
+++ tests/FSharpPlus.Tests/Tests.fs
@@ -0,0 +1,10 @@
+module FSharpPlus.Tests
+
+open FSharpPlus
+open NUnit.Framework
+
+[<Test>]
+let ``hello returns 42`` () =
+  let result = Library.hello 42
+  printfn "%i" result
+  Assert.AreEqual(42,result)
