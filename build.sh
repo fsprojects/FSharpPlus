aff4f0dff01dc7ef508abaccc9b1520a2909c011
Gustavo Leon <gustavo.perezleon@gmail.com>
2017-01-15 00:49:09 +0100

    Clean up

---
+++ build.sh
@@ -0,0 +1,77 @@
+#!/usr/bin/env bash
+
+set -eu
+
+cd "$(dirname "$0")"
+
+PAKET_BOOTSTRAPPER_EXE=.paket/paket.bootstrapper.exe
+PAKET_EXE=.paket/paket.exe
+FAKE_EXE=packages/build/FAKE/tools/FAKE.exe
+
+FSIARGS=""
+FSIARGS2=""
+OS=${OS:-"unknown"}
+if [ "$OS" != "Windows_NT" ]
+then
+  # Can't use FSIARGS="--fsiargs -d:MONO" in zsh, so split it up
+  # (Can't use arrays since dash can't handle them)
+  FSIARGS="--fsiargs"
+  FSIARGS2="-d:MONO"
+fi
+
+run() {
+  if [ "$OS" != "Windows_NT" ]
+  then
+    mono "$@"
+  else
+    "$@"
+  fi
+}
+
+yesno() {
+  # NOTE: Defaults to NO
+  read -p "$1 [y/N] " ynresult
+  case "$ynresult" in
+    [yY]*) true ;;
+    *) false ;;
+  esac
+}
+
+set +e
+run $PAKET_BOOTSTRAPPER_EXE
+bootstrapper_exitcode=$?
+set -e
+
+if [ "$OS" != "Windows_NT" ] &&
+       [ $bootstrapper_exitcode -ne 0 ] &&
+       [ $(certmgr -list -c Trust | grep X.509 | wc -l) -le 1 ] &&
+       [ $(certmgr -list -c -m Trust | grep X.509 | wc -l) -le 1 ]
+then
+  echo "Your Mono installation has no trusted SSL root certificates set up."
+  echo "This may result in the Paket bootstrapper failing to download Paket"
+  echo "because Github's SSL certificate can't be verified. One way to fix"
+  echo "this issue would be to download the list of SSL root certificates"
+  echo "from the Mozilla project by running the following command:"
+  echo ""
+  echo "    mozroots --import --sync"
+  echo ""
+  echo "This will import over 100 SSL root certificates into your Mono"
+  echo "certificate repository."
+  echo ""
+  if yesno "Run 'mozroots --import --sync' now?"
+  then
+    mozroots --import --sync
+  else
+    echo "Attempting to continue without running mozroots. This might fail."
+  fi
+  # Re-run bootstrapper whether or not the user ran mozroots, because maybe
+  # they fixed the problem in a separate terminal window.
+  run $PAKET_BOOTSTRAPPER_EXE
+fi
+
+run $PAKET_EXE restore
+
+[ ! -e build.fsx ] && run $PAKET_EXE update
+[ ! -e build.fsx ] && run $FAKE_EXE init.fsx
+run $FAKE_EXE "$@" $FSIARGS $FSIARGS2 build.fsx
+
---
+++ src/FSharpPlus/ParallelArray.fs
@@ -0,0 +1,42 @@
+﻿namespace FSharpPlus
+
+open FsControl
+open FSharpPlus.Operators
+
+type ParallelArray<'t> =
+    | Infinite of 't
+    | Bounded  of 't array
+
+
+module ParallelArray =
+    let run = function
+        | Bounded a -> a
+        | _         -> invalidOp "Resulting array would be infinite."
+
+    let map f = function
+        | Infinite s -> Infinite   (f s)
+        | Bounded  a -> Bounded (Array.Parallel.map f a)
+
+    let ap f x = 
+        match (f, x) with
+        | Infinite f, Infinite x -> Infinite (f x)
+        | Infinite f, Bounded  x -> Bounded (Array.Parallel.map f x)
+        | Bounded  f, Infinite x -> Bounded (Array.Parallel.map ((|>) x) f)
+        | Bounded  f, Bounded  x -> 
+            if f.LongLength < x.LongLength then Bounded (Array.Parallel.mapi (fun i f -> f x.[i]) f)
+            else                                Bounded (Array.Parallel.mapi (fun i x -> f.[i] x) x)
+
+    //let inline append (a:ParallelArray<'m>) (b:ParallelArray<'m>) = liftA2 mappend a b :ParallelArray<'m>
+
+type parray<'t> = ParallelArray<'t>
+
+[<AutoOpen>]
+module ParallelArrayOperators =
+    let parray s = Bounded s
+
+type ParallelArray with
+    static member Map (x:parray<_>, f) = ParallelArray.map f x
+    static member Return (x:'a) = Infinite x
+    static member (<*>) (f:parray<'a->'b>, x:parray<_>) = ParallelArray.ap f x :parray<'b>
+    static member inline get_Empty() = Bounded (getEmpty()) : parray<'m>
+    static member inline Append (x:parray<'m>, y:parray<'m>) = liftA2 append x y:parray<'m>
