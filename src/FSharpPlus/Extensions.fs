5d7645262b52c4b2c1989343b71772a87d11cde6
Gustavo Leon <gustavo.perezleon@gmail.com>
2017-03-12 19:56:11 +0100

    Fix indentation

--- src/FSharpPlus/Extensions.fs
+++ src/FSharpPlus/Extensions.fs
@@ -25,115 +25,115 @@ module Error =
 
 /// Additional operations on List
 module List =
-        let singleton x = [x]
-        let cons x y = x :: y
-        let apply f x = List.collect (fun f -> List.map ((<|) f) x) f
-        let tails x = let rec loop = function [] -> [] | _::xs as s -> s::(loop xs) in loop x
-        let take i list = Seq.take i list |> Seq.toList
+    let singleton x = [x]
+    let cons x y = x :: y
+    let apply f x = List.collect (fun f -> List.map ((<|) f) x) f
+    let tails x = let rec loop = function [] -> [] | _::xs as s -> s::(loop xs) in loop x
+    let take i list = Seq.take i list |> Seq.toList
 
-        let skip i list =
-            let rec listSkip lst = function 
-                | 0 -> lst 
-                | n -> listSkip (List.tail lst) (n-1)
-            listSkip list i
+    let skip i list =
+        let rec listSkip lst = function 
+            | 0 -> lst 
+            | n -> listSkip (List.tail lst) (n-1)
+        listSkip list i
 
-        let drop i list = 
-            let rec loop i lst = 
-                match (lst, i) with
-                | ([] as x, _) | (x, 0) -> x
-                | x, n -> loop (n-1) (List.tail x)
-            if i > 0 then loop i list else list
+    let drop i list = 
+        let rec loop i lst = 
+            match (lst, i) with
+            | ([] as x, _) | (x, 0) -> x
+            | x, n -> loop (n-1) (List.tail x)
+        if i > 0 then loop i list else list
 
 
 /// Additional operations on Seq
 module Seq =
-        let bind (f:'a->seq<'b>) x = Seq.collect f x
-        let apply f x = bind (fun f -> Seq.map ((<|) f) x) f
-        let foldBack f x z = Array.foldBack f (Seq.toArray x) z
+    let bind (f:'a->seq<'b>) x = Seq.collect f x
+    let apply f x = bind (fun f -> Seq.map ((<|) f) x) f
+    let foldBack f x z = Array.foldBack f (Seq.toArray x) z
 
-        let chunkBy projection (source : _ seq) = seq {
-            use e = source.GetEnumerator()
-            if e.MoveNext() then
-                let g = ref (projection e.Current)
-                let members = ref (ResizeArray())
-                (!members).Add(e.Current)
-                while (e.MoveNext()) do
-                    let key = projection e.Current
-                    if !g = key then (!members).Add(e.Current)
-                    else
-                        yield (!g, !members)
-                        g := key
-                        members := ResizeArray()
-                        (!members).Add(e.Current)
-                yield (!g, !members)}
+    let chunkBy projection (source : _ seq) = seq {
+        use e = source.GetEnumerator()
+        if e.MoveNext() then
+            let g = ref (projection e.Current)
+            let members = ref (ResizeArray())
+            (!members).Add(e.Current)
+            while (e.MoveNext()) do
+                let key = projection e.Current
+                if !g = key then (!members).Add(e.Current)
+                else
+                    yield (!g, !members)
+                    g := key
+                    members := ResizeArray()
+                    (!members).Add(e.Current)
+            yield (!g, !members)}
 
-        // http://codebetter.com/matthewpodwysocki/2009/05/06/functionally-implementing-intersperse/
-        let intersperse sep list = seq {
-            let notFirst = ref false
-            for element in list do 
-                if !notFirst then yield sep
-                yield element
-                notFirst := true}
+    // http://codebetter.com/matthewpodwysocki/2009/05/06/functionally-implementing-intersperse/
+    let intersperse sep list = seq {
+        let notFirst = ref false
+        for element in list do 
+            if !notFirst then yield sep
+            yield element
+            notFirst := true}
 
-        let intercalate sep list = seq {
-            let notFirst = ref false
-            for element in list do 
-                if !notFirst then yield! sep
-                yield! element
-                notFirst := true}
+    let intercalate sep list = seq {
+        let notFirst = ref false
+        for element in list do 
+            if !notFirst then yield! sep
+            yield! element
+            notFirst := true}
 
-        let split options separators source = seq {
-            match separators |> Seq.map Seq.toList |> Seq.toList with
-            | []         -> yield source
-            | separators ->
-                let buffer = ResizeArray()
-                let candidate = separators |> List.map List.length |> List.max |> ResizeArray
-                let mutable i = 0
-                for item in source do
-                    candidate.Add item
-                    match separators |> List.filter (fun sep -> sep.Length > i && item = sep.[i]) with
-                    | [] ->
+    let split options separators source = seq {
+        match separators |> Seq.map Seq.toList |> Seq.toList with
+        | []         -> yield source
+        | separators ->
+            let buffer = ResizeArray()
+            let candidate = separators |> List.map List.length |> List.max |> ResizeArray
+            let mutable i = 0
+            for item in source do
+                candidate.Add item
+                match separators |> List.filter (fun sep -> sep.Length > i && item = sep.[i]) with
+                | [] ->
+                    i <- 0
+                    buffer.AddRange candidate
+                    candidate.Clear()                    
+                | seps ->
+                    if seps |> List.exists (fun sep -> sep.Length = i + 1) then
                         i <- 0
-                        buffer.AddRange candidate
-                        candidate.Clear()                    
-                    | seps ->
-                        if seps |> List.exists (fun sep -> sep.Length = i + 1) then
-                            i <- 0
-                            if options = System.StringSplitOptions.None || buffer.Count > 0 then yield buffer.ToArray() :> seq<_>
-                            buffer.Clear()
-                            candidate.Clear()                        
-                        else i <- i + 1
-                if candidate.Count > 0 then buffer.AddRange candidate
-                if options = System.StringSplitOptions.None || buffer.Count > 0 then yield buffer :> seq<_> }
+                        if options = System.StringSplitOptions.None || buffer.Count > 0 then yield buffer.ToArray() :> seq<_>
+                        buffer.Clear()
+                        candidate.Clear()                        
+                    else i <- i + 1
+            if candidate.Count > 0 then buffer.AddRange candidate
+            if options = System.StringSplitOptions.None || buffer.Count > 0 then yield buffer :> seq<_> }
 
-        let replace (oldValue:seq<'t>) (newValue:seq<'t>) (source:seq<'t>) :seq<'t> = seq {
-            let old = oldValue |> Seq.toList
-            if (old.Length = 0) then
-                yield! source
-            else
-                let candidate = ResizeArray(old.Length)
-                let mutable sindex = 0
-                for item in source do
-                    candidate.Add(item)
-                    if (item = old.[sindex]) then
-                        sindex <- sindex + 1
-                        if (sindex >= old.Length) then
-                            sindex <- 0
-                            yield! newValue
-                            candidate.Clear()                    
-                    else
+    let replace (oldValue:seq<'t>) (newValue:seq<'t>) (source:seq<'t>) :seq<'t> = seq {
+        let old = oldValue |> Seq.toList
+        if (old.Length = 0) then
+            yield! source
+        else
+            let candidate = ResizeArray(old.Length)
+            let mutable sindex = 0
+            for item in source do
+                candidate.Add(item)
+                if (item = old.[sindex]) then
+                    sindex <- sindex + 1
+                    if (sindex >= old.Length) then
                         sindex <- 0
-                        yield! candidate
-                        candidate.Clear()                
-                yield! candidate}
+                        yield! newValue
+                        candidate.Clear()                    
+                else
+                    sindex <- 0
+                    yield! candidate
+                    candidate.Clear()                
+            yield! candidate}
 
-        let drop i (source:seq<_>) =
-            let mutable count = i
-            use e = source.GetEnumerator()
-            while (count > 0 && e.MoveNext()) do count <- count-1
-            seq {while (e.MoveNext()) do yield e.Current}
+    let drop i (source:seq<_>) =
+        let mutable count = i
+        use e = source.GetEnumerator()
+        while (count > 0 && e.MoveNext()) do count <- count-1
+        seq {while (e.MoveNext()) do yield e.Current}
 
-        let replicate count initial = System.Linq.Enumerable.Repeat(initial, count) 
+    let replicate count initial = System.Linq.Enumerable.Repeat(initial, count) 
 
 
 /// Module containing F#+ Extension Methods  
