ef031101992842b6456ee8f5684bcd53e06639e4
Roberto Vespa <roby@robychechi.it>
2017-10-20 15:07:33 +0200

    Fixing (hopefully!) Bind signature

--- docsrc/content/abstraction-monad.fsx
+++ docsrc/content/abstraction-monad.fsx
@@ -22,7 +22,7 @@ Minimal comple te definition
 *)
 (**
     static member Return (x:'T) : 'Applicative<'T>
-    static member Bind (f:'T->'U, x:Applicative<'T>) : Applicative<'U>
+    static member (>>=) (x:Monad<'T>, f:'T->Monad<'U>) : Monad<'U>
 *)
 (**
 
