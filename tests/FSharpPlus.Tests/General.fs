5ebd8af8adad3062935126fe5bbc5b7dbeebcca0
Gusty <1261319+gusty@users.noreply.github.com>
2017-12-25 18:42:51 +0000

    + Dedicated file for CE tests

--- tests/FSharpPlus.Tests/General.fs
+++ tests/FSharpPlus.Tests/General.fs
@@ -1,8 +1,7 @@
-﻿module FSharpPlus.Tests
+﻿namespace FSharpPlus.Tests
 
 open System
-open FSharpPlus.Operators
-open FSharpPlus.Builders
+open FSharpPlus
 open FSharpPlus.Data
 open NUnit.Framework
 
@@ -320,51 +319,6 @@ type MonadPlus() =
         let y = getEmpty() <|> v
         Assert.AreEqual (v, x)
         Assert.AreEqual (v, y)
-
-[<TestFixture>]
-type Workflows() = 
-
-    [<Test>]
-    member x.MonadFx() = 
-        let effects = ResizeArray()
-        let zero = monad {
-            effects.Add(1)
-            do! Async.Sleep 10
-            effects.Add(2) }        
-        Assert.AreEqual(effects |> toList, [])
-
-        let combine = monad { 
-            if true then do! Async.Sleep 10
-            return! zero }
-        Assert.AreEqual(effects |> toList, [])
-        Async.RunSynchronously combine
-        Assert.AreEqual(effects |> toList, [1;2])
-
-    member x.MonadPlus() = 
-        let effects = ResizeArray()
-        let lst: _ list = monad.plus {
-            effects.Add(3)
-            return 5;
-            return 6; }
-        Assert.AreEqual(effects |> toList, [1;2;3])
-        Assert.AreEqual(lst, [5;6])
-
-        let effects = ResizeArray()
-        let seq3: seq<_> = monad.plus { 
-            effects.Add "Start"
-            try
-                try
-                    10 / 0 |> ignore
-                finally
-                    effects.Add "execute this"
-            with
-            | e -> 
-                effects.Add (sprintf "Exception! %s" e.Message)
-                return 42 }
-        Assert.AreEqual(effects |> toList, [])
-        let seqValue = seq3 |> Seq.toList
-        Assert.AreEqual(effects |> toList, ["Start"; "execute this"; "Exception! Attempted to divide by zero."])
-        Assert.AreEqual(seqValue, [42])
 
 
 module NumericLiteralG =
