f7fb820f5bc16c0cfa4300d4106d357567f46a0c
gusty <1261319+gusty@users.noreply.github.com>
2018-01-06 19:46:17 +0000

    Encode PlantUML automatically in doc generation

--- docsrc/tools/generate.fsx
+++ docsrc/tools/generate.fsx
@@ -39,6 +39,7 @@ open System.IO
 open Fake.FileHelper
 open FSharp.Literate
 open FSharp.MetadataFormat
+open FSharp.Markdown
 open FSharpPlus
 
 // When called from 'build.fsx', use the public project URL as <root>
@@ -121,6 +122,55 @@ let buildReference () =
     |> intercalate [|System.String.Empty|]
   File.WriteAllLines(pathIndex, ndx)
 
+
+// PlantUml processing
+open System.IO.Compression
+open System.Text
+
+
+let toUrl input =
+    let encodeByte b =
+        if   b < 10uy then 48uy + b
+        elif b < 36uy then 55uy + b
+        elif b < 62uy then 61uy + b
+        elif b = 62uy then byte '-'
+        elif b = 63uy then byte '_'
+        else               byte '?'
+
+    let encode3Bytes b1 b2 b3 =
+        let c1 =  b1 >>> 2
+        let c2 = (b2 >>> 4) ||| (b1 &&& 0x3uy <<< 4)
+        let c3 = (b3 >>> 6) ||| (b2 &&& 0xFuy <<< 2)
+        let c4 =                 b3 &&& 0x3Fuy
+        [|
+            encodeByte (c1 &&& 0x3Fuy)
+            encodeByte (c2 &&& 0x3Fuy)
+            encodeByte (c3 &&& 0x3Fuy)
+            encodeByte (c4 &&& 0x3Fuy)
+        |] |> Encoding.ASCII.GetChars
+
+    let encode bytes =
+        let c = Array.length bytes
+        let s = StringBuilder ()
+        for i in 0..3..c-1 do
+            let b1 =                   bytes.[i]
+            let b2 = if c > i + 1 then bytes.[i + 1] else 0uy
+            let b3 = if c > i + 2 then bytes.[i + 2] else 0uy
+            encode3Bytes b1 b2 b3 |> s.Append |> ignore
+        string s
+
+    use output = new MemoryStream ()
+    let writeFrom (input:string) output =
+        use writer = new StreamWriter(new DeflateStream(output, CompressionLevel.Optimal), Encoding.UTF8)
+        writer.Write input
+    output |> writeFrom input |> ignore
+    output.ToArray () |> encode
+
+let abstractions = content </> "abstractions.plantuml"
+let plantUMLDiag = toUrl (File.ReadAllText abstractions)
+let customize _ (doc:LiterateDocument) = doc.With (paragraphs = (doc.Paragraphs |>> function InlineBlock x -> InlineBlock (replace "{plantUMLDiag}" plantUMLDiag x) | x -> x))
+
+
 // Build documentation from `fsx` and `md` files in `docs/content`
 let buildDocumentation () =
 
@@ -130,7 +180,9 @@ let buildDocumentation () =
     ( content, docTemplate, output, replacements = ("root", root)::info,
       layoutRoots = layoutRootsAll.["en"],
       generateAnchors = true,
-      processRecursive = false)
+      processRecursive = false,
+      customizeDocument = customize
+      )
 
   // And then process files which are placed in the sub directories
   // (some sub directories might be for specific language).
