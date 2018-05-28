4b0e82769e51c26be4bfc05e3bfa15b1ee803590
Oskar Gewalli <oskar@gewalli.se>
2018-05-28 21:19:29 +0200

    DList, NonEmptyList implements ReadOnly interfaces

--- src/FSharpPlus/NonEmptyList.fs
+++ src/FSharpPlus/NonEmptyList.fs
@@ -3,13 +3,16 @@
 open System.Text
 open System.Runtime.InteropServices
 open System.ComponentModel
+open System.Collections.Generic
 open FSharpPlus.Control
 open FSharpPlus
 
 /// A type-safe list that contains at least one element.
 type NonEmptyList<'t> = {Head: 't; Tail: 't list} with
-    interface System.Collections.Generic.IEnumerable<'t> with member x.GetEnumerator() = (let {Head = x; Tail = xs} = x in seq (x::xs)).GetEnumerator()
+    interface IEnumerable<'t> with member x.GetEnumerator() = (let {Head = x; Tail = xs} = x in seq (x::xs)).GetEnumerator()
     interface System.Collections.IEnumerable             with member x.GetEnumerator() = (let {Head = x; Tail = xs} = x in seq (x::xs)).GetEnumerator() :> System.Collections.IEnumerator
+    interface IReadOnlyCollection<'t>                    with member s.Count = 1 + List.length s.Tail
+    interface IReadOnlyList<'t>                          with member s.Item with get(index) = s.Item index
     member this.head = let {Head = a; Tail = _} = this in a
     member this.tail = let           {Tail = a} = this in a
     member this.Item = function 0 -> this.head | n -> this.tail.[n-1]
