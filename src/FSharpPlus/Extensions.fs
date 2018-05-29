d231618b988ff993d73863511ca4a4f459d19978
gusty <1261319+gusty@users.noreply.github.com>
2018-05-29 18:32:41 +0100

    + Some IReadOnlyCollection functions

--- src/FSharpPlus/Extensions.fs
+++ src/FSharpPlus/Extensions.fs
@@ -212,6 +212,16 @@ module String =
             |> normalize NormalizationForm.FormC
 
 
+/// Additional operations on IReadOnlyCollection<'T>
+[<RequireQualifiedAccess>]
+module IReadOnlyCollection =
+    open System.Collections.Generic
+
+    let ofArray (source: 'T[]   ) = source                :> IReadOnlyCollection<'T>
+    let ofList  (source: 'T list) = source                :> IReadOnlyCollection<'T>
+    let ofSeq   (source: seq<'T>) = source |> Array.ofSeq :> IReadOnlyCollection<'T>
+    let map mapping (source: IReadOnlyCollection<'T>) = Seq.map mapping source |> Seq.toArray :> IReadOnlyCollection<'U>
+
 
 /// Additional operations on Map<'Key, 'Value>
 [<RequireQualifiedAccess>]
