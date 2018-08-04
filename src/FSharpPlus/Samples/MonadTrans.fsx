a4ead80cabcb4c2e991210110e66a91c21bb07c3
gusty <1261319+gusty@users.noreply.github.com>
2018-08-04 19:42:34 +0100

    Include missing sample file / formatting

--- src/FSharpPlus/Samples/MonadTrans.fsx
+++ src/FSharpPlus/Samples/MonadTrans.fsx
@@ -12,9 +12,8 @@ let fn : ResultT<Reader<int,Result<_,string>>> =
     monad {
        let! x1 = lift ask
        let! x2 = 
-        if x1 > 0 then 
-            result 1
-            else ResultT (result (Error "Negative value")) 
+           if x1 > 0 then result 1
+           else ResultT (result (Error "Negative value")) 
        return x1 + x2
     }
 
@@ -29,25 +28,27 @@ module Suave=
     // in conjunction with generic Kleisli composition (fish) operator
     type WebPart<'a> = 'a -> OptionT<Async<'a option>>
     let inline succeed x = async.Return (Some x)
+
     module WebPart=
         /// Comment from <a href="https://github.com/SuaveIO/suave/blob/v2.4.3/src/Suave/WebPart.fsi#L39-L42">WebPart.fsi</a>
         /// Entry-point for composing the applicative routes of the http application,
         /// by iterating the options, applying the context, arg, to the predicate
         /// from the list of options, until there's a match/a Some(x) which can be
         /// run.
-        let choose (options : WebPart<'a> list) =fun x -> choice (List.map ( (|>) x) options)
+        let choose (options : WebPart<'a> list) = fun x -> choice (List.map ((|>) x) options)
 
     module Http=
         type HttpResponse = { status : int; content:string }
         type HttpRequest = { url : Uri; ``method``:string }
-        
         type HttpContext = { request:HttpRequest; response:HttpResponse }
+
     module Successful=
         open Http
         let private withStatusCode statusCode s=
             OptionT << fun ctx -> { ctx with response = { ctx.response with status = statusCode; content = s }} |> succeed 
         let OK s = withStatusCode 200 s
         let BAD_REQUEST s = withStatusCode 400 s
+
     module Filters=
         open Http
         let ``method`` (m : string) =
@@ -57,26 +58,29 @@ module Suave=
   
         let path s =
             OptionT << fun (x : HttpContext) -> async.Return (if (s = x.request.url.AbsolutePath) then Some x else None)
+
     // Stub implementations:
-    let toJson o :string= failwith "this would be toJson from for instance Fleece"
+    let toJson o : string = failwith "this would be toJson from for instance Fleece"
     let inline ofJson s = failwith "this would be ofJson from for instance Fleece"
-    module Request=
-        let tryGet s (r:Http.HttpRequest) = Ok "FORM VALUE"
-    let authenticated (f:Http.HttpContext -> int -> OptionT<Async<'a option>>) =
+
+    module Request =
+        let tryGet _s (_r: Http.HttpRequest) = Ok "FORM VALUE"
+
+    let authenticated (f: Http.HttpContext -> int -> OptionT<Async<'a option>>) =
         // we assume that authenticated executes f only if auth, otherwise returns 401
         // we fake it as:
-        fun (ctx:Http.HttpContext) -> f ctx -1
+        fun (ctx: Http.HttpContext) -> f ctx -1
 
     // Usage:
     open Successful
     open Filters
-    type Note = { id:int; text: string }
-    type NoteList = { notes: Note list; offset:int; chunk:int; total:int }
+    type Note = { id: int; text: string }
+    type NoteList = { notes: Note list; offset: int; chunk: int; total: int }
     type IDb =
         abstract member getUserNotes: int -> Async<NoteList>
         abstract member addUserNote: int -> string -> Async<Note>
     type OverviewViewModel = { myNotes: Note list }
-    let app (db:IDb) =
+    let app (db: IDb) =
         let overview =
             GET >=> (authenticated <| fun ctx userId ->
                 monad {
@@ -98,4 +102,3 @@ module Suave=
         WebPart.choose [ path "/" >=> (OK "/")
                          path "/note" >=> register
                          path "/notes" >=> overview ]
-
