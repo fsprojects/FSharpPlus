8f6218d3d8c4e55c009ee6627dd8c549bd01e15c
gusty <1261319+gusty@users.noreply.github.com>
2018-09-17 00:14:36 +0100

    Replace Const and Identity usage with ConstId

--- src/FSharpPlus/Lens.fs
+++ src/FSharpPlus/Lens.fs
@@ -18,6 +18,29 @@ module Lens =
         type Exchange<'T,'U> = Exchange of 'T * 'U with
             static member Dimap (Exchange (sa, bt), f, g) = Exchange (sa << f, g << bt)
 
+        type ConstIdType =
+            | TUndefined
+            | TIdentity
+            | TConst
+            with static member (&&&) (x, y) = 
+                    match x, y with
+                    | v         , v' when v = v'    -> v
+                    | TUndefined, v | v, TUndefined -> v
+                    | _         , _                 -> failwithf "Unexpected %A %A" x y
+
+        type ConstId<'monoid,'t> = ConstId of ('t * 'monoid * ConstIdType) with
+            static member inline Return x = ConstId (x, getZero (), TUndefined)   : ConstId<'Monoid,'T>
+            static member inline (<*>) (ConstId (f, wf, vf), ConstId (t, wx, vx)) : ConstId<'Monoid,'U> =
+                let v = vf &&& vx
+                ConstId ((if v = TConst then Unchecked.defaultof<_> else f t), (if v = TIdentity then Unchecked.defaultof<_> else plus wf wx), v)
+
+        module ConstId =
+            let map f (ConstId (t: 'T, w, v)) : ConstId<'Monoid,'U> = ConstId ((if v = TConst then Unchecked.defaultof<_> else f t), w, v)
+            let asId x = ConstId (x, (), TIdentity) : ConstId<unit, 't>
+            let runId (ConstId (t, _, v) : ConstId<'Monoid,'T>) = if v = TConst then failwith "Identity expected, but got Const" else t
+            let asConst x = ConstId (Unchecked.defaultof<_>, x, TConst)
+            let runConst (ConstId (_, w, v)) : 'T = if v = TIdentity then failwith "Const expected, but got Identity" else w
+
     open Internals
     open FSharpPlus.Control
 
@@ -28,26 +51,26 @@ module Lens =
     /// <param name="value">The value we want to write in the part targeted by the lens.</param>
     /// <param name="source">The original object.</param>
     /// <returns>The new object with the value modified.</returns>
-    let setl optic value source = Identity.run (optic (fun _ -> Identity value) source)
+    let setl optic value source = ConstId.runId (optic (fun _ -> ConstId.asId value) source)
 
     /// <summary>Update a value in a lens.</summary>
     /// <param name="optic">The lens.</param>
     /// <param name="updater">A function that converts the value we want to write in the part targeted by the lens.</param>
     /// <param name="source">The original object.</param>
     /// <returns>The new object with the value modified.</returns>
-    let over optic updater source = Identity.run (optic (Identity << updater) source)
+    let over optic updater source = ConstId.runId (optic (ConstId.asId << updater) source)
 
     /// <summary>Read from a lens.</summary>
     /// <param name="optic">The lens.</param>
     /// <param name="source">The object.</param>
     /// <returns>The part the lens is targeting.</returns>
-    let view optic source = Const.run (optic Const source)
+    let view optic source = ConstId.runConst (optic ConstId.asConst source)
 
     /// <summary>Retrieve the first value targeted by a Prism, Fold or Traversal (or Some result from a Getter or Lens). See also (^?).</summary>
     /// <param name="optic">The prism.</param>
     /// <param name="source">The object.</param>
     /// <returns>The value (if any) the prism is targeting.</returns>
-    let preview optic source = source |> optic (fun x -> Const (FSharpPlus.Data.First (Some x))) |> Const.run |> First.run
+    let preview optic source = source |> optic (fun x -> ConstId.asConst (FSharpPlus.Data.First (Some x))) |> ConstId.runConst |> First.run
 
     /// <summary>Build a 'Lens' from a getter and a setter.</summary>
     /// <remarks>The lens should be assigned as an inline function of the free parameter, not a value, otherwise compiler will fail with a type constraint mismatch.</remarks>
@@ -55,7 +78,7 @@ module Lens =
     /// <param name="setter">The setter function, having as first parameter the object and second the value to set.</param>
     /// <param name="f">The free parameter.</param>
     /// <returns>The lens.</returns>
-    let inline lens getter setter f = fun s -> setter s </Map.InvokeOnInstance/> f (getter s)
+    let lens getter setter f = fun s -> setter s </ConstId.map/> f (getter s)
 
 
     /// <summary>Build a 'Prism' from a constructor and a getter.</summary>
@@ -65,7 +88,7 @@ module Lens =
     /// <param name="getter">The getter function, having as first parameter the object and second the value to set.</param>
     /// <param name="f">The free parameter.</param>
     /// <returns>The prism.</returns>
-    let inline prism (constructor: 'b -> 't) (getter: 's -> Result<'a,'t>) f = f |> (fun g -> either (Ok << g) Error) |> dimap' getter (either (Map.InvokeOnInstance constructor) Return.InvokeOnInstance)
+    let inline prism (constructor: 'b -> 't) (getter: 's -> Result<'a,'t>) f = f |> (fun g -> either (Ok << g) Error) |> dimap' getter (either (ConstId.map constructor) ConstId.Return)
 
     /// <summary>Build a 'Prism' from a constructor and a getter.</summary>
     /// <remarks>The prism should be assigned as an inline function of the free parameter, not a value, otherwise compiler will fail with a type constraint mismatch.</remarks>
@@ -80,33 +103,33 @@ module Lens =
     /// <param name="func">The transform function.</param>
     /// <param name="inv">The inverse of the transform function.</param>
     /// <returns>The iso.</returns>
-    let inline iso (func: 's -> 'a) (inv: 'b -> 't) = dimap func (Map.InvokeOnInstance inv)
+    let inline iso (func: 's -> 'a) (inv: 'b -> 't) = dimap func (ConstId.map inv)
 
     /// Merge two lenses, getters, setters, folds or traversals.
     /// <param name="optic1">The first optic.</param>
     /// <param name="optic2">The second optic.</param>
     /// <param name="f">The free parameter.</param>
     /// <returns>An optic for a Result which uses the first optic for the Ok and the second for the Error.</returns>
-    let inline choosing optic1 optic2 f = function
-        | Error x -> Error </Map.InvokeOnInstance/> optic1 f x
-        | Ok    x -> Ok    </Map.InvokeOnInstance/> optic2 f x
+    let choosing optic1 optic2 f = function
+        | Error x -> Error </ConstId.map/> optic1 f x
+        | Ok    x -> Ok    </ConstId.map/> optic2 f x
 
     // Some common Lens
 
     /// Lens for the first element of a tuple
-    let inline _1 f t = Map.InvokeOnInstance (fun x -> mapItem1 (fun _ -> x) t) (f (item1 t))
+    let inline _1 f t = ConstId.map (fun x -> mapItem1 (fun _ -> x) t) (f (item1 t))
 
     /// Lens for the second element of a tuple
-    let inline _2 f t = Map.InvokeOnInstance (fun x -> mapItem2 (fun _ -> x) t) (f (item2 t))
+    let inline _2 f t = ConstId.map (fun x -> mapItem2 (fun _ -> x) t) (f (item2 t))
 
     /// Lens for the third element of a tuple
-    let inline _3 f t = Map.InvokeOnInstance (fun x -> mapItem3 (fun _ -> x) t) (f (item3 t))
+    let inline _3 f t = ConstId.map (fun x -> mapItem3 (fun _ -> x) t) (f (item3 t))
 
     /// Lens for the fourth element of a tuple
-    let inline _4 f t = Map.InvokeOnInstance (fun x -> mapItem4 (fun _ -> x) t) (f (item4 t))
+    let inline _4 f t = ConstId.map (fun x -> mapItem4 (fun _ -> x) t) (f (item4 t))
 
     /// Lens for the fifth element of a tuple
-    let inline _5 f t = Map.InvokeOnInstance (fun x -> mapItem5 (fun _ -> x) t) (f (item5 t))
+    let inline _5 f t = ConstId.map (fun x -> mapItem5 (fun _ -> x) t) (f (item5 t))
 
     // Prism
     let inline _Ok    x = (prism Ok    <| either Ok (Error << Error)) x
@@ -116,14 +139,14 @@ module Lens =
 
     // Traversal
     let inline _all ref f s =
-        let update old = if old = ref then f old else Return.InvokeOnInstance old
+        let update old = if old = ref then f old else ConstId.Return old
         traverse update s
 
     // functions
     let inline to' k = dimap k (Contramap.InvokeOnInstance k)
 
-    let foldMapOf l f = Const.run </rmap'/> l (Const </rmap'/> f)
-    let foldOf    l   = Const.run </rmap'/> l Const
+    let foldMapOf l f = ConstId.runConst </rmap'/> l (ConstId.asConst </rmap'/> f)
+    let foldOf    l   = ConstId.runConst </rmap'/> l ConstId.asConst
     let foldrOf l f z = flip Endo.run z << foldMapOf l (Endo </rmap'/> f)
     let foldlOf l f z = (flip Endo.run z </lmap'/> Dual.run) </rmap'/> foldMapOf l (Dual </rmap'/> Endo </rmap'/> flip f)
 
@@ -135,10 +158,10 @@ module Lens =
     let elemOf l = anyOf l << (=)
     let inline items x = traverse x
 
-    let inline filtered p f s = if p s then f s else Return.InvokeOnInstance s
-    let inline both f (a, b) = tuple2 </Map.InvokeOnInstance/> f a </Apply.InvokeOnInstance/> f b
+    let inline filtered p f s = if p s then f s else ConstId.Return s
+    let inline both f (a, b) = tuple2 </ConstId.map/> f a </curry ConstId.(<*>)/> f b
 
-    let inline withIso ai k = let (Exchange (sa, bt)) = ai (Exchange (id, Identity)) in k sa (Identity.run </rmap'/> bt)
+    let inline withIso ai k = let (Exchange (sa, bt)) = ai (Exchange (id, ConstId.asId)) in k sa (ConstId.runId </rmap'/> bt)
     let inline from' l   = withIso l <| fun sa bt -> iso bt sa
     let inline mapping k = withIso k <| fun sa bt -> iso (Map.InvokeOnInstance sa) (Map.InvokeOnInstance bt)
 
@@ -175,4 +198,4 @@ module Lens =
     /// <param name="x">The functor.</param>
     /// <param name="f">The mapper function.</param>
     /// <returns>The mapped Functor.</returns>
-    let inline (<&>) (x: '``Functor<'T>``) (f: 'T->'U) : '``Functor<'U>`` = Map.InvokeOnInstance f x
+    let (<&>) x (f: 'T->'U) = ConstId.map f x
