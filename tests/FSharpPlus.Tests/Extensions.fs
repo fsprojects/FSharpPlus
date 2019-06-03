db9b04f17f8768af20b1936b5329fecb9f6ed2dd
Paulo Nobre <pnobre@pnobre.com>
2019-06-03 16:44:13 +0100

    fixed typo and created few extra tests

--- tests/FSharpPlus.Tests/Extensions.fs
+++ tests/FSharpPlus.Tests/Extensions.fs
@@ -33,15 +33,26 @@ let ``Map.intersect returns any dictionary when intersected with the empty map (
   areEqual Map.empty r1
 
 [<Test>]
-let ``Map.intersect returns same results independent of the order (associative)`` () = 
+let ``Map.intersect returns same results independent of the order (commutative)`` () = 
   let m1 = [1, "1"; 2,"2"; 3,"3"] |> Map.ofList
-  let m2 = [3, "3"; 4,"4"; 5,"6"] |> Map.ofList
+  let m2 = [3, "3"; 4,"4"; 5,"5"] |> Map.ofList
 
   let r1 = m1 |> Map.intersect m2
   let r2 = m2 |> Map.intersect m1
 
   areEqual r1 r2
 
+[<Test>]
+let ``Map.intersect returns same results independent of grouping (associative)`` () = 
+  let m1 = [1, "1"; 2,"2"; 3,"3"] |> Map.ofList
+  let m2 = [3, "3"; 4,"4"; 5,"5"] |> Map.ofList
+  let m3 = [5, "5"; 6,"6"; 7,"7"] |> Map.ofList
+
+  let r1 = Map.intersect (Map.intersect m1 m2) m3
+  let r2 =  Map.intersect m1 (Map.intersect m2 m3)
+
+  areEqual r1 r2
+  
 [<Test>]
 let ``Map.intersect provides same end result as Map.intersectWith picking the first source value for dupes`` () =
   let m1 = [1, "2"; 2,"4"; 4,"8"] |> Map.ofList
@@ -62,12 +73,23 @@ let ``Dict.intersect returns any dictionary when intersected with the empty dict
   areEqual [] r1
 
 [<Test>]
-let ``Dict.intersect returns same results independent of the order (associative)`` () = 
+let ``Dict.intersect returns same results independent of the order (commutative)`` () = 
   let m1 = dict [1, "1"; 2,"2"; 3,"3"]
-  let m2 = dict [3, "3"; 4,"4"; 5,"6"]
+  let m2 = dict [3, "3"; 4,"4"; 5,"5"]
 
   let r1 = m1 |> Dict.intersect m2 |> Seq.toList
   let r2 = m2 |> Dict.intersect m1 |> Seq.toList
+
+  areEqual r1 r2
+
+[<Test>]
+let ``Dict.intersect returns same results independent of the grouping (associative)`` () = 
+  let m1 = dict [1, "1"; 2,"2"; 3,"3"]
+  let m2 = dict [3, "3"; 4,"4"; 5,"5"]
+  let m3 = dict [5, "5"; 6,"6"; 7,"7"]
+
+  let r1 = (Dict.intersect (Dict.intersect m1 m2) m3) |> Seq.toList
+  let r2 = (Dict.intersect m1 (Dict.intersect m2 m3)) |> Seq.toList
 
   areEqual r1 r2
 
@@ -92,15 +114,27 @@ let ``IReadOnlyDictionary.intersect returns any dictionary when intersected with
   areEqual [] r1
 
 [<Test>]
-let ``IReadOnlyDictionary.intersect returns same results independent of the order (associative)`` () = 
+let ``IReadOnlyDictionary.intersect returns same results independent of the order (commutative)`` () = 
   let m1 = Dictionary(dict [1, "1"; 2,"2"; 3,"3"]) :> IReadOnlyDictionary<int, string>
-  let m2 = Dictionary(dict [3, "3"; 4,"4"; 5,"6"]) :> IReadOnlyDictionary<int, string>
+  let m2 = Dictionary(dict [3, "3"; 4,"4"; 5,"5"]) :> IReadOnlyDictionary<int, string>
 
   let r1 = m1 |> IReadOnlyDictionary.intersect m2 |> Seq.toList
   let r2 = m2 |> IReadOnlyDictionary.intersect m1 |> Seq.toList
 
   areEqual r1 r2
 
+
+[<Test>]
+let ``IReadOnlyDictionary.intersect returns same results independent of the grouping (distributive)`` () = 
+  let m1 = Dictionary(dict [1, "1"; 2,"2"; 3,"3"]) :> IReadOnlyDictionary<int, string>
+  let m2 = Dictionary(dict [3, "3"; 4,"4"; 5,"5"]) :> IReadOnlyDictionary<int, string>
+  let m3 = Dictionary(dict [5, "5"; 6,"6"; 7,"7"]) :> IReadOnlyDictionary<int, string>
+
+  let r1 = (IReadOnlyDictionary.intersect (IReadOnlyDictionary.intersect m1 m2) m3) |> Seq.toList
+  let r2 = (IReadOnlyDictionary.intersect m1 (IReadOnlyDictionary.intersect m2 m3)) |> Seq.toList
+
+  areEqual r1 r2
+  
 [<Test>]
 let ``IReadOnlyDictionary.intersect provides same end result as IReadOnlyDictionary.intersectWith picking the first source value for dupes`` () =
   let m1 = Dictionary(dict [1, "2"; 2,"4"; 4,"8"]) :> IReadOnlyDictionary<int, string>
