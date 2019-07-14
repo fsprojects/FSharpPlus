77ba7cc1e4e7799ca79a88aaf3c2fb234255aa62
Gustavo Leon <1261319+gusty@users.noreply.github.com>
2019-07-14 11:19:08 +0200

    Link from docs index

--- docsrc/content/index.fsx
+++ docsrc/content/index.fsx
@@ -81,7 +81,7 @@ Contributing and copyright
 The project is hosted on [GitHub][gh] where you can [report issues][issues], fork 
 the project and submit pull requests. If you're adding a new public API, please also 
 consider adding [samples][content] that can be turned into a documentation. You might
-also want to read the [library design notes][readme] to understand how it works.
+also want to read the [library design notes][design] to understand how it works.
 
 The library is available under Public Domain license, which allows modification and 
 redistribution for both commercial and non-commercial purposes. For more information see the 
@@ -92,4 +92,5 @@ redistribution for both commercial and non-commercial purposes. For more informa
   [issues]: https://github.com/fsprojects/FSharpPlus/issues
   [readme]: https://github.com/fsprojects/FSharpPlus/blob/master/README.md
   [license]: https://github.com/fsprojects/FSharpPlus/blob/master/LICENSE.txt
+  [design]: https://github.com/fsprojects/FSharpPlus/blob/master/DESIGN_GUIDELINES.md
 *)
