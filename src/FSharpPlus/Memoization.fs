0db33fc47ae0fb0e9839718adfea051f4c05eda1
Gustavo Leon <1261319+gusty@users.noreply.github.com>
2019-08-02 15:17:48 +0200

    Align memoizeN code with F#+ (#172)

--- src/FSharpPlus/Memoization.fs
+++ src/FSharpPlus/Memoization.fs
@@ -1,26 +1,26 @@
 ﻿namespace FSharpPlus
 
+open FSharpPlus.Internals
 
 [<AutoOpen>]
 module Memoization =
 
     // From https://gist.github.com/gusty/70e5af737f2f6aed2bc0303a2e17c7d7
-
     open System.Collections.Concurrent
 
     // Key wrapper to allow null values, since dict keys can't be null
     [<Struct>]
     type MemoizationKeyWrapper<'a> = MemoizationKeyWrapper of 'a
 
-    type MemoizeConcurrentHelper = MemoizeConcurrentHelper with
-        static member getOrAdd (cd:ConcurrentDictionary<MemoizationKeyWrapper<'a>,'b>) (f:'a -> 'b) k =
+    type MemoizeN =
+        inherit Default1
+        static member getOrAdd (cd: ConcurrentDictionary<MemoizationKeyWrapper<'a>,'b>) (f: 'a -> 'b) k =
             cd.GetOrAdd (MemoizationKeyWrapper k, (fun (MemoizationKeyWrapper x) -> x) >> f)
 
     let inline memoizeN (f:'``(T1 -> T2 -> ... -> Tn)``): '``(T1 -> T2 -> ... -> Tn)`` =
-        (MemoizeConcurrentHelper $ Unchecked.defaultof<'``(T1 -> T2 -> ... -> Tn)``>) f
+        let inline call_2 (a: ^MemoizeN, b: ^b) = ((^MemoizeN or ^b) : (static member MemoizeN : ^MemoizeN * 'b -> _ ) (a, b))
+        call_2 (Unchecked.defaultof<MemoizeN>, Unchecked.defaultof<'``(T1 -> T2 -> ... -> Tn)``>) f
 
-    type MemoizeConcurrentHelper with
-        static member ($) (_:obj,  _: 'a -> 'b) =
-            MemoizeConcurrentHelper.getOrAdd (ConcurrentDictionary ())
-        static member inline ($) (MemoizeConcurrentHelper, _:'t -> 'a -> 'b) =
-            MemoizeConcurrentHelper.getOrAdd (ConcurrentDictionary ()) << (<<) memoizeN
+    type MemoizeN with
+        static member        MemoizeN (_: Default1, _:      'a -> 'b) = MemoizeN.getOrAdd (ConcurrentDictionary ())
+        static member inline MemoizeN (_: MemoizeN, _:'t -> 'a -> 'b) = MemoizeN.getOrAdd (ConcurrentDictionary ()) << (<<) memoizeN
