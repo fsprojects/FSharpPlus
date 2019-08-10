f4561d856456ee641f31e1881ed7d4a148106886
Gustavo Leon <1261319+gusty@users.noreply.github.com>
2019-08-10 09:13:44 +0200

    Add a failing test

--- tests/FSharpPlus.Tests/General.fs
+++ tests/FSharpPlus.Tests/General.fs
@@ -948,6 +948,15 @@ module Traversable =
         let resNone   = traverse (fun x -> if x > 4 then Some x else None) (Seq.initInfinite id) // optimized method, otherwise it doesn't end
         ()
 
+    [<Test>]
+    let traverseInfiniteAsyncSequences =
+        let s = Seq.initInfinite async.Return
+        let s' = sequence s
+        let l = s' |> Async.RunSynchronously |> Seq.take 10 |> Seq.toList
+        Assert.AreEqual([0;1;2;3;4;5;6;7;8;9], l)
+        ()
+
+    [<Test>]
     let traverseTask () =
         let a = traverse Task.FromResult [1;2]
         Assert.AreEqual([1;2], a.RunSynchronously())
