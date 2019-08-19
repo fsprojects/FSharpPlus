fc3b76daad874f4553a0f37d36f4c879379f6164
Gustavo Leon <1261319+gusty@users.noreply.github.com>
2019-08-19 08:00:28 +0200

    This should work but ...
    Now the ambiguity problem is gone, but we run into unsolved type variables.
    
    Using the specific bind for Free instead of the generic one, should work fine. But that doesn't seem to be a good compromise.

--- tests/FSharpPlus.Tests/General.fs
+++ tests/FSharpPlus.Tests/General.fs
@@ -408,7 +408,9 @@ module Free =
     // primitive functor types
     let aFreeOfListInt = Roll [Roll [Roll [Pure 2]]]
     let aFreeOfListFloat = aFreeOfListInt >>= (fun x -> Roll [ Pure "99" ]) >>= (fun x -> Roll [ Pure 90.4 ])
-    let aFreeOfIdentityInt = Roll (Identity (Pure 1)) >>= fun x -> Roll (Identity (Pure 42))
+    
+    // user defined functor types
+    let aFreeOfIdentityInt = Roll (Identity (Pure 1)) |> Free.bind (fun x -> Roll (Identity (Pure 42)))
     
     
 module Collections =
