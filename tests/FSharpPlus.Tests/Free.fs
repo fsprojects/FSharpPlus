9d57c54f0b21904dceb924bcef2cf3bced25622d
Gustavo Leon <1261319+gusty@users.noreply.github.com>
2019-08-20 23:33:07 +0200

    Move tests to own file

---
+++ tests/FSharpPlus.Tests/Free.fs
@@ -0,0 +1,21 @@
+﻿namespace FSharpPlus.Tests
+
+open System
+open FSharpPlus
+open FSharpPlus.Builders
+open FSharpPlus.Data
+open NUnit.Framework
+
+open Helpers
+open SideEffects
+
+module Free =
+    // compile tests only for now
+    
+    // primitive functor types
+    let aFreeOfListInt = Roll [Roll [Roll [Pure 2]]]
+    let aFreeOfListFloat = aFreeOfListInt >>= (fun x -> Roll [ Pure "99" ]) >>= (fun x -> Roll [ Pure 90.4 ])
+    
+    // user defined functor types
+    let aFreeOfIdentityInt = Roll (Identity (Pure 1)) |> Free.bind (fun x -> Roll (Identity (Pure 42)))
+
--- tests/FSharpPlus.Tests/General.fs
+++ tests/FSharpPlus.Tests/General.fs
@@ -402,17 +402,6 @@ module Functor =
         ()
 
 
-module Free =
-    // compile tests only for now
-    
-    // primitive functor types
-    let aFreeOfListInt = Roll [Roll [Roll [Pure 2]]]
-    let aFreeOfListFloat = aFreeOfListInt >>= (fun x -> Roll [ Pure "99" ]) >>= (fun x -> Roll [ Pure 90.4 ])
-    
-    // user defined functor types
-    let aFreeOfIdentityInt = Roll (Identity (Pure 1)) |> Free.bind (fun x -> Roll (Identity (Pure 42)))
-    
-    
 module Collections =
 
     open System.Collections
