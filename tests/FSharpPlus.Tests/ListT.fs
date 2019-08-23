032f0e512ffab3902c22b57c5223f55f9e59ca9d
Gustavo Leon <1261319+gusty@users.noreply.github.com>
2019-08-23 15:11:45 +0200

    + Some basic tests

---
+++ tests/FSharpPlus.Tests/ListT.fs
@@ -0,0 +1,24 @@
+module FSharpPlus.Tests.ListT
+
+open System
+open FSharpPlus
+open FSharpPlus.Data
+open NUnit.Framework
+open FsCheck
+open Helpers
+open System.Collections.Generic
+
+module BasicTests =
+    [<Test>]
+    let wrap_unwrap () =
+        let c = listT (lazy (['a'..'g']))
+        let res = c |> ListT.run |> listT |> ListT.run |> extract
+        let exp = c |> ListT.run |> extract
+        CollectionAssert.AreEqual (res, exp)
+  
+    [<Test>]
+    let infiniteLists =
+        let (infinite: ListT<Lazy<_>>) = ListT.unfold (fun x -> monad { return (Some (x, x + 1) ) }) 0
+        let finite = take 12 infinite
+        let res = finite <|> infinite
+        CollectionAssert.AreEqual (res |> take 13 |> ListT.run |> extract, [0;1;2;3;4;5;6;7;8;9;10;11;0])
