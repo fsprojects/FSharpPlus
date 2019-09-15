fe611580d6f0625e3fb59638f7460ff51507f0b7
JB <jbotto94@users.noreply.github.com>
2019-09-15 19:36:46 +0100

    Uploading JS test framework

---
+++ tests/FSharpPlusFable.Tests/Tests.fs
@@ -0,0 +1,27 @@
+module Tests
+
+open ExtensionsTests
+open Testing
+
+
+let AllTests =  testList "AllTests" [ExtensionsTest]
+
+#if FABLE_COMPILER
+
+open Fable.Core
+open Fable.Core.JsInterop
+
+flattenTest AllTests
+
+#else
+
+open Expecto
+open Expecto.TestResults
+
+[<EntryPoint>]
+    let main args =
+        printfn "Result: %i" (runTestsWithArgs defaultConfig args AllTests)
+        0
+
+
+#endif
---
+++ tests/FSharpPlusFable.Tests/build.sh
@@ -0,0 +1,1 @@
+dotnet fsi build.fsx $@
