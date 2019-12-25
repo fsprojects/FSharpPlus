b999a7ab126f99e252888f332cc1ac42676bd742
Gustavo Leon <1261319+gusty@users.noreply.github.com>
2019-12-25 21:25:06 +0100

    Better applicative

--- src/FSharpPlus/Free.fs
+++ src/FSharpPlus/Free.fs
@@ -38,6 +38,13 @@ module FreeInternals =
             | _      -> let x = _unroll x in _roll (Map.InvokeOnInstance (loop f) x) : Free<'``Functor<'U>``,'U>
         loop f x
 
+    let inline applyWithMapOnInstance (f: Free<'``Functor<'T->'U>``,'T->'U>) (x: Free<'``Functor<'T>``,'T>) : Free<'``Functor<'U>``,'U> =
+        let rec loop (f: Free<_,_>) (x: Free<_,_>) =
+            match f.getFree () with
+            | Pure f -> mapWithMapOnInstance f x
+            | _      -> let f = _unroll f in _roll (flip loop x </Map.InvokeOnInstance/> f)
+        loop f x
+
     let inline bindWithMapOnInstance (f: 'T -> Free<'``Functor<'U>``,'U>) (x: Free<'``Functor<'T>``,'T>) : Free<'``Functor<'U>``,'U> =
         let rec loop f (x: Free<_,_>) =
             match x.getFree () with
@@ -83,6 +90,13 @@ module Free =
             | _ -> let x = unroll x in Roll (Map.Invoke(loop f) x) : Free<'``Functor<'U>``,'U>
         loop f x
 
+    let inline apply (f: Free<'``Functor<'T->'U>``,'T->'U>) (x: Free<'``Functor<'T>``,'T>) : Free<'``Functor<'U>``,'U> =
+        let rec loop (f: Free<_,_>) (x: Free<_,_>)  =
+            match f.getFree () with
+            | FreeNode.Pure f -> map f x
+            | _  -> let f = unroll f in Roll (flip loop x </Map.Invoke/> f)
+        loop f x
+        
     /// Folds the Free structure into a Monad
     let inline fold (f: '``Functor<'T>`` -> '``Monad<'T>``) (x: Free<'``Functor<'T>``,'T>) : '``Monad<'T>`` =
         let rec loop f = function
@@ -97,11 +111,11 @@ module Free =
 type FreeBase<'FT,'T> with
     static member inline Map   (x: FreeBase<'``Functor<'T>``,'T>, f: 'T -> 'U) = Free.map f (x :?> Free<'``Functor<'T>``,'T>) : Free<'``Functor<'U>``,'U>
     static member inline (>>=) (x: FreeBase<'``Functor<'T>``,'T>, f: 'T -> Free<'``Functor<'U>``,'U>) = Free.bind f (x :?> Free<'``Functor<'T>``,'T>) : Free<'``Functor<'U>``,'U>
-    static member inline (<*>) (f: Free<_,_>, x: FreeBase<_,_>) = Free.bind (fun (x1: 'T->'U) -> Free.bind (fun x2 -> Pure (x1 x2)) (x :?> Free<'``Functor<'T>``,'T>)) f
+    static member inline (<*>) (f: Free<'``Functor<'T->'U>``,'T->'U>, x: FreeBase<'``Functor<'T>``,'T>) = Free.apply f (x :?> Free<'``Functor<'T>``,'T>) : Free<'``Functor<'U>``,'U>
 
 
 type Free<'FT,'T> with
     static member Return x = Pure x
     static member inline Map   (x: Free<'``Functor<'T>``,'T>, f: 'T -> 'U) = mapWithMapOnInstance f x : Free<'``Functor<'U>``,'U>
     static member inline (>>=) (x: Free<'``Functor<'T>``,'T>, f: 'T -> Free<'``Functor<'U>``,'U>) = bindWithMapOnInstance f x : Free<'``Functor<'U>``,'U>
-    static member inline (<*>) (f: Free<_,_>, x: Free<_,_>) = bindWithMapOnInstance (fun (x1: 'T->'U) -> bindWithMapOnInstance (fun x2 -> Pure (x1 x2)) x) f
+    static member inline (<*>) (f: Free<'``Functor<'T->'U>``,'T->'U>, x: Free<'``Functor<'T>``,'T>) = applyWithMapOnInstance f x : Free<'``Functor<'U>``,'U>
