9e0d70ee8581a76b90d11c89a267ea2853ef9151
Gauthier Segay <gauthier.segay@gmail.com>
2019-12-28 17:24:22 +0100

    * handle BifoldMap on external type * sample test on custom Either type

--- src/FSharpPlus/Bifoldable.fs
+++ src/FSharpPlus/Bifoldable.fs
@@ -7,14 +7,15 @@ open FSharpPlus.Internals
 
 type BifoldMap =
     inherit Default1
-    //static member inline BifoldMap (x:^BF         , f, g, [<Optional>]_impl: Default1)  = (^BF : (static member BifoldMap : ^BF -> _ -> _ -> ^b) x, f, g)
-    static member        BifoldMap (x: Result<_,_>, f, g, [<Optional>]_impl: BifoldMap) = match x with Ok x -> f x | Error x -> g x
-    static member        BifoldMap (x: Choice<_,_>, f, g, [<Optional>]_impl: BifoldMap) = match x with Choice1Of2 x -> f x | Choice2Of2 x -> g x
-    static member        BifoldMap (x,y : _*_     , f, g, [<Optional>]_impl: BifoldMap) = f x, g y
-    
+
+    static member        BifoldMap (x: Result<_,_>, f, g, _impl: BifoldMap) = match x with Ok x -> f x | Error x -> g x
+    static member        BifoldMap (x: Choice<_,_>, f, g, _impl: BifoldMap) = match x with Choice1Of2 x -> f x | Choice2Of2 x -> g x
+    static member        BifoldMap (x,y : _*_     , f, g, _impl: BifoldMap) = f x, g y
     
     static member inline Invoke (map1: 'T1->'U) (map2: 'T2->'U) (bifoldmap: '``BifoldMap<T1,T2>``) : 'U =
         let inline call_2 (a: ^a, b: ^b, f, g) = ((^a or ^b) : (static member BifoldMap : _*_*_*_ -> _) b,f,g,a)
         let inline call (a: 'a, b: 'b, f, g) = call_2 (a,b,f,g)
         call (Unchecked.defaultof<BifoldMap>, bifoldmap, map1, map2)
 
+type BifoldMap with
+    static member inline BifoldMap (x: 'BF         , f: _ -> 'b, g: _ -> 'b, [<Optional>]_impl: Default1)  = (^BF : (static member BifoldMap : ^BF -> _ -> _ -> ^b) x, f, g)
--- tests/FSharpPlus.Tests/BifoldableTests.fs
+++ tests/FSharpPlus.Tests/BifoldableTests.fs
@@ -2,15 +2,40 @@
 open FSharpPlus.Operators.Bifold
 open NUnit.Framework
 
+let listMapSeqLength = List.map Seq.length
+let listMapTimes2 = List.map ((*) 2)
+
 [<Test>]
 let ``bifoldMap over Choice`` () =
-    let listMapSeqLength = List.map Seq.length
-    let listMapTimes2 = List.map ((*) 2)
     
     let c1 : Choice<int list,string list> = Choice1Of2 [1..2]
     let c2 : Choice<int list,string list> = Choice2Of2 ["a";"bbbb"]
     let r1 = bifoldMap listMapTimes2 listMapSeqLength c1
     let r2 = bifoldMap listMapTimes2 listMapSeqLength c2
+    let e1 = [2;4]
+    let e2 = [1;4]
+
+    Assert.AreEqual(e1, r1)
+    Assert.AreEqual(e2, r2)
+
+
+type Either<'a,'b> = Left of 'a | Right of 'b
+open System.Runtime.InteropServices
+open FSharpPlus.Internals
+type Either<'a,'b> with
+    static member inline BifoldMap (x: Either<_,_>, f, g, [<Optional>]_impl: Default1) =
+      match x with
+      | Left a -> f a
+      | Right a -> g a
+
+[<Test>]
+let ``bifoldMap picks up on external type defining it`` () =
+
+    let l : Either<int list, string list> = Left [1..2]
+    let r : Either<int list, string list> = Right ["a";"bbbb"]
+
+    let r1 = bifoldMap listMapTimes2 listMapSeqLength l
+    let r2 = bifoldMap listMapTimes2 listMapSeqLength r
     let e1 = [2;4]
     let e2 = [1;4]
 
