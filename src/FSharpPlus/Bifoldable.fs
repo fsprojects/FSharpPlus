e352264cb0a64feaa60f4c4723297aacb4ae1702
Gauthier Segay <gauthier.segay@gmail.com>
2019-12-29 13:05:41 +0100

    * remove extraneous `call_2` * rename 'BF to appropriate type class name '``Bifoldalble<'T1,'T2>`` * prepare for defaults for BifoldBack, but need to look some more on how this should be implemented * place holder for `Bifold` implementation, which should lean on `BifoldMap`

--- src/FSharpPlus/Bifoldable.fs
+++ src/FSharpPlus/Bifoldable.fs
@@ -13,13 +13,12 @@ type BifoldMap =
     static member inline BifoldMap ((x,y)         , f, g, _impl: BifoldMap) = Plus.Invoke (f x) (g y)
     
     static member inline Invoke (map1: 'T1->'U) (map2: 'T2->'U) (bifoldmap: '``BifoldMap<T1,T2>``) : 'U =
-        let inline call_2 (a: ^a, b: ^b, f, g) = ((^a or ^b) : (static member BifoldMap : _*_*_*_ -> _) b,f,g,a)
-        let inline call (a: 'a, b: 'b, f, g) = call_2 (a,b,f,g)
+        let inline call (a: ^a, b: ^b, f, g) = ((^a or ^b) : (static member BifoldMap : _*_*_*_ -> _) b,f,g,a)
         call (Unchecked.defaultof<BifoldMap>, bifoldmap, map1, map2)
 
 type BifoldMap with
-    static member inline BifoldMap (x: 'BF         , f: _ -> 'b, g: _ -> 'b, [<Optional>]_impl: Default1)  = (^BF : (static member BifoldMap : ^BF -> _ -> _ -> ^b) x, f, g)
-    static member inline BifoldMap (_: 'BF when 'BF : null and 'BF: struct, _, _, _: Default1) = failwithf "impossible to be called"
+    static member inline BifoldMap (x: '``Bifoldable<'T1,'T2>``, f: _ -> 'b, g: _ -> 'b, [<Optional>]_impl: Default1)  = (^``Bifoldable<'T1,'T2>`` : (static member BifoldMap : ^``Bifoldable<'T1,'T2>`` -> _ -> _ -> ^b) x, f, g)
+    static member inline BifoldMap (_: '``Bifoldable<'T1,'T2>`` when '``Bifoldable<'T1,'T2>`` : null and '``Bifoldable<'T1,'T2>``: struct, _, _, _: Default1) = failwithf "impossible to be called"
 
 
 type BifoldBack =
@@ -30,6 +29,16 @@ type BifoldBack =
     static member inline BifoldBack ((x,y)          , f: 'a->'s->'s, g : 'b->'s->'s, z: 's, _impl: BifoldBack) = (f x (g y z))
 
     static member inline Invoke (fold1: 'T1->'S->'S) (fold2: 'T2->'S->'S) (z: 'S) (bifoldback: '``BifoldBack<'T1,'T2>``) : 'S =
-        let inline call_2 (a: ^a, b: ^b, f, g, z) = ((^a or ^b) : (static member BifoldBack : _*_*_*_*_ -> _) b,f,g,z,a)
-        let inline call (a: 'a, b: 'b, f, g, z) = call_2 (a,b,f,g,z)
+        let inline call (a: ^a, b: ^b, f, g, z) = ((^a or ^b) : (static member BifoldBack : _*_*_*_*_ -> _) b,f,g,z,a)
         call (Unchecked.defaultof<BifoldBack>, bifoldback, fold1, fold2, z)
+
+(*
+type BifoldBack with
+    static member inline BifoldBack (x: '``Bifoldable<'T1,'T2>``, f, g, z, [<Optional>]_impl: Default1) = (^``Bifoldable<'T1,'T2>`` : (static member BifoldBack : ^``Bifoldable<'T1,'T2>``->_->_->_-> ^b) x, f, g, z)
+    static member inline BifoldBack (x: '``Bifoldable<'T1,'T2>`` when '``Bifoldable<'T1,'T2>`` : null and '``Bifoldable<'T1,'T2>``: struct, _, _, _, _: Default1) = failwithf "impossible to be called"
+*)
+
+(*
+type Bifold =
+  //
+*)
