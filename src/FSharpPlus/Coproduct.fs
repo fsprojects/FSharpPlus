61a16ccefc12355f5f47371f480558580a5c42db
Gustavo Leon <gustavo.perezleon@gmail.com>
2020-01-11 17:10:55 +0100

    Use abstract class

--- src/FSharpPlus/Coproduct.fs
+++ src/FSharpPlus/Coproduct.fs
@@ -3,17 +3,19 @@
 open FSharpPlus
 open FSharpPlus.Control
 
+[<AbstractClass>]
 type CoproductBase<'``functorL<'t>``,'``functorR<'t>``> (left: '``functorL<'t>``, right: '``functorR<'t>``, isLeft: bool) =
     let (left, right, isLeft)    = left, right, isLeft
-    with member _.getContents () = left, right, isLeft
+    with
+        member _.getContents () = left, right, isLeft
+        override x.GetHashCode () = Unchecked.hash (x.getContents ())
+        override x.Equals o =
+            match o with
+            | :? CoproductBase<'``functorL<'t>``,'``functorR<'t>``> as y -> Unchecked.equals (x.getContents ()) (y.getContents ())
+            | _ -> false
 
 type Coproduct<[<EqualityConditionalOn>]'``functorL<'t>``,'``functorR<'t>``> (left: '``functorL<'t>``, right: '``functorR<'t>``, isLeft: bool) =
     inherit CoproductBase<'``functorL<'t>``,'``functorR<'t>``> (left, right, isLeft)
-    override x.GetHashCode () = Unchecked.hash (x.getContents ())
-    override x.Equals o =
-        match o with
-        | :? Coproduct<'``functorL<'t>``,'``functorR<'t>``> as y -> Unchecked.equals (x.getContents ()) (y.getContents ())
-        | _ -> false
 
 [<AutoOpen>]
 module CoproductPrimitives =
@@ -24,7 +26,7 @@ module CoproductPrimitives =
 
 type CoproductBase<'``functorL<'t>``,'``functorR<'t>``> with
     static member inline Map (x: CoproductBase<'``FunctorL<'T>``,'``FunctorR<'T>``>, f: 'T -> 'U) : Coproduct<'``FunctorL<'U>``,'``FunctorR<'U>``> =
-        let (l, r, isL) =  (x :?> Coproduct<'``FunctorL<'T>``,'``FunctorR<'T>``>).getContents ()
+        let (l, r, isL) = x.getContents ()
         if isL then InL (Map.Invoke f l)
         else        InR (Map.Invoke f r)
 
