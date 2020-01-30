a93ce1bfc6e0955da561b651ec2b1458ddc44a1b
Adam Davies <41101+adz@users.noreply.github.com>
2020-01-30 21:51:10 +1030

    Move MonadOps to own file

--- src/FSharpPlus/Control/ArrowApply.fs
+++ src/FSharpPlus/Control/ArrowApply.fs
@@ -30,16 +30,3 @@ type App with
     static member inline App (_output: '``ArrowApply<('ArrowApply<'T,'U> * 'T)>,'U)>``, _mthd: Default1) = App.InvokeOnInstance () : '``ArrowApply<('ArrowApply<'T,'U> * 'T)>,'U)>``
     static member inline App (_output: ^t when ^t : null and ^t : struct              , _mthd: Default1) = id
 #endif
-
-
-namespace FSharpPlus.Internals
-module internal MonadOps =
-    open FSharpPlus.Control
-
-    let inline (>>=) x f = Bind.Invoke x f
-    #if !FABLE_COMPILER
-    let inline result  x = Return.Invoke x
-    #endif
-    let inline (<*>) f x = Apply.Invoke f x
-    let inline (<|>) x y = Append.Invoke x y
-    let inline (>=>) (f: 'a->'``Monad<'b>``) (g: 'b->'``Monad<'c>``) (x: 'a) : '``Monad<'c>`` = f x >>= g
