f5fc7cfb39da15e1fbfda51f1f683ed1111f5bde
Gusty <1261319+gusty@users.noreply.github.com>
2020-02-08 08:23:10 +0100

    Use short-circuit in traverse

--- src/FSharpPlus/Control/Traversable.fs
+++ src/FSharpPlus/Control/Traversable.fs
@@ -14,8 +14,6 @@ type Sequence =
     inherit Default1
     static member inline InvokeOnInstance (t: '``Traversable<Functor<'T>>``) = (^``Traversable<Functor<'T>>`` : (static member Sequence : _ -> _) t) : '``Functor<'Traversable<'T>>``
 
-    // (f: 'T->'``Functor<'U>``) (t: '``Traversable<'T>``) : '``Functor<'Traversable<'U>>``
-
     #if !FABLE_COMPILER
     [<EditorBrowsable(EditorBrowsableState.Never)>]
     static member inline ForInfiniteSequences (t: seq<_>, isFailure) =
@@ -49,15 +47,9 @@ type Traverse =
        Seq.foldBack cons_f t (result Seq.empty)
     #endif
 
-    static member Traverse (t: 't seq, f: 't->'u option, [<Optional>]_output: option<seq<'u>>, [<Optional>]_impl: Default2) =
-       let ok = ref true
-       let res = Seq.toArray (seq {
-           use e = t.GetEnumerator ()
-           while e.MoveNext() && ok.Value do
-               match f e.Current with
-               | Some v -> yield v
-               | None   -> ok.Value <- false})
-       if ok.Value then Some (Array.toSeq res) else None
+    static member inline Traverse (t: seq<'T>, f: 'T->'``Functor<'U>``, [<Optional>]_output: '``Functor<seq<'U>>``, [<Optional>]_impl: Default2) =
+        let mapped = Seq.map f t
+        Sequence.ForInfiniteSequences (mapped, IsLeftZeroForApply.Invoke) : '``Functor<seq<'U>>``
 
     static member Traverse (t: 't seq, f: 't->Async<'u>, [<Optional>]_output: Async<seq<'u>>, [<Optional>]_impl: Default2) : Async<seq<_>> = async {
         let! ct = Async.CancellationToken
@@ -103,11 +95,7 @@ type Sequence with
                         let cons_f x ys = Map.Invoke (cons: 'a->seq<_>->seq<_>) x <*> ys
                         Seq.foldBack cons_f t (result Seq.empty)
 
-    static member        Sequence (t: seq<option<'t>>   , [<Optional>]_output: option<seq<'t>>    , [<Optional>]_impl: Default3) = Sequence.ForInfiniteSequences(t, Option.isNone)                                : option<seq<'t>>
-    static member        Sequence (t: seq<Result<'t,'e>>, [<Optional>]_output: Result<seq<'t>, 'e>, [<Optional>]_impl: Default3) = Sequence.ForInfiniteSequences(t, function (Error _     ) -> true | _ -> false) : Result<seq<'t>, 'e>
-    static member        Sequence (t: seq<Choice<'t,'e>>, [<Optional>]_output: Choice<seq<'t>, 'e>, [<Optional>]_impl: Default3) = Sequence.ForInfiniteSequences(t, function (Choice2Of2 _) -> true | _ -> false) : Choice<seq<'t>, 'e>
-    static member        Sequence (t: seq<list<'t>>     , [<Optional>]_output: list<seq<'t>>      , [<Optional>]_impl: Default3) = Sequence.ForInfiniteSequences(t, List.isEmpty)                                 : list<seq<'t>>
-    static member        Sequence (t: seq<'t []>        , [<Optional>]_output: seq<'t> []         , [<Optional>]_impl: Default3) = Sequence.ForInfiniteSequences(t, Array.isEmpty)                                : seq<'t> []
+    static member inline Sequence (t: seq<'``Applicative<'T>``>, [<Optional>]_output: '``Applicative<seq<'T>>``   , [<Optional>]_impl: Default3) = Sequence.ForInfiniteSequences (t, IsLeftZeroForApply.Invoke) : '``Applicative<seq<'T>>``
     #endif
 
     static member        Sequence (t: seq<Async<'t>>    , [<Optional>]_output: Async<seq<'t>>     , [<Optional>]_impl: Default3) = Async.Sequence t                                                               : Async<seq<'t>>
