443cb8235e0730031f4aa75483e828f667e75de0
Oskar Gewalli <wallymathieu@users.noreply.github.com>
2020-02-20 18:44:00 +0100

    Download nuget exe and download dependencies

--- docsrc/tools/download_nugets.sh
+++ docsrc/tools/download_nugets.sh
@@ -2,8 +2,18 @@
 pushd $(dirname "${0}") > /dev/null
 cd ../../
 mkdir -p packages/docs/
-nuget install FSharp.Core  -ExcludeVersion -version 4.6.2  -source https://www.nuget.org/api/v2   -OutputDirectory packages/docs/
-nuget install System.Runtime  -ExcludeVersion -version 4.3.1  -source https://www.nuget.org/api/v2   -OutputDirectory packages/docs/
-nuget install MathNet.Numerics.FSharp  -ExcludeVersion -version 4.8.1  -source https://www.nuget.org/api/v2   -OutputDirectory packages/docs/
-nuget install FSharp.Literate  -ExcludeVersion -version 4.0.0-alpha03  -source https://www.nuget.org/api/v2   -OutputDirectory packages/docs/
-nuget install FSharp.Compiler.Service -ExcludeVersion  -source https://www.nuget.org/api/v2   -OutputDirectory packages/docs/
+NUGET="./bin/nuget.exe"
+if test ! -f "$NUGET"; then
+  curl -o ./bin/nuget.exe https://dist.nuget.org/win-x86-commandline/latest/nuget.exe
+fi
+if test "$OS" = "Windows_NT"; then
+  MONO=""
+else
+  MONO="mono"
+fi
+
+$MONO $NUGET install FSharp.Core "-ExcludeVersion" "-version" 4.6.2 "-source" https://www.nuget.org/api/v2 "-OutputDirectory" packages/docs/
+$MONO $NUGET install System.Runtime "-ExcludeVersion" "-version" 4.3.1  "-source" https://www.nuget.org/api/v2 "-OutputDirectory" packages/docs/
+$MONO $NUGET install MathNet.Numerics.FSharp "-ExcludeVersion" "-version" 4.8.1  -source https://www.nuget.org/api/v2 "-OutputDirectory" packages/docs/
+$MONO $NUGET install FSharp.Literate "-ExcludeVersion" "-version" 4.0.0-alpha03  -source https://www.nuget.org/api/v2 "-OutputDirectory" packages/docs/
+$MONO $NUGET install FSharp.Compiler.Service "-ExcludeVersion" "-source" https://www.nuget.org/api/v2 "-OutputDirectory" packages/docs/
