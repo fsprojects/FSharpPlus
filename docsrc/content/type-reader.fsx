414a1bf6b61375b2389488656cf10b63490d2457
Oskar Gewalli <wallymathieu@users.noreply.github.com>
2020-03-14 22:48:53 +0100

    Sample from haskell wiki

--- docsrc/content/type-reader.fsx
+++ docsrc/content/type-reader.fsx
@@ -13,5 +13,72 @@ Examples
 
 
 #r @"../../src/FSharpPlus/bin/Release/net45/FSharpPlus.dll"
-
+open System
 open FSharpPlus
+open FSharpPlus.Data
+(**
+Sample from [The Reader monad on Haskell Wiki](https://wiki.haskell.org/All_About_Monads#The_Reader_monad)
+*)
+/// This the abstract syntax representation of a template
+type Template =
+    /// Text
+    | T of string
+    /// Variable
+    | V of Template
+    /// Quote
+    | Q of Template
+    /// Include
+    | I of Template*(Definition list)
+    /// Compound
+    | C of Template list
+and Definition = | D of Template*Template
+
+/// Our environment consists of an association list of named templates and
+/// an association list of named variable values.
+type Environment = {templates: Map<string,Template>
+                    variables: Map<string,string>}
+
+/// lookup a variable from the environment
+let lookupVar (name:string) (env:Environment) : string option = tryItem name env.variables
+
+/// lookup a template from the environment
+let lookupTemplate (name:string) (env:Environment) : Template option = tryItem name env.templates
+
+/// add a list of resolved definitions to the environment
+let addDefs (defs:(string*string) list) env = { env with variables = plus (Map.ofList defs) env.variables}
+
+/// resolve a template into a string
+let rec resolve : Template -> Reader<Environment,string>  = function 
+                       | T s -> result s
+                       | V t -> monad {
+                                   let! varName = resolve t
+                                   let! env = ask
+                                   let varValue = lookupVar varName env
+                                   return option id "" varValue }
+                        | Q t -> monad {
+                                   let! tmplName = resolve t
+                                   let! env = ask
+                                   let body = lookupTemplate tmplName env
+                                   return option string "" body }
+                        | I (t,ds) -> monad {
+                                    let! tmplName = resolve t
+                                    let! env = ask
+                                    let body = lookupTemplate tmplName env
+                                    match body with
+                                    | Some t' ->
+                                                let! defs = List.traverse resolveDef ds
+                                                return! local (addDefs defs) (resolve t')
+                                    | None -> return ""
+                                    }
+                        | C ts   -> monad {
+                                      let! resolved = List.traverse resolve ts
+                                      return String.Concat resolved
+                                    }
+and
+   /// resolve a Definition and produce a (name,value) pair
+   resolveDef: Definition -> Reader<Environment,string*string> = 
+                                      function 
+                                      | D (t,d) -> monad {
+                                        let! name = resolve t
+                                        let! value = resolve d
+                                        return (name,value) }
