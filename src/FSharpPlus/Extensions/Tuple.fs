5cc3a6a714f8ced35d14ef28a583678c306a926b
Gusty <1261319+gusty@users.noreply.github.com>
2020-06-13 07:22:55 +0200

    + Tuple2 and Tuple3 extensions

---
+++ src/FSharpPlus/Extensions/Tuple.fs
@@ -0,0 +1,14 @@
+﻿namespace FSharpPlus
+
+/// Additional operations on Tuple (,)
+[<RequireQualifiedAccess>]
+module Tuple2 =
+    let mapItem1 f (x, y) = (f x, y)
+    let mapItem2 f (x, y) = (x, f y)
+
+/// Additional operations on Tuple (,,)
+[<RequireQualifiedAccess>]
+module Tuple3 =
+    let mapItem1 f (x, y, z) = (f x, y, z)
+    let mapItem2 f (x, y, z) = (x, f y, z)
+    let mapItem3 f (x, y, z) = (x, y, f z)
