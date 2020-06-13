6a69bec69b6e2233eb23c19b0ed1e6837235d30e
Gusty <1261319+gusty@users.noreply.github.com>
2020-06-13 08:58:15 +0200

    Complete links of related types in the docs

--- docsrc/content/abstraction-category.fsx
+++ docsrc/content/abstraction-category.fsx
@@ -62,7 +62,7 @@ From .Net/F#
  
 From F#+
 
- -  ``Kleisli<'T, 'Monad<'U>>``
+ -  [``Kleisli<'T, 'Monad<'U>>``](type-kleisli.html)
 
  [Suggest another](https://github.com/fsprojects/FSharpPlus/issues/new) concrete implementation
 *)
