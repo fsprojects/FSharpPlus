db130ecedd9d49af596b30439bab21f8ab2b8051
Oskar Gewalli <wallymathieu@users.noreply.github.com>
2020-07-15 21:50:51 +0200

    +Return values

--- docsrc/content/type-any.fsx
+++ docsrc/content/type-any.fsx
@@ -33,5 +33,7 @@ open FSharpPlus
 open FSharpPlus.Data
 
 let res1 = (Any true) ++ ( zero ) ++ (Any false)
+// val res1 : Any = Any true
 let even x = x % 2 = 0
 let res2 = sum <| map ( fun x -> Any (even x) ) [2;4;6;7;8]
+// val res2 : Any = Any true
