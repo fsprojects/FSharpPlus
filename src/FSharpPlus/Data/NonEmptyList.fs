15ed24ba6a2e683cb20c6c0c140f4c0d37a896c1
aammfe <aammfe@gmail.com>
2020-08-29 01:57:54 +0500

    Add Mapi Support in NonEmpty List

--- src/FSharpPlus/Data/NonEmptyList.fs
+++ src/FSharpPlus/Data/NonEmptyList.fs
@@ -8,27 +8,48 @@ open FSharpPlus.Control
 
 
 /// A type-safe list that contains at least one element.
-type NonEmptyList<'t> = {Head: 't; Tail: 't list} with
-    interface IEnumerable<'t> with member x.GetEnumerator () = (let {Head = x; Tail = xs} = x in seq (x::xs)).GetEnumerator ()
-    interface System.Collections.IEnumerable with member x.GetEnumerator () = (let {Head = x; Tail = xs} = x in seq (x::xs)).GetEnumerator () :> System.Collections.IEnumerator
-    interface IReadOnlyCollection<'t>        with member s.Count = 1 + List.length s.Tail
-    interface IReadOnlyList<'t>              with member s.Item with get index = s.Item index
+type NonEmptyList<'t> =
+    { Head: 't
+      Tail: 't list }
+    interface IEnumerable<'t> with
+        member x.GetEnumerator() =
+            (let { Head = x; Tail = xs } = x in seq (x :: xs)).GetEnumerator()
+
+    interface System.Collections.IEnumerable with
+        member x.GetEnumerator() =
+            (let { Head = x; Tail = xs } = x in seq (x :: xs)).GetEnumerator() :> System.Collections.IEnumerator
+
+    interface IReadOnlyCollection<'t> with
+        member s.Count = 1 + List.length s.Tail
+
+    interface IReadOnlyList<'t> with
+        member s.Item
+            with get index = s.Item index
 
     [<System.Obsolete("Use Head instead.")>]
-    member this.head = let {Head = a; Tail = _} = this in a
+    member this.head = let { Head = a; Tail = _ } = this in a
 
     [<System.Obsolete("Use Tail instead.")>]
-    member this.tail = let           {Tail = a} = this in a
+    member this.tail = let { Tail = a } = this in a
 
-    member this.Item = function 0 -> this.Head | n -> this.Tail.[n-1]
-    member this.GetSlice = function
-        | None  , None
+    member this.Item =
+        function
+        | 0 -> this.Head
+        | n -> this.Tail.[n - 1]
+
+    member this.GetSlice =
+        function
+        | None, None
         | Some 0, None
         | Some 0, Some 0 -> this
-        | Some a, None   -> let {Head = _; Tail = xs} = this in {Head = xs.[a-1]; Tail = xs.[a..   ]}
-        | None  , Some b 
-        | Some 0, Some b -> let {Head = x; Tail = xs} = this in {Head = x       ; Tail = xs.[ ..b-1]}
-        | Some a, Some b -> let {Head = _; Tail = xs} = this in {Head = xs.[a-1]; Tail = xs.[a..b-1]}
+        | Some a, None -> let { Head = _; Tail = xs } = this in { Head = xs.[a - 1]; Tail = xs.[a..] }
+        | None, Some b
+        | Some 0, Some b -> let { Head = x; Tail = xs } = this in { Head = x; Tail = xs.[..b - 1] }
+        | Some a, Some b ->
+            let { Head = _; Tail = xs } = this
+            { Head = xs.[a - 1]
+              Tail = xs.[a..b - 1] }
+
     member this.Length = 1 + List.length this.Tail
 
 
@@ -36,13 +57,17 @@ type NonEmptyList<'t> = {Head: 't; Tail: 't list} with
 [<RequireQualifiedAccess>]
 module NonEmptyList =
     /// <summary>Builds a non empty list.</summary>
-    let create x xs = {Head = x; Tail = xs}
+    let create x xs = { Head = x; Tail = xs }
     /// <summary>Builds a non empty list with a single element.</summary>
-    let singleton x = {Head = x; Tail = []}
+    let singleton x = { Head = x; Tail = [] }
     /// <summary>Builds a list from the given non empty list.</summary>
-    let toList {Head = x; Tail = xs} = x::xs
+    let toList { Head = x; Tail = xs } = x :: xs
     /// <summary>Builds a sequence from the given non empty list.</summary>
-    let toSeq  {Head = x; Tail = xs} = seq { yield x; yield! xs; }
+    let toSeq { Head = x; Tail = xs } =
+        seq {
+            yield x
+            yield! xs
+        }
     /// <summary>Builds an array from the given non empty list.</summary>
     let toArray nel = toList nel |> List.toArray
     /// <summary>Builds a non empty list from the given array.</summary>
@@ -50,62 +75,76 @@ module NonEmptyList =
     /// <returns>Non empty list containing the elements of the array.</returns>
     /// <exception cref="System.ArgumentException">Thrown when the input array is empty.</exception>
     /// <remarks>Throws exception for empty array</remarks>
-    let ofArray (array : _ array) =
+    let ofArray (array: _ array) =
         match array |> Array.toList with
-        | []    -> invalidArg "array" "The input array was empty."
-        | x::xs -> create x xs
+        | [] -> invalidArg "array" "The input array was empty."
+        | x :: xs -> create x xs
     /// <summary>Builds a non empty list from the given list.</summary>
     /// <param name="list">The input list.</param>
     /// <returns>Non empty list containing the elements of the list.</returns>
     /// <exception cref="System.ArgumentException">Thrown when the input list is empty.</exception>
     /// <remarks>Throws exception for empty list</remarks>
-    let ofList (list : _ list) =
+    let ofList (list: _ list) =
         match list with
-        | []    -> invalidArg "list" "The input list was empty."
-        | x::xs -> create x xs
+        | [] -> invalidArg "list" "The input list was empty."
+        | x :: xs -> create x xs
     /// <summary>Builds a non empty list from the given sequence.</summary>
     /// <param name="seq">The input list.</param>
     /// <returns>Non empty list containing the elements of the list.</returns>
     /// <exception cref="System.ArgumentException">Thrown when the input list is empty.</exception>
     /// <remarks>Throws exception for empty list</remarks>
-    let ofSeq (seq : _ seq) =
+    let ofSeq (seq: _ seq) =
         match seq |> Seq.toList with
-        | []    -> invalidArg "seq" "The input sequence was empty."
-        | x::xs -> create x xs
+        | [] -> invalidArg "seq" "The input sequence was empty."
+        | x :: xs -> create x xs
     /// Returns the length of a non empty list. You can also use property nel.Length.
-    let length (nel:_ NonEmptyList) = nel.Length
+    let length (nel: _ NonEmptyList) = nel.Length
     /// <summary>Build a new non empty list whose elements are the results of applying the given function
     /// to each of the elements of the non empty list.</summary>
-    let map f  {Head = x; Tail = xs} = {Head = f x; Tail = List.map f xs}
+    let map f { Head = x; Tail = xs } = { Head = f x; Tail = List.map f xs }
+
+    /// <summary>Build a new non empty list whose elements are the results of applying the given function with index
+    /// to each of the elements of the non empty list.</summary>
+    let mapi f { Head = x; Tail = xs } =
+        let maperI = (fun i item -> f (i + 1) item)
+        { Head = f 0 x
+          Tail = List.mapi maperI xs }
 
     /// <summary>Splits a list of pairs into two lists.</summary>
     /// <param name="list">The input list.</param>
     /// <returns>Two lists of split elements.</returns>
-    let unzip (list: NonEmptyList<'T1 * 'T2>) = let t1, t2 = List.unzip list.Tail in {Head = fst list.Head  ; Tail = t1}, {Head = snd list.Head; Tail = t2}
+    let unzip (list: NonEmptyList<'T1 * 'T2>) =
+        let t1, t2 = List.unzip list.Tail in { Head = fst list.Head; Tail = t1 }, { Head = snd list.Head; Tail = t2 }
 
     /// <summary>Combines the two lists into a list of pairs. The two lists must have equal lengths.</summary>
     /// <param name="list1">The first input list.</param>
     /// <param name="list2">The second input list.</param>
     /// <returns>A single list containing pairs of matching elements from the input lists.</returns>
-    let zip (list1: NonEmptyList<'T>) (list2: NonEmptyList<'U>) = {Head = (list1.Head, list2.Head); Tail = List.zip list1.Tail list2.Tail}
+    let zip (list1: NonEmptyList<'T>) (list2: NonEmptyList<'U>) =
+        { Head = (list1.Head, list2.Head)
+          Tail = List.zip list1.Tail list2.Tail }
     /// Returns a new NonEmptyList with the element added to the beginning.
-    let cons e {Head = x; Tail = xs} = {Head = e ; Tail = x::xs}
+    let cons e { Head = x; Tail = xs } = { Head = e; Tail = x :: xs }
     /// Returns the first element of a new non empty list. You can also use property nel.Head.
-    let head {Head = x; Tail = _ } = x
+    let head { Head = x; Tail = _ } = x
     /// <summary>Returns a new NonEmptyList of the elements trailing the first element.</summary>
     /// <exception cref="System.ArgumentException">Thrown when the tail is empty.</exception>
     /// <remarks>Throws exception for empty tail</remarks>
-    let tail {Head = _; Tail = xs } = ofList xs
+    let tail { Head = _; Tail = xs } = ofList xs
+
     let rec tails s =
-        let {Tail = xs} = s
+        let { Tail = xs } = s
         match xs with
-        | []   -> {Head = s; Tail = []}
-        | h::t -> cons s (tails {Head = h; Tail = t})
+        | [] -> { Head = s; Tail = [] }
+        | h :: t -> cons s (tails { Head = h; Tail = t })
 
 #if !FABLE_COMPILER
-    let inline traverse (f: 'T->'``Functor<'U>``) (s: NonEmptyList<'T>) =
-        let lst = traverse f (toList s) : '``Functor<'List<'U>>``
-        (create << List.head |> fun f x -> f x (List.tail x)) <!> lst : '``Functor<NonEmptyList<'U>>``
+    let inline traverse (f: 'T -> '``Functor<'U>``) (s: NonEmptyList<'T>) =
+        let lst: '``Functor<'List<'U>>`` = traverse f (toList s)
+        (create
+         << List.head
+         |> fun f x -> f x (List.tail x))
+        <!> lst: '``Functor<NonEmptyList<'U>>``
 #endif
 
     /// <summary>Returns the average of the elements in the list.</summary>
@@ -117,26 +156,29 @@ module NonEmptyList =
     /// <param name="projection">The function to transform the list elements into the type to be averaged.</param>
     /// <param name="list">The input list.</param>
     /// <returns>The resulting average.</returns>
-    let inline averageBy (projection: 'T -> ^U) list = List.averageBy projection (list.Head :: list.Tail)
+    let inline averageBy (projection: 'T -> ^U) list =
+        List.averageBy projection (list.Head :: list.Tail)
 
     /// <summary>Applies a function to each element of the list, threading an accumulator argument
     /// through the computation. Apply the function to the first two elements of the list.
-    /// Then feed this result into the function along with the third element and so on. 
-    /// Return the final result. If the input function is <c>f</c> and the elements are <c>i0...iN</c> then computes 
+    /// Then feed this result into the function along with the third element and so on.
+    /// Return the final result. If the input function is <c>f</c> and the elements are <c>i0...iN</c> then computes
     /// <c>f (... (f i0 i1) i2 ...) iN</c>.</summary>
     /// <param name="reduction">The function to reduce two list elements to a single element.</param>
     /// <param name="list">The input list.</param>
     /// <returns>The final reduced value.</returns>
-    let reduce (reduction: 'T -> 'T -> 'T) list = List.reduce reduction (list.Head :: list.Tail)
+    let reduce (reduction: 'T -> 'T -> 'T) list =
+        List.reduce reduction (list.Head :: list.Tail)
 
     /// <summary>Applies a function to each element of the list, starting from the end, threading an accumulator argument
-    /// through the computation. If the input function is <c>f</c> and the elements are <c>i0...iN</c> then computes 
+    /// through the computation. If the input function is <c>f</c> and the elements are <c>i0...iN</c> then computes
     /// <c>f i0 (...(f iN-1 iN))</c>.</summary>
     /// <param name="reduction">A function that takes in the next-to-last element of the list and the
     /// current accumulated result to produce the next accumulated result.</param>
     /// <param name="list">The input list.</param>
     /// <returns>The final result of the reductions.</returns>
-    let reduceBack (reduction: 'T -> 'T -> 'T) list = List.reduceBack reduction (list.Head :: list.Tail)
+    let reduceBack (reduction: 'T -> 'T -> 'T) list =
+        List.reduceBack reduction (list.Head :: list.Tail)
 
     /// <summary>Returns the greatest of all elements of the list, compared via Operators.max.</summary>
     /// <param name="list">The input list.</param>
@@ -147,7 +189,8 @@ module NonEmptyList =
     /// <param name="projection">The function to transform the list elements into the type to be compared.</param>
     /// <param name="list">The input list.</param>
     /// <returns>The maximum element.</returns>
-    let maxBy (projection: 'T -> 'U) list = List.maxBy projection (list.Head :: list.Tail)
+    let maxBy (projection: 'T -> 'U) list =
+        List.maxBy projection (list.Head :: list.Tail)
 
     /// <summary>Returns the lowest of all elements of the list, compared via Operators.min.</summary>
     /// <param name="list">The input list.</param>
@@ -158,89 +201,119 @@ module NonEmptyList =
     /// <param name="projection">The function to transform list elements into the type to be compared.</param>
     /// <param name="list">The input list.</param>
     /// <returns>The minimum value.</returns>
-    let minBy (projection: 'T -> 'U) list = List.minBy projection (list.Head :: list.Tail)
+    let minBy (projection: 'T -> 'U) list =
+        List.minBy projection (list.Head :: list.Tail)
 
     /// Equivalent to [start..stop] on regular lists.
-    let inline range (start: 'T) stop = create start (List.drop 1 [start..stop])
+    let inline range (start: 'T) stop =
+        create start (List.drop 1 [ start .. stop ])
 
 #if !FABLE_COMPILER
     /// Reduces using alternative operator `<|>`.
-    let inline choice (list: NonEmptyList<'``Alt<'T>``>) = reduce (<|>) list : '``Alt<'T>``
+    let inline choice (list: NonEmptyList<'``Alt<'T>``>): '``Alt<'T>`` = reduce (<|>) list
 #endif
 
     /// Transforms a list to a NonEmptyList, returning an option to signal when the original list was empty.
     let tryOfList s =
         match s with
-        | []    -> None
-        | x::xs -> Some (create x xs)
+        | [] -> None
+        | x :: xs -> Some(create x xs)
 
 type NonEmptyList<'t> with
     [<EditorBrowsable(EditorBrowsableState.Never)>]
-    static member Map (x: NonEmptyList<'a>, f: 'a->'b) = NonEmptyList.map f x
-    
+    static member Map(x: NonEmptyList<'a>, f: 'a -> 'b) = NonEmptyList.map f x
+
+    [<EditorBrowsable(EditorBrowsableState.Never)>]
+    static member MapI(x: NonEmptyList<_>, f) = NonEmptyList.mapi f x
+
     [<EditorBrowsable(EditorBrowsableState.Never)>]
     static member Unzip s = NonEmptyList.unzip s
 
     [<EditorBrowsable(EditorBrowsableState.Never)>]
-    static member Zip (x, y) = NonEmptyList.zip x y
+    static member Zip(x, y) = NonEmptyList.zip x y
 
-    static member (>>=) ({Head = x; Tail = xs}, f: _->NonEmptyList<'b>) =
-        let {Head = y; Tail = ys} = f x
-        let ys' = List.collect (NonEmptyList.toList << f) xs
-        {Head = y; Tail = (ys @ ys')}
+    static member (>>=)({ Head = x; Tail = xs }, f: _ -> NonEmptyList<'b>) =
+        let { Head = y; Tail = ys } = f x
 
-    static member Return (x: 'a) = {Head = x; Tail = []}
-    static member (<*>)  (f: NonEmptyList<'T->'U>, x: NonEmptyList<'T>) =
-        let r = NonEmptyList.toList f </List.apply/> NonEmptyList.toList x
-        {Head = r.Head; Tail = r.Tail}
+        let ys' =
+            List.collect (NonEmptyList.toList << f) xs
 
-    static member Lift2 (f: 'T -> 'U -> 'V, x, y) = NonEmptyList.ofList (List.lift2 f (NonEmptyList.toList x) (NonEmptyList.toList y))
+        { Head = y; Tail = (ys @ ys') }
 
-    static member Extract   {Head = h; Tail = _} = h : 't
+    static member Return(x: 'a) = { Head = x; Tail = [] }
 
-    #if !FABLE_COMPILER
-    static member Duplicate (s: NonEmptyList<'a>, [<Optional>]_impl: Duplicate) = NonEmptyList.tails s
-    #endif
+    static member (<*>)(f: NonEmptyList<'T -> 'U>, x: NonEmptyList<'T>) =
+        let r =
+            NonEmptyList.toList f
+            </ List.apply
+            /> NonEmptyList.toList x
 
-    static member (=>>)     (s, g) = NonEmptyList.map g (NonEmptyList.tails s) : NonEmptyList<'b>
-    
+        { Head = r.Head; Tail = r.Tail }
 
-    static member (+) ({Head = h; Tail = t},  x) = {Head = h; Tail = t @ NonEmptyList.toList x}
+    static member Lift2(f: 'T -> 'U -> 'V, x, y) =
+        NonEmptyList.ofList (List.lift2 f (NonEmptyList.toList x) (NonEmptyList.toList y))
 
-    static member FoldBack ({Head = x; Tail = xs}, f, z) = List.foldBack f (x::xs) z
+    static member Extract { Head = h; Tail = _ }: 't = h
 
-    #if !FABLE_COMPILER
+#if !FABLE_COMPILER
+    static member Duplicate(s: NonEmptyList<'a>, [<Optional>] _impl: Duplicate) = NonEmptyList.tails s
+#endif
+
+    static member (=>>)(s, g): NonEmptyList<'b> =
+        NonEmptyList.map g (NonEmptyList.tails s)
+
+
+    static member (+)({ Head = h; Tail = t }, x) =
+        { Head = h
+          Tail = t @ NonEmptyList.toList x }
+
+    static member FoldBack({ Head = x; Tail = xs }, f, z) = List.foldBack f (x :: xs) z
+
+#if !FABLE_COMPILER
     [<EditorBrowsable(EditorBrowsableState.Never)>]
-    static member ToList (s: NonEmptyList<'a>, [<Optional>]_impl: ToList) = NonEmptyList.toList s    
+    static member ToList(s: NonEmptyList<'a>, [<Optional>] _impl: ToList) = NonEmptyList.toList s
 
     [<EditorBrowsable(EditorBrowsableState.Never)>]
-    static member ToSeq (s: NonEmptyList<'a>, [<Optional>]_impl: ToSeq ) = NonEmptyList.toList s |> List.toSeq
+    static member ToSeq(s: NonEmptyList<'a>, [<Optional>] _impl: ToSeq) = NonEmptyList.toList s |> List.toSeq
 
     [<EditorBrowsable(EditorBrowsableState.Never)>]
-    static member inline Traverse (s: NonEmptyList<'T>, f: 'T->'``Functor<'U>``) : '``Functor<NonEmptyList<'U>>`` = NonEmptyList.traverse f s
+    static member inline Traverse(s: NonEmptyList<'T>, f: 'T -> '``Functor<'U>``): '``Functor<NonEmptyList<'U>>`` =
+        NonEmptyList.traverse f s
 
-    static member Replace (source: NonEmptyList<'T>, oldValue: NonEmptyList<'T>, newValue: NonEmptyList<'T>, _impl: Replace ) =
-        let lst = source |> NonEmptyList.toSeq |> Seq.replace oldValue newValue |> Seq.toList
-        {Head = lst.Head; Tail = lst.Tail}
+    static member Replace(source: NonEmptyList<'T>,
+                          oldValue: NonEmptyList<'T>,
+                          newValue: NonEmptyList<'T>,
+                          _impl: Replace) =
+        let lst =
+            source
+            |> NonEmptyList.toSeq
+            |> Seq.replace oldValue newValue
+            |> Seq.toList
 
-    static member Reduce ({Head = x; Tail = xs}, reduction: 'T -> 'T -> 'T) = List.reduce reduction (x :: xs)
+        { Head = lst.Head; Tail = lst.Tail }
 
-    static member inline Choice (source: NonEmptyList<'``Alt<'T>``>) =
-        use e = (NonEmptyList.toSeq source).GetEnumerator ()
+    static member Reduce({ Head = x; Tail = xs }, reduction: 'T -> 'T -> 'T) = List.reduce reduction (x :: xs)
+
+    static member inline Choice(source: NonEmptyList<'``Alt<'T>``>) =
+        use e =
+            (NonEmptyList.toSeq source).GetEnumerator()
+
         e.MoveNext() |> ignore
         let mutable res = e.Current
         while e.MoveNext() && not (IsAltLeftZero.Invoke res) do
             res <- Append.Invoke res e.Current
         res
-    #endif
+#endif
 
 
 [<AutoOpen>]
 module NonEmptyListBuilder =
-    type NelBuilder () =
+    type NelBuilder() =
         [<CompilerMessage("A NonEmptyList doesn't support the Zero operation.", 708, IsError = true)>]
-        member __.Zero () = raise Internals.Errors.exnUnreachable
-        member __.Combine (a: NonEmptyList<'T>, b) = a + b
+        member __.Zero() = raise Internals.Errors.exnUnreachable
+
+        member __.Combine(a: NonEmptyList<'T>, b) = a + b
         member __.Yield x = NonEmptyList.singleton x
-        member __.Delay expr = expr () : NonEmptyList<'T>
-    let nel = NelBuilder ()
+        member __.Delay expr: NonEmptyList<'T> = expr ()
+
+    let nel = NelBuilder()
