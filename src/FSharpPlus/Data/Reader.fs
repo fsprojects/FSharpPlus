53ed99bf95aa961f21769de5f9f5fc7058a7b5b3
Gustavo Leon <1261319+gusty@users.noreply.github.com>
2020-09-21 00:31:30 +0200

    Use it in ReaderT

--- src/FSharpPlus/Data/Reader.fs
+++ src/FSharpPlus/Data/Reader.fs
@@ -111,7 +111,7 @@ type ReaderT<'r,'``monad<'t>``> with
     [<EditorBrowsable(EditorBrowsableState.Never)>]
     static member inline Zip (x: ReaderT<'S,'``Monad<'T>``>, y: ReaderT<'S,'``Monad<'U>``>) = ReaderT.zip x y
 
-    static member inline TryWith (source: ReaderT<'R,'``Monad<'T>``>, f: exn -> ReaderT<'R,'``Monad<'T>``>) = ReaderT (fun s -> TryWith.Invoke (ReaderT.run source s) (fun x -> ReaderT.run (f x) s))
+    static member inline TryWith (source: ReaderT<'R,'``Monad<'T>``>, f: exn -> ReaderT<'R,'``Monad<'T>``>) = ReaderT (fun s -> TryWithStrict.Invoke (fun () -> ReaderT.run source s) (fun x -> ReaderT.run (f x) s))
     static member inline TryFinally (computation: ReaderT<'R,'``Monad<'T>``>, f) = ReaderT (fun s -> TryFinally.Invoke     (ReaderT.run computation s) f)
     static member inline Using (resource, f: _ -> ReaderT<'R,'``Monad<'T>``>)    = ReaderT (fun s -> Using.Invoke resource (fun x -> ReaderT.run (f x) s))
     static member inline Delay (body : unit   ->  ReaderT<'R,'``Monad<'T>``>)    = ReaderT (fun s -> Delay.Invoke (fun _ -> ReaderT.run (body ()) s))
