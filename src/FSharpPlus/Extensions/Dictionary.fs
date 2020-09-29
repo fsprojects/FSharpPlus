d74a51232a8a7b9e96ebaee94dfe2ddc82681fe4
Adam Davies <41101+adz@users.noreply.github.com>
2020-09-30 08:36:06 +0930

    Extensions docs (#355)
    Documenting code in extensions

--- src/FSharpPlus/Extensions/Dictionary.fs
+++ src/FSharpPlus/Extensions/Dictionary.fs
@@ -8,18 +8,51 @@ module Dictionary =
 
     #if !FABLE_COMPILER
     open System.Linq
+
+    /// Converts a Dictionary to an IReadOnlyDictionary
     let toIReadOnlyDictionary (source: Dictionary<'Key, 'Value>) = ReadOnlyDictionary source :> IReadOnlyDictionary<_,_>
+    
     #endif
 
+    /// <summary>Tries to get the value of the given key.</summary>
+    /// <remarks>Note: this is a function wrapper for the Dictionary.TryGetValue method,
+    /// which also represents the result as an Option&lt;value&gt; instead of a bool
+    /// and an out-value.
+    /// </remarks>
+    /// <param name="k">The key to find.</param>
+    /// <param name="dct">The input dictionary.</param>
+    ///
+    /// <returns>An option wrapped value</returns>
     let tryGetValue k (dct: Dictionary<'Key, 'Value>) =
         match dct.TryGetValue k with
         | true, v -> Some v
         | _       -> None
+
+    /// <summary>Does the dictionary contain the given key?</summary>
+    /// <remarks>Note: this is a function wrapper for the Dictionary.ContainsKey method.</remarks>
+    /// <param name="k">The key to find.</param>
+    /// <param name="dct">The input dictionary.</param>
+    ///
+    /// <returns>A bool indicating if the key was found</returns>
     let containsKey k (dct: Dictionary<'Key, 'Value>) = dct.ContainsKey k
 
+    /// <summary>Returns the keys of the given dictionary.</summary>
+    /// <param name="source">The input dictionary.</param>
+    ///
+    /// <returns>A seq of the keys in the dictionary.</returns>
     let keys   (source: Dictionary<_,_>) = Seq.map (fun (KeyValue(k, _)) -> k) source
+
+    /// <summary>Returns the values of the given dictionary.</summary>
+    /// <param name="source">The input dictionary.</param>
+    ///
+    /// <returns>A seq of the values in the dictionary.</returns>
     let values (source: Dictionary<_,_>) = Seq.map (fun (KeyValue(_, v)) -> v) source
 
+    /// <summary>Maps the given function over each value in the dictionary.</summary>
+    /// <param name="f">The mapping function.</param>
+    /// <param name="x">The input dictionary.</param>
+    ///
+    /// <returns>The mapped dictionary.</returns>
     let map f (x: Dictionary<'Key, 'T>) =
         let dct = Dictionary<'Key, 'U> ()
         for KeyValue(k, v) in x do
@@ -41,7 +74,7 @@ module Dictionary =
             | None    -> ()
         dct
 
-    /// <summary>Tuple values of two dictionaries.</summary>
+    /// <summary>Tuples values of two dictionaries.</summary>
     /// <remarks>Keys that are not present on both dictionaries are dropped.</remarks>
     /// <param name="x">The first input dictionary.</param>
     /// <param name="y">The second input dictionary.</param>
@@ -55,6 +88,10 @@ module Dictionary =
             | None    -> ()
         dct
 
+    /// <summary>Splits a dictionary with tuple pair values to two separate dictionaries.</summary>
+    /// <param name="source">The source dictionary.</param>
+    ///
+    /// <returns>A tuple of each untupled dictionary.</returns>
     let unzip (source: Dictionary<'Key, 'T1 * 'T2>) =
         let dct1 = Dictionary<'Key, 'T1> ()
         let dct2 = Dictionary<'Key, 'T2> ()
--- src/FSharpPlus/Extensions/Task.fs
+++ src/FSharpPlus/Extensions/Task.fs
@@ -12,7 +12,7 @@ module Task =
     /// <summary>Creates a task workflow from another workflow 'x', mapping its result with 'f'.</summary>
     let map (f : 'T -> 'U) (t : Task<'T>) : Task<'U> = t.ContinueWith(fun (t' : Task<'T>) -> f (t'.Result))
 
-    /// <summary>Creates a task workflow from two workflows 'x' and 'y', mapping it results with 'f'.</summary>
+    /// <summary>Creates a task workflow from two workflows 'x' and 'y', mapping its results with 'f'.</summary>
     /// <remarks>Workflows are run in sequence.</remarks>
     /// <param name="f">The mapping function.</param>
     /// <param name="x">First task workflow.</param>
@@ -22,7 +22,7 @@ module Task =
             y.ContinueWith(fun (y' : Task<'U>) ->
                 (f x'.Result y'.Result))).Unwrap()
 
-    /// <summary>Create a task workflow that is the result of applying the resulting function of a task workflow
+    /// <summary>Creates a task workflow that is the result of applying the resulting function of a task workflow
     /// to the resulting value of another task workflow</summary>
     /// <param name="f">Task workflow returning a function</param>
     /// <param name="x">Task workflow returning a value</param>
@@ -31,12 +31,12 @@ module Task =
             t.ContinueWith(fun (t' : Task<'T>) ->
                 f'.Result t'.Result)).Unwrap()
 
-    /// <summary>Creates a task workflow from two workflows 'x' and 'y', tupling it results.</summary>
+    /// <summary>Creates a task workflow from two workflows 'x' and 'y', tupling its results.</summary>
     let zip (x : Task<'T>) (y : Task<'U>) : Task<'T * 'U> =
         x.ContinueWith(fun (x' : Task<'T>) ->
             y.ContinueWith(fun (y' : Task<'U>) ->
                 (x'.Result, y'.Result))).Unwrap()
 
-    /// Flatten two nested tasks into one.
+    /// Flattens two nested tasks into one.
     let join (t : Task<Task<'T>>) : Task<'T> = t.Unwrap()
 #endif
