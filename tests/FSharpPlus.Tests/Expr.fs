4b431800d4e24cdb31228fcfd26a982bf3c656cd
Gusty <1261319+gusty@users.noreply.github.com>
2020-10-11 15:43:12 +0200

    + 2-layers test

--- tests/FSharpPlus.Tests/Expr.fs
+++ tests/FSharpPlus.Tests/Expr.fs
@@ -18,14 +18,34 @@ module Expr =
 
         areEqual "11" res
 
+    let unquote   x = Swensen.Unquote.Operators.evalRaw x
+    let powerpack x = Microsoft.FSharp.Linq.QuotationEvaluator.EvaluateUntyped x
+    
     // [<Test>]
     // let ``Simple quotation combination (QuotationEvaluator)`` () =
     //     ``Simple quotation combination`` FSharp.Quotations.Evaluator.QuotationEvaluator.EvaluateUntyped
 
     [<Test>]
-    let ``Simple quotation combination (Unquote)`` () =
-        ``Simple quotation combination`` Swensen.Unquote.Operators.evalRaw
+    let ``Simple quotation combination [Unquote]`` () = ``Simple quotation combination`` unquote
     
     [<Test>]
-    let ``Simple quotation combination (PowerPack)`` () =
-        ``Simple quotation combination`` Microsoft.FSharp.Linq.QuotationEvaluator.EvaluateUntyped 
+    let ``Simple quotation combination [PowerPack]`` () = ``Simple quotation combination`` powerpack
+
+
+    let ``2-layers quotation combination`` evaluator =
+        let expr = 
+            <@ 4 + 5 @>
+            >>= fun x ->
+                let a = x + 10
+                <@  (a, a*a) @>
+            >>= fun (x, y) ->
+                <@  ([x + y], x, y, [|x; y|]) @>
+        let res = Expr.run evaluator expr
+
+        areEqual ([380], 19, 361, [|19; 361|]) res
+
+    [<Test>]
+    let ``2-layers quotation combination [Unquote]`` () = ``2-layers quotation combination`` unquote
+    
+    [<Test>]
+    let ``2-layers quotation combination [PowerPack]`` () = ``2-layers quotation combination`` powerpack
