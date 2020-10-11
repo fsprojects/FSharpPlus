0e33e6f2b0357db6c4bea48608f74ae9917ac7f0
Oskar Gewalli <wallymathieu@users.noreply.github.com>
2020-10-11 15:53:35 +0200

    F# markdown content instead of evaluation

--- docsrc/tools/manually_build_docs.sh
+++ docsrc/tools/manually_build_docs.sh
@@ -15,4 +15,4 @@ dotnet run --project ./docsrc/tools
 # dotnet run --project ./docsrc/tools ReleaseDocs
 
 # To run the documentation without going through docsrc/tools:
-# (export TargetPath=src/FSharpPlus/bin/Debug/netstandard2.0/FSharpPlus.dll ;dotnet fsdocs build --projects src/FSharpPlus/FSharpPlus.fsproj --input docsrc/content/ --output docs/)
+# (export TargetPath=src/FSharpPlus/bin/Release/net45/FSharpPlus.dll ;dotnet fsdocs build --projects src/FSharpPlus/FSharpPlus.fsproj --input docsrc/content/ --output docs/)
