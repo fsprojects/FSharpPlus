ad9cdf0070ab90ac1b28f0781e0b500ef3eb0379
Gusty <1261319+gusty@users.noreply.github.com>
2020-10-11 13:42:54 +0200

    + tests

---
+++ tests/FSharpPlus.Tests/Expr.fs
@@ -0,0 +1,31 @@
+﻿namespace FSharpPlus.Tests
+
+open System
+open NUnit.Framework
+open FSharpPlus
+open FSharpPlus.Tests.Helpers
+
+module Expr =
+
+    let ``Simple quotation combination`` evaluator =
+        let one = <@ 1 @>
+        let add10AndToString x =
+            let a = string (x + 10)
+            <@ a @>
+
+        let expr = one >>= add10AndToString
+        let res = Expr.run evaluator expr
+
+        areEqual "11" res
+
+    [<Test>]
+    let ``Simple quotation combination (QuotationEvaluator)`` () =
+        ``Simple quotation combination`` FSharp.Quotations.Evaluator.QuotationEvaluator.EvaluateUntyped
+
+    [<Test>]
+    let ``Simple quotation combination (Unquote)`` () =
+        ``Simple quotation combination`` Swensen.Unquote.Operators.evalRaw
+    
+    [<Test>]
+    let ``Simple quotation combination (PowerPack)`` () =
+        ``Simple quotation combination`` Microsoft.FSharp.Linq.QuotationEvaluator.EvaluateUntyped 
