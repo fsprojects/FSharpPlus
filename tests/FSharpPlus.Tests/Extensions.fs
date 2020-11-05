1c4eaab4d5f343bc6b7cdc68e8b27b620cf4569e
Pawel Zelmanski <pzelmanski1@gmail.com>
2020-11-05 20:56:19 +0100

    removed redundant tests

--- tests/FSharpPlus.Tests/Extensions.fs
+++ tests/FSharpPlus.Tests/Extensions.fs
@@ -347,27 +347,3 @@ module Extensions =
     List.map2Shortest (+) [1;2;3] [2;3] |> areEqual [3;5]
     Array.map2Shortest (+) [|1;2|] [|2;3;4|] |> areEqual [|3;5|]
     ResizeArray.map2Shortest (+) (ResizeArray [1;2;3]) (ResizeArray [2;3]) |> areEqual (ResizeArray [3;5])
-    
-  [<Test>]
-  let chooseIndex () =
-    let someIfIndexEven i x =
-      if i % 2 = 0 then Some x
-      else None
-    List.chooseIndex someIfIndexEven [1;2;3;4;5] |> areEqual [1; 3; 5]
-    Array.chooseIndex someIfIndexEven [|1;2;3;4;5|] |> areEqual [|1; 3; 5|]
-    Seq.chooseIndex someIfIndexEven [1;2;3;4;5] |> areEqual (seq [1; 3; 5])
-    Map.chooseIndex someIfIndexEven (Map [1,2;2,3;3,4]) |> areEqual (Map [2,3])
-    Dictionary.chooseIndex someIfIndexEven (dict [1,"2"; 2,"4"; 4,"8"]) |> areEqual ((dict [2,"4"; 4,"8"]) :> IDictionary<_,_> |> Dictionary<int, string>)
-    Dict.chooseIndex someIfIndexEven (dict [1,"2"; 2,"4"; 4,"8"]) |> areEqual (dict [2,"4"; 4,"8"])
-
-  [<Test>]
-  let choosei () =
-    let someIfIndexEven i x =
-      if i % 2 = 0 then Some x
-      else None
-    (choosei someIfIndexEven [1;2;3;4;5]) |> areEqual [1;3;5]
-    (choosei someIfIndexEven [|1;2;3;4;5|]) |> areEqual [|1;3;5|]
-    (choosei someIfIndexEven (seq [1;2;3;4;5])) |> areEqual (seq [1;3;5])
-    (choosei someIfIndexEven (Map [1,2;2,3;3,4])) |> areEqual (Map [2,3])
-    (choosei someIfIndexEven (Dictionary<int, string> (dict [1,"2"; 2,"4"; 4,"8"])) |> areEqual ((dict [2,"4"; 4,"8"]) :> IDictionary<_,_> |> Dictionary<int, string>))
-    (choosei someIfIndexEven (dict [1,"2"; 2,"4"; 4,"8"]) |> areEqual (dict [2,"4"; 4,"8"]))
