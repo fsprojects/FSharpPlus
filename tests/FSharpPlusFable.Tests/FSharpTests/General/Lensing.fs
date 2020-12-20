4e5b86cd5c1e619d7b2cf8fec601c44f89d6a80d
Oskar Gewalli <wallymathieu@users.noreply.github.com>
2020-12-20 21:11:03 +0100

    Option.isNone doesn't seem to work on Fable

--- tests/FSharpPlusFable.Tests/FSharpTests/General/Lensing.fs
+++ tests/FSharpPlusFable.Tests/FSharpTests/General/Lensing.fs
@@ -16,8 +16,7 @@ module Book =
     let inline _authorName b = _author << Person._name <| b
 #endif
 let lensing = testList "Lensing" [
-    #if !FABLE_COMPILER || FABLE_COMPILER_3
-
+#if !FABLE_COMPILER || FABLE_COMPILER_3
     testCase "Lens" (fun () ->
                // equal (view _1 (1, '2')) 1
                // equal (view _2 ('1', 2)) 2
@@ -32,9 +31,13 @@ let lensing = testList "Lensing" [
                // equal (Some 1) (preview _Error (Error 1))
                // equal None (preview _Error (Ok 1))
                )
+    #if !FABLE_COMPILER
     testCase "prism1" (fun () -> equal true (Option.isNone (preview _Some None)))
+    #endif
     testCase "prism2" (fun () -> equal (Some 1) (preview _Some (Some 1)))
     testCase "prism3" (fun () -> equal (Some ()) (preview _None None))
+    #if !FABLE_COMPILER
     testCase "prism4" (fun () -> equal true (Option.isNone (preview _None (Some 1))))
     #endif
+#endif
 ]
