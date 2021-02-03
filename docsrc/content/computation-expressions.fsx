07a3cab488b7263d0bddbe74aea219242416b325
Gustavo Leon <1261319+gusty@users.noreply.github.com>
2021-02-03 19:58:22 +0100

    Open namspaces

--- docsrc/content/computation-expressions.fsx
+++ docsrc/content/computation-expressions.fsx
@@ -3,6 +3,10 @@
 // it to define helpers that you do not want to show in the documentation.
 #I "../../bin"
 
+#r @"../../src/FSharpPlus/bin/Release/net45/FSharpPlus.dll"
+open FSharpPlus
+open FSharpPlus.Data
+
 (**
 Computations Expressions
 ========================
