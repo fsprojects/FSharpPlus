5136030f7def662bfc57e38c3341184262d8dd02
Gustavo Leon <1261319+gusty@users.noreply.github.com>
2021-02-21 19:54:55 +0100

    fix previous

--- src/FSharpPlus/Control/Traversable.fs
+++ src/FSharpPlus/Control/Traversable.fs
@@ -81,7 +81,7 @@ type Traverse =
     static member inline Traverse (t: option<_>, f, [<Optional>]_output: 'R, [<Optional>]_impl: Traverse) : 'R = match t with Some x -> Map.Invoke Some (f x) | _ -> result None
 
     static member inline Traverse (t:Map<_,_>  , f, [<Optional>]_output: 'R, [<Optional>]_impl: Traverse) : 'R =
-        let insert_f k x ys = Map.Invoke (Map.add k) (f x) <*> ys	        let insert_f m k v = Map.Invoke (Map.add k) v <*> m
+        let insert_f k x ys = Map.Invoke (Map.add k) (f x) <*> ys
         Map.foldBack insert_f t (result Map.empty)
 
     static member inline Traverse (t: Result<'T,'Error>, f: 'T->'``Functor<'U>``, [<Optional>]_output: '``Functor<Result<'U,'Error>>``, [<Optional>]_impl: Traverse) : '``Functor<Result<'U,'Error>>`` =
