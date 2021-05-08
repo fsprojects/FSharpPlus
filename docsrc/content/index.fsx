4bbdafd720a04d248d426f621a583f4bb2f3a97b
Gauthier Segay <smoothdeveloper@users.noreply.github.com>
2021-05-08 09:12:44 +0200

    nuget: FSharpPlus (#435)

--- docsrc/content/index.fsx
+++ docsrc/content/index.fsx
@@ -2,6 +2,7 @@
 // This block of code is omitted in the generated HTML documentation. Use 
 // it to define helpers that you do not want to show in the documentation.
 #I "../../bin"
+#r @"../../src/FSharpPlus/bin/Release/net45/FSharpPlus.dll"
 
 (**
 FSharpPlus
@@ -52,7 +53,11 @@ Example 1
 This example demonstrates using an extension function defined in this library.
 
 *)
-#r @"../../src/FSharpPlus/bin/Release/net45/FSharpPlus.dll"
+(**
+```f#
+#r @"nuget: FSharpPlus"
+```
+*)
 open FSharpPlus
 
 let x = String.replace "old" "new" "Good old days"
