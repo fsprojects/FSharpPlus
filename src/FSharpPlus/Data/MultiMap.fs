36ccd56d88959cd7ca52e021ad420aa16a29b141
Gusty <1261319+gusty@users.noreply.github.com>
2021-06-03 09:25:02 +0200

    Initial MultiMap implementation

---
+++ src/FSharpPlus/Data/MultiMap.fs
@@ -0,0 +1,46 @@
+﻿namespace FSharpPlus.Data
+
+open System.ComponentModel
+open System.Collections.Generic
+open FSharpPlus
+
+/// A Map where keys are not unique.
+type MultiMap<'Key, 'Value when 'Key : comparison> = private MMap of Map<'Key, 'Value list> with
+
+    interface IEnumerable<KeyValuePair<'Key, 'Value>> with
+        member x.GetEnumerator () =
+            let (MMap x) = x
+            (x |> Map.toSeq |> Seq.collect (fun (k, v) -> v |> map (fun v -> KeyValuePair (k, v)))).GetEnumerator ()
+
+    interface System.Collections.IEnumerable with
+        member x.GetEnumerator () =
+            let (MMap x) = x
+            (x |> Map.toSeq |> Seq.collect (fun (k, v) -> v |> map (fun v -> KeyValuePair (k, v)))).GetEnumerator () :> System.Collections.IEnumerator
+
+    member x.Item
+        with get (key: 'Key) : 'Value list =
+            let (MMap x) = x
+            match Map.tryFind key x with
+            | Some values -> values
+            | None        -> []
+
+    static member get_Zero () = MMap (Map.ofList [])
+    static member (+) (MMap x, MMap y) = MMap (x ++ y)
+
+
+module MultiMap =
+
+    /// Converts a list of tuples to a MultiMap.
+    let ofList (x: list<'Key * 'Value>) = x |> Seq.groupBy fst |> Seq.map (fun (k, v) -> k, v |> Seq.map snd |> Seq.toList) |> Map.ofSeq |> MMap
+
+    /// Converts a MultiMap to a list of tuples.
+    let toList (MMap x) = x |> toList |> List.collect (fun x -> x.Value |> map (fun v -> (x.Key, v)))
+
+
+type MultiMap<'Key, 'Value when 'Key : comparison> with
+
+    [<EditorBrowsable(EditorBrowsableState.Never)>]
+    static member ToList x = MultiMap.toList x
+
+    [<EditorBrowsable(EditorBrowsableState.Never)>]
+    static member OfList x = MultiMap.ofList x
