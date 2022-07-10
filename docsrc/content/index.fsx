7c17bb9b1711035575ebd868296a7cdac922e466
Oskar Gewalli <wallymathieu@users.noreply.github.com>
2022-07-10 12:19:18 +0300

    - ../../bin

--- docsrc/content/index.fsx
+++ docsrc/content/index.fsx
@@ -1,7 +1,6 @@
 (*** hide ***)
 // This block of code is omitted in the generated HTML documentation. Use 
 // it to define helpers that you do not want to show in the documentation.
-#I "../../bin"
 #r @"../../src/FSharpPlus/bin/Release/net45/FSharpPlus.dll"
 
 (**
