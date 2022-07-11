1d53098e27fe1e307ac88cf48daca240e4e70c62
Oskar Gewalli <88096+wallymathieu@users.noreply.github.com>
2022-07-11 08:27:56 +0300

    Build docs on GitHub action instead of in AppVeyor

--- docsrc/content/abstraction-bifoldable.fsx
+++ docsrc/content/abstraction-bifoldable.fsx
@@ -1,7 +1,6 @@
 (*** hide ***)
 // This block of code is omitted in the generated HTML documentation. Use 
 // it to define helpers that you do not want to show in the documentation.
-#I "../../bin"
 
 (**
 Bifoldable
