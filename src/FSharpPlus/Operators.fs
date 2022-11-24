75e9b8acbd418f5e7d15196f15d4969310e6bbe6
rodriguestiago0 <tiagomcrodrigues@gmail.com>
2022-11-24 22:22:39 +0000

    Update src/FSharpPlus/Operators.fs

--- src/FSharpPlus/Operators.fs
+++ src/FSharpPlus/Operators.fs
@@ -140,7 +140,7 @@ module Operators =
     /// <category index="0">Common Combinators</category>
     let inline valueTuple5<'T1, 'T2, 'T3, 'T4, 'T5> (t1: 'T1) (t2: 'T2) (t3: 'T3) (t4: 'T4) (t5: 'T5) = ValueTuple.Create t1, t2, t3, t4, t5
 
-    /// Tuple six arguments
+    /// ValueTuple six arguments
     /// <category index="0">Common Combinators</category>
     let inline valueTuple6<'T1, 'T2, 'T3, 'T4, 'T5, 'T6> (t1: 'T1) (t2: 'T2) (t3: 'T3) (t4: 'T4) (t5: 'T5) (t6: 'T6) = ValueTuple.Create t1, t2, t3, t4, t5, t6
 
