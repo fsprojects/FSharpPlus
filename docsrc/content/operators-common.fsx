5aba4961dec70163692b4592958e2e8eae298da8
Tiago Rodrigues <tiagomcrodrigues@gmail.com>
2022-11-24 17:05:39 +0000

    Remove code

--- docsrc/content/operators-common.fsx
+++ docsrc/content/operators-common.fsx
@@ -139,22 +139,6 @@ let inline tuple7 a b c d e f g   = a,b,c,d,e,f,g
 let inline tuple8 a b c d e f g h = a,b,c,d,e,f,g,h
 
 (**
-
-## valueTuple2, valueTuple2, ...valueTuple2
-
-Functions that generate a valueTuple. The number indicates the number of arguments
-that are defined, and the corresponding size of valueTuple.
-
-*)
-let inline valueTuple2 a b             = ValueTuple.Create a,b
-let inline valueTuple2 a b c           = ValueTuple.Create a,b,c
-let inline valueTuple2 a b c d         = ValueTuple.Create a,b,c,d
-let inline valueTuple2 a b c d e       = ValueTuple.Create a,b,c,d,e
-let inline valueTuple2 a b c d e f     = ValueTuple.Create a,b,c,d,e,f
-let inline valueTuple2 a b c d e f g   = ValueTuple.Create a,b,c,d,e,f,g
-let inline valueTuple2 a b c d e f g h = ValueTuple.Create a,b,c,d,e,f,g,h
-
-(**
 ## Explicit
 
 Explicit allows you to use `explicit` generic method for standard types and types that implement the static explicit type cast signature.
