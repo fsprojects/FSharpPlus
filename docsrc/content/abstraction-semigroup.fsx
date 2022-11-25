f20398d088e545970350a0511e624ee2c923d6b7
Tiago Rodrigues <tiagomcrodrigues@gmail.com>
2022-11-25 16:45:06 +0000

    Update documentation

--- docsrc/content/abstraction-semigroup.fsx
+++ docsrc/content/abstraction-semigroup.fsx
@@ -42,7 +42,7 @@ From .Net/F#
  -  ``Map<'T,'U>``
  -  ``TimeSpan`` 
  -  ``Tuple<*>``
- -  ``ValueTuple<*> (* up to 8 elements)``
+ -  ``ValueTuple<*> ( * up to 7 elements)``
  -  ``'T1* ... *'Tn``
  -  ``Task<'T>``
  -  ``'T->'Semigroup``
