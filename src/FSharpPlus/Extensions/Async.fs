2091c9e366aeb1ae4c684ea4657cb7654b70dc54
gusty <1261319+gusty@users.noreply.github.com>
2024-02-10 12:07:15 +0100

    Proper Async.zip implementation

--- src/FSharpPlus/Extensions/Async.fs
+++ src/FSharpPlus/Extensions/Async.fs
@@ -4,6 +4,23 @@ namespace FSharpPlus
 [<RequireQualifiedAccess>]
 module Async =
 
+    open System
+    open System.Threading.Tasks
+    
+    #if !FABLE_COMPILER
+    // Proper Async.StartImmediateAsTask implementation, without aggregate exception wrapping.
+    let private startImmediateAsTask (computation: Async<'T>, cancellationToken) : Task<'T> =        
+        let ts = TaskCompletionSource<'T> ()
+        Async.StartWithContinuations (
+            computation,
+            ts.SetResult,
+            (function
+                | :? AggregateException as agg -> ts.SetException agg.InnerExceptions
+                | exn -> ts.SetException exn),
+            (fun _ -> ts.SetCanceled ()),
+            cancellationToken)
+        ts.Task
+    #endif
     open FSharpPlus.Extensions
 
     /// <summary>Creates an async workflow from another workflow 'x', mapping its result with 'f'.</summary>
@@ -31,62 +48,61 @@ module Async =
         let! c = z
         return f a b c}
 
-    /// <summary>Creates an async workflow from two workflows 'x' and 'y', mapping its results with 'f'.</summary>
-    /// <remarks>Similar to lift2 but although workflows are started in sequence they might end independently in different order.</remarks>
-    /// <param name="f">The mapping function.</param>
-    /// <param name="x">First async workflow.</param>
-    /// <param name="y">Second async workflow.</param>
+    /// <summary>Creates an async workflow from two workflows, mapping its results with a specified function.</summary>
+    /// <remarks>Similar to lift2 but although workflows are started in sequence they might end independently in different order
+    /// and all errors are collected.
+    /// </remarks>
+    /// <param name="mapper">The mapping function.</param>
+    /// <param name="async1">First async workflow.</param>
+    /// <param name="async2">Second async workflow.</param>
     #if FABLE_COMPILER
-    let map2 f x y = lift2 f x y
+    let map2 mapper (async1: Async<'T1>) (async2: Async<'T2>) : Async<'U> =
+        lift2 mapper async1 async2
     #else
-    let map2 f x y = async {
+    let map2 mapper (async1: Async<'T1>) (async2: Async<'T2>) : Async<'U> = async {
         let! ct = Async.CancellationToken
-        let x = Async.StartImmediateAsTask (x, ct)
-        let y = Async.StartImmediateAsTask (y, ct)
-        let! x' = Async.Await x
-        let! y' = Async.Await y
-        return f x' y' }
+        let t1 = startImmediateAsTask (async1, ct)
+        let t2 = startImmediateAsTask (async2, ct)
+        return! Async.Await (Task.map2 mapper t1 t2) }
     #endif
 
-    /// <summary>Creates an async workflow from three workflows 'x', 'y' and 'z', mapping its results with 'f'.</summary>
-    /// <remarks>Similar to lift3 but although workflows are started in sequence they might end independently in different order.</remarks>
-    /// <param name="f">The mapping function.</param>
-    /// <param name="x">First async workflow.</param>
-    /// <param name="y">Second async workflow.</param>
-    /// <param name="z">third async workflow.</param>
+    /// <summary>Creates an async workflow from three workflows, mapping its results with a specified function.</summary>
+    /// <remarks>Similar to lift3 but although workflows are started in sequence they might end independently in different order
+    /// and all errors are collected.
+    /// </remarks>
+    /// <param name="mapper">The mapping function.</param>
+    /// <param name="async1">First async workflow.</param>
+    /// <param name="async2">Second async workflow.</param>
+    /// <param name="async3">third async workflow.</param>
     #if FABLE_COMPILER
-    let map3 f x y z = lift3 f x y z
+    let map3 mapper (async1: Async<'T1>) (async2: Async<'T2>) (async3: Async<'T3>) : Async<'U> =
+        lift3 mapper async1 async2 async3
     #else
-    let map3 f x y z = async {
+    let map3 mapper (async1: Async<'T1>) (async2: Async<'T2>) (async3: Async<'T3>) : Async<'U> = async {
         let! ct = Async.CancellationToken
-        let x = Async.StartImmediateAsTask (x, ct)
-        let y = Async.StartImmediateAsTask (y, ct)
-        let z = Async.StartImmediateAsTask (z, ct)
-        let! x' = Async.Await x
-        let! y' = Async.Await y
-        let! z' = Async.Await z
-        return f x' y' z' }
+        let t1 = startImmediateAsTask (async1, ct)
+        let t2 = startImmediateAsTask (async2, ct)
+        let t3 = startImmediateAsTask (async3, ct)
+        return! Async.Await (Task.map3 mapper t1 t2 t3) }
     #endif
 
     /// <summary>Creates an async workflow from two workflows 'x' and 'y', tupling its results.</summary>
     let zipSequentially x y = async {
         let! a = x
         let! b = y
-        return a, b}
+        return a, b }
 
-    /// <summary>Creates an async workflow from two workflows 'x' and 'y', tupling its results.</summary>
-    /// <remarks>Similar to zipSequentially but although workflows are started in sequence they might end independently in different order.</remarks>
-    #if FABLE_COMPILER
-    let zip x y = zipSequentially x y
-    #else
-    let zip x y = async {
-        let! ct = Async.CancellationToken
-        let x = Async.StartImmediateAsTask (x, ct)
-        let y = Async.StartImmediateAsTask (y, ct)
-        let! x' = Async.Await x
-        let! y' = Async.Await y
-        return x', y' }
-    #endif
+    /// <summary>Creates an async workflow from two workflows, tupling its results.</summary>
+    /// <remarks>Similar to zipSequentially but although workflows are started in sequence they might end independently in different order
+    /// and all errors are collected.
+    /// </remarks>
+    let zip (async1: Async<'T1>) (async2: Async<'T2>) = map2 (fun x y -> x, y) async1 async2
+
+    /// <summary>Creates an async workflow from three workflows, tupling its results.</summary>
+    /// <remarks>Similar to zipSequentially but although workflows are started in sequence they might end independently in different order
+    /// and all errors are collected.
+    /// </remarks>
+    let zip3 (async1: Async<'T1>) (async2: Async<'T2>) (async3: Async<'T3>) = map3 (fun x y z -> x, y, z) async1 async2 async3
 
     /// Flatten two nested asyncs into one.
     let join x = async.Bind (x, id)
---
+++ tests/FSharpPlus.Tests/Asyncs.fs
@@ -0,0 +1,112 @@
+﻿namespace FSharpPlus.Tests
+
+#if !NET462 && !NETSTANDARD2_0
+
+module Async =
+
+    open System
+    open System.Threading
+    open System.Threading.Tasks
+    open NUnit.Framework
+    open FSharpPlus
+    open FSharpPlus.Data
+    open FSharpPlus.Extensions
+    open FSharpPlus.Tests.Helpers
+    
+    exception TestException of string
+    
+    type Async with
+        static member StartImmediateAsTaskCorrect (computation: Async<'T>, ?cancellationToken) : Task<'T> =
+            let cancellationToken = defaultArg cancellationToken (new CancellationToken ())
+            let ts = TaskCompletionSource<'T> ()
+            Async.StartWithContinuations (
+                computation,
+                ts.SetResult,
+                (function
+                    | :? AggregateException as agg -> ts.SetException agg.InnerExceptions
+                    | exn -> ts.SetException exn),
+                (fun _ -> ts.SetCanceled ()),
+                cancellationToken)
+            ts.Task
+
+        static member AsTaskAndWait computation =
+            let t = Async.StartImmediateAsTaskCorrect computation
+            Task.WaitAny t |> ignore
+            t
+
+        static member WhenAll (source: Async<'T> seq) = source |> Seq.map (fun x -> Async.StartImmediateAsTaskCorrect x) |> Task.WhenAll |> Async.Await
+
+
+    module AsyncTests =
+
+        let createAsync isFailed delay (value: 'T) =
+            if not isFailed && delay = 0 then async.Return value
+            else
+                async {
+                    if delay = 0 then do! Async.raise (TestException (sprintf "Ouch, can't create: %A" value ))
+                    do! Async.Sleep delay
+                    if isFailed then do! Async.raise (TestException (sprintf "Ouch, can't create: %A" value ))
+                    return value }
+
+
+        [<Test>]
+        let testAsyncZip () =
+            let t1 = createAsync true 0 1
+            let t2 = createAsync true 0 2
+            let t3 = createAsync true 0 3
+
+            let c = new CancellationToken true
+            let t4 = Task.FromCanceled<int> c |> Async.Await
+
+            let t5 = createAsync false 0 5
+            let t6 = createAsync false 0 6
+
+            let t12 =    Async.WhenAll [t1; t2]
+            let t12t12 = Async.WhenAll [t12; t12]
+            let t33    = Async.WhenAll [t3; t3]
+
+            let t12123 = Async.zip3 t12t12 t33 t4
+            let ac1 =
+                try
+                    (t12123 |> Async.AsTaskAndWait).Exception.InnerExceptions |> Seq.map (fun x -> int (Char.GetNumericValue x.Message.[35]))
+                with e ->
+                    failwithf "Failure in testAsyncZip. Async status is %A . Exception is %A" (t12123 |> Async.AsTaskAndWait).Status e
+
+            CollectionAssert.AreEquivalent ([1; 2; 1; 2; 3], ac1, "Async.zip(3) should add only non already existing exceptions.")
+
+            let t13 = Async.zip3 (Async.zip t1 t3) t4 (Async.zip t5 t6)
+            Assert.AreEqual (true, (t13 |> Async.AsTaskAndWait).IsFaulted, "Async.zip(3) between a value, an exception and a cancellation -> exception wins.")
+            let ac2 = (t13 |> Async.AsTaskAndWait).Exception.InnerExceptions |> Seq.map (fun x -> int (Char.GetNumericValue x.Message.[35]))
+            CollectionAssert.AreEquivalent ([1; 3], ac2, "Async.zip between 2 exceptions => both exceptions returned, even after combining with cancellation and values.")
+
+        [<Test>]
+        let testAsyncZipAsync () =
+            let t1 = createAsync true 20 1
+            let t2 = createAsync true 10 2
+            let t3 = createAsync true 30 3
+
+            let c = new CancellationToken true
+            let t4 = Task.FromCanceled<int> c |> Async.Await
+
+            let t5 = createAsync false 20 5
+            let t6 = createAsync false 10 6
+
+            let t12 =    Async.WhenAll [t1; t2]
+            let t12t12 = Async.WhenAll [t12; t12]
+            let t33    = Async.WhenAll [t3; t3]
+
+            let t12123 = Async.zip3 t12t12 t33 t4            
+            let ac1 =
+                try
+                    (t12123 |> Async.AsTaskAndWait).Exception.InnerExceptions |> Seq.map (fun x -> int (Char.GetNumericValue x.Message.[35]))
+                with e ->
+                    failwithf "Failure in testAsyncZipAsync. Async status is %A . Exception is %A" (t12123  |> Async.AsTaskAndWait).Status e
+
+            CollectionAssert.AreEquivalent ([1; 2; 1; 2; 3], ac1, "Async.zip(3)Async should add only non already existing exceptions.")
+
+            let t13 = Async.zip3 (Async.zip t1 t3) t4 (Async.zip t5 t6)
+            Assert.AreEqual (true, (t13 |> Async.AsTaskAndWait).IsFaulted, "Async.zip(3)Async between a value, an exception and a cancellation -> exception wins.")
+            let ac2 = (t13 |> Async.AsTaskAndWait).Exception.InnerExceptions |> Seq.map (fun x -> int (Char.GetNumericValue x.Message.[35]))
+            CollectionAssert.AreEquivalent ([1; 3], ac2, "Async.zipAsync between 2 exceptions => both exceptions returned, even after combining with cancellation and values.")
+     
+#endif
