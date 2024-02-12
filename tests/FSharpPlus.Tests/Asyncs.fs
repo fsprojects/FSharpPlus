1fdbf734872f92e7fe5d2243b2fd7f68bcc12c0c
gusty <1261319+gusty@users.noreply.github.com>
2024-02-12 10:57:53 +0100

    + Tests for Async

--- tests/FSharpPlus.Tests/Asyncs.fs
+++ tests/FSharpPlus.Tests/Asyncs.fs
@@ -96,4 +96,16 @@ module Async =
             let ac2 = (t13 |> Async.AsTaskAndWait).Exception.InnerExceptions |> Seq.map (fun x -> int (Char.GetNumericValue x.Message.[35]))
             CollectionAssert.AreEquivalent ([1; 3], ac2, "Async.zipAsync between 2 exceptions => both exceptions returned, even after combining with cancellation and values.")
      
+        [<Test>]
+        let testAsyncTraversals =
+            let t1 = createAsync true  20 1
+            let t2 = createAsync true  10 2
+            let t3 = createAsync false 30 3
+
+            let t123 = Async.map3 (fun x y z -> [x; y; z]) t1 t2 t3
+            let t123' = transpose [t1; t2; t3]
+            let t123'' = sequence [t1; t2; t3]
+            CollectionAssert.AreEquivalent ((Async.AsTaskAndWait t123).Exception.InnerExceptions, (Async.AsTaskAndWait t123').Exception.InnerExceptions, "Async.map3 (fun x y z -> [x; y; z]) t1 t2 t3 is the same as transpose [t1; t2; t3]")
+            CollectionAssert.AreNotEquivalent ((Async.AsTaskAndWait t123).Exception.InnerExceptions, (Async.AsTaskAndWait t123'').Exception.InnerExceptions, "Async.map3 (fun x y z -> [x; y; z]) t1 t2 t3 is not the same as sequence [t1; t2; t3]")
+
 #endif
