dbd5e1d608b5b1516e66ec8081b1f7db57a2c981
Devin Lyons <dlyons@mtasolutions.com>
2024-10-16 13:44:11 -0800

    Add NonEmptyList helpers to match List.

--- tests/FSharpPlus.Tests/Data.fs
+++ tests/FSharpPlus.Tests/Data.fs
@@ -395,7 +395,487 @@ module NonEmptyList =
     let ``mapi on non empty list should equal mapi on list`` () =
         let mapOp a b = sprintf "%d-%d" a b
         fsCheck "list of int" (Prop.forAll (Arb.fromGen NonEmptyListIntOfSeqGen) (fun (q : NonEmptyList<int>, l) -> (q |> NonEmptyList.mapi mapOp |> NonEmptyList.toList) = (l |> List.mapi mapOp)))
+        
+    [<Test>]
+    let ``append works`` () =
+        let list1 = NonEmptyList.create 1 [2; 3]
+        let list2 = NonEmptyList.create 4 [5; 6]
+        let result = NonEmptyList.append list1 list2
+        Assert.AreEqual(NonEmptyList.create 1 [2; 3; 4; 5; 6], result)
+    
+    [<Test>]
+    let ``choose works`` () =
+        let list = NonEmptyList.create 1 [2; 3; 4]
+        let result = NonEmptyList.choose (fun x -> if x % 2 = 0 then Some (x * 2) else None) list
+        Assert.AreEqual(NonEmptyList.create 4 [8], result)
+    
+    [<Test>]
+    let ``collect works`` () =
+        let list = NonEmptyList.create 1 [2; 3]
+        let result = NonEmptyList.collect (fun x -> NonEmptyList.create x [x * 2]) list
+        Assert.AreEqual(NonEmptyList.create 1 [2; 2; 4; 3; 6], result)
+    
+    [<Test>]
+    let ``concat works`` () =
+        let lists = NonEmptyList.create (NonEmptyList.create 1 [2]) [NonEmptyList.create 3 [4]; NonEmptyList.create 5 [6]]
+        let result = NonEmptyList.concat lists
+        Assert.AreEqual(NonEmptyList.create 1 [2; 3; 4; 5; 6], result)
+    
+    [<Test>]
+    let ``contains works`` () =
+        let list = NonEmptyList.create 1 [2; 3; 4]
+        let result = NonEmptyList.contains 3 list
+        Assert.IsTrue(result)
+    
+    [<Test>]
+    let ``distinct works`` () =
+        fsCheck "list of int" (Prop.forAll (Arb.fromGen NonEmptyListIntOfSeqGen) (fun (q, l) -> 
+            (q |> NonEmptyList.distinct |> NonEmptyList.toList) = (l |> List.distinct)))
+    
+    [<Test>]
+    let ``exists works`` () =
+        let list = NonEmptyList.create 1 [2; 3; 4]
+        let result = NonEmptyList.exists (fun x -> x = 3) list
+        Assert.IsTrue(result)
+    
+    [<Test>]
+    let ``filter works`` () =
+        let list = NonEmptyList.create 1 [2; 3; 4]
+        let result = NonEmptyList.filter (fun x -> x % 2 = 0) list
+        Assert.AreEqual(NonEmptyList.create 2 [4], result)
+    
+    [<Test>]
+    let ``find works`` () =
+        let list = NonEmptyList.create 1 [2; 3; 4]
+        let result = NonEmptyList.find (fun x -> x = 3) list
+        Assert.AreEqual(3, result)
+    
+    [<Test>]
+    let ``findIndex works`` () =
+        let list = NonEmptyList.create 1 [2; 3; 4]
+        let result = NonEmptyList.findIndex (fun x -> x = 3) list
+        Assert.AreEqual(2, result)
+    
+    [<Test>]
+    let ``fold works`` () =
+        let list = NonEmptyList.create 1 [2; 3; 4]
+        let result = NonEmptyList.fold (+) 0 list
+        Assert.AreEqual(10, result)
+    
+    [<Test>]
+    let ``foldBack works`` () =
+        let list = NonEmptyList.create 1 [2; 3; 4]
+        let result = NonEmptyList.foldBack (+) list 0
+        Assert.AreEqual(10, result)
+    
+    [<Test>]
+    let ``forall works`` () =
+        let list = NonEmptyList.create 1 [2; 3; 4]
+        let result = NonEmptyList.forall (fun x -> x > 0) list
+        Assert.IsTrue(result)
+    
+    [<Test>]
+    let ``groupBy works`` () =
+        let list = NonEmptyList.create 1 [2; 3; 4; 5]
+        let result = NonEmptyList.groupBy (fun x -> x % 2) list
+        let expected = NonEmptyList.create (1, NonEmptyList.create 1 [3; 5]) [(0, NonEmptyList.create 2 [4])]
+        Assert.AreEqual(expected, result)
 
+    [<Test>]
+    let ``indexed works`` () =
+        let list = NonEmptyList.create 1 [2; 3; 4]
+        let result = NonEmptyList.indexed list
+        Assert.AreEqual(NonEmptyList.create (0, 1) [(1, 2); (2, 3); (3, 4)], result)
+    
+    [<Test>]
+    let ``init works`` () =
+        let result = NonEmptyList.init 4 (fun i -> i * 2)
+        Assert.AreEqual(NonEmptyList.create 0 [2; 4; 6], result)
+    
+    [<Test>]
+    let ``insertAt works`` () =
+        let list = NonEmptyList.create 1 [2; 3; 4]
+        let result = NonEmptyList.insertAt 2 99 list
+        Assert.AreEqual(NonEmptyList.create 1 [2; 99; 3; 4], result)
+    
+    [<Test>]
+    let ``insertManyAt works`` () =
+        let list = NonEmptyList.create 1 [2; 3; 4]
+        let result = NonEmptyList.insertManyAt 2 [99; 100] list
+        Assert.AreEqual(NonEmptyList.create 1 [2; 99; 100; 3; 4], result)
+    
+    [<Test>]
+    let ``item works`` () =
+        let list = NonEmptyList.create 1 [2; 3; 4]
+        let result = NonEmptyList.item 2 list
+        Assert.AreEqual(3, result)
+    
+    [<Test>]
+    let ``iter works`` () =
+        let list = NonEmptyList.create 1 [2; 3; 4]
+        let mutable sum = 0
+        NonEmptyList.iter (fun x -> sum <- sum + x) list
+        Assert.AreEqual(10, sum)
+    
+    [<Test>]
+    let ``iter2 works`` () =
+        let list1 = NonEmptyList.create 1 [2; 3]
+        let list2 = NonEmptyList.create 4 [5; 6]
+        let mutable sum = 0
+        NonEmptyList.iter2 (fun x y -> sum <- sum + x + y) list1 list2
+        Assert.AreEqual(21, sum)
+    
+    [<Test>]
+    let ``iteri works`` () =
+        let list = NonEmptyList.create 1 [2; 3; 4]
+        let mutable sum = 0
+        NonEmptyList.iteri (fun i x -> sum <- sum + i + x) list
+        Assert.AreEqual(16, sum)
+    
+    [<Test>]
+    let ``iteri2 works`` () =
+        let list1 = NonEmptyList.create 1 [2; 3]
+        let list2 = NonEmptyList.create 4 [5; 6]
+        let mutable sum = 0
+        NonEmptyList.iteri2 (fun i x y -> sum <- sum + i + x + y) list1 list2
+        Assert.AreEqual(24, sum)
+    
+    [<Test>]
+    let ``last works`` () =
+        let list = NonEmptyList.create 1 [2; 3; 4]
+        let result = NonEmptyList.last list
+        Assert.AreEqual(4, result)
+    
+    [<Test>]
+    let ``map2 works`` () =
+        let list1 = NonEmptyList.create 1 [2; 3]
+        let list2 = NonEmptyList.create 4 [5; 6]
+        let result = NonEmptyList.map2 (fun x y -> x + y) list1 list2
+        Assert.AreEqual(NonEmptyList.create 5 [7; 9], result)
+    
+    [<Test>]
+    let ``map3 works`` () =
+        let list1 = NonEmptyList.create 1 [2; 3]
+        let list2 = NonEmptyList.create 4 [5; 6]
+        let list3 = NonEmptyList.create 7 [8; 9]
+        let result = NonEmptyList.map3 (fun x y z -> x + y + z) list1 list2 list3
+        Assert.AreEqual(NonEmptyList.create 12 [15; 18], result)
+    
+    [<Test>]
+    let ``mapFold works`` () =
+        let list = NonEmptyList.create 1 [2; 3]
+        let result, state = NonEmptyList.mapFold (fun acc x -> (x * 2, acc + x)) 0 list
+        Assert.AreEqual(NonEmptyList.create 2 [4; 6], result)
+        Assert.AreEqual(6, state)
+    
+    [<Test>]
+    let ``mapFoldBack works`` () =
+        let list = NonEmptyList.create 1 [2; 3]
+        let result, state = NonEmptyList.mapFoldBack (fun x acc -> (x * 2, acc + x)) list 0
+        Assert.AreEqual(NonEmptyList.create 2 [4; 6], result)
+        Assert.AreEqual(6, state)
+    
+    [<Test>]
+    let ``mapi works`` () =
+        let list = NonEmptyList.create 1 [2; 3]
+        let result = NonEmptyList.mapi (fun i x -> i + x) list
+        Assert.AreEqual(NonEmptyList.create 1 [3; 5], result)
+    
+    [<Test>]
+    let ``mapi2 works`` () =
+        let list1 = NonEmptyList.create 1 [2; 3]
+        let list2 = NonEmptyList.create 4 [5; 6]
+        let result = NonEmptyList.mapi2 (fun i x y -> i + x + y) list1 list2
+        Assert.AreEqual(NonEmptyList.create 5 [8; 11], result)
+    
+    [<Test>]
+    let ``max works`` () =
+        let list = NonEmptyList.create 1 [2; 3; 4]
+        let result = NonEmptyList.max list
+        Assert.AreEqual(4, result)
+    
+    [<Test>]
+    let ``maxBy works`` () =
+        let list = NonEmptyList.create 1 [2; 3; 4]
+        let result = NonEmptyList.maxBy (fun x -> -x) list
+        Assert.AreEqual(1, result)
+    
+    [<Test>]
+    let ``min works`` () =
+        let list = NonEmptyList.create 1 [2; 3; 4]
+        let result = NonEmptyList.min list
+        Assert.AreEqual(1, result)
+    
+    [<Test>]
+    let ``minBy works`` () =
+        let list = NonEmptyList.create 1 [2; 3; 4]
+        let result = NonEmptyList.minBy (fun x -> -x) list
+        Assert.AreEqual(4, result)
+    
+    [<Test>]
+    let ``pairwise works`` () =
+        let list = NonEmptyList.create 1 [2; 3; 4]
+        let result = NonEmptyList.pairwise list
+        Assert.AreEqual(NonEmptyList.create (1, 2) [(2, 3); (3, 4)], result)
+    
+    [<Test>]
+    let ``partition works`` () =
+        let list = NonEmptyList.create 1 [2; 3; 4]
+        let result1, result2 = NonEmptyList.partition (fun x -> x % 2 = 0) list
+        Assert.AreEqual(NonEmptyList.create 2 [4], result1)
+        Assert.AreEqual(NonEmptyList.create 1 [3], result2)
+    
+    [<Test>]
+    let ``permute works`` () =
+        let list = NonEmptyList.create 1 [2; 3; 4]
+        let result = NonEmptyList.permute (fun i -> (i + 1) % 4) list
+        Assert.AreEqual(NonEmptyList.create 4 [1; 2; 3], result)
+    
+    [<Test>]
+    let ``pick works`` () =
+        let list = NonEmptyList.create 1 [2; 3; 4]
+        let result = NonEmptyList.pick (fun x -> if x % 2 = 0 then Some (x * 2) else None) list
+        Assert.AreEqual(4, result)
+    
+    [<Test>]
+    let ``removeAt works`` () =
+        let list = NonEmptyList.create 1 [2; 3; 4]
+        let result = NonEmptyList.removeAt 2 list
+        Assert.AreEqual(NonEmptyList.create 1 [2; 4], result)
+    
+    [<Test>]
+    let ``removeManyAt works`` () =
+        let list = NonEmptyList.create 1 [2; 3; 4]
+        let result = NonEmptyList.removeManyAt 1 2 list
+        Assert.AreEqual(NonEmptyList.create 1 [4], result)
+    
+    [<Test>]
+    let ``replicate works`` () =
+        let result = NonEmptyList.replicate 4 99
+        Assert.AreEqual(NonEmptyList.create 99 [99; 99; 99], result)
+    
+    [<Test>]
+    let ``rev works`` () =
+        let list = NonEmptyList.create 1 [2; 3; 4]
+        let result = NonEmptyList.rev list
+        Assert.AreEqual(NonEmptyList.create 4 [3; 2; 1], result)
+    
+    [<Test>]
+    let ``scan works`` () =
+        let list = NonEmptyList.create 1 [2; 3; 4]
+        let result = NonEmptyList.scan (+) 0 list
+        Assert.AreEqual(NonEmptyList.create 0 [1; 3; 6; 10], result)
+    
+    [<Test>]
+    let ``scanBack works`` () =
+        let list = NonEmptyList.create 1 [2; 3; 4]
+        let result = NonEmptyList.scanBack (+) list 0
+        Assert.AreEqual(NonEmptyList.create 10 [9; 7; 4; 0], result)
+    
+    [<Test>]
+    let ``singleton works`` () =
+        let result = NonEmptyList.singleton 99
+        Assert.AreEqual(NonEmptyList.create 99 [], result)
+    
+    [<Test>]
+    let ``skip works`` () =
+        let list = NonEmptyList.create 1 [2; 3; 4]
+        let result = NonEmptyList.skip 2 list
+        Assert.AreEqual(NonEmptyList.create 3 [4], result)
+    
+    [<Test>]
+    let ``skipWhile works`` () =
+        let list = NonEmptyList.create 1 [2; 3; 4]
+        let result = NonEmptyList.skipWhile (fun x -> x < 3) list
+        Assert.AreEqual(NonEmptyList.create 3 [4], result)
+    
+    [<Test>]
+    let ``sort works`` () =
+        let list = NonEmptyList.create 3 [1; 4; 2]
+        let result = NonEmptyList.sort list
+        Assert.AreEqual(NonEmptyList.create 1 [2; 3; 4], result)
+    
+    [<Test>]
+    let ``sortBy works`` () =
+        let list = NonEmptyList.create 3 [1; 4; 2]
+        let result = NonEmptyList.sortBy (fun x -> -x) list
+        Assert.AreEqual(NonEmptyList.create 4 [3; 2; 1], result)
+    
+    [<Test>]
+    let ``sortByDescending works`` () =
+        let list = NonEmptyList.create 3 [1; 4; 2]
+        let result = NonEmptyList.sortByDescending id list
+        Assert.AreEqual(NonEmptyList.create 4 [3; 2; 1], result)
+    
+    [<Test>]
+    let ``sortDescending works`` () =
+        let list = NonEmptyList.create 3 [1; 4; 2]
+        let result = NonEmptyList.sortDescending list
+        Assert.AreEqual(NonEmptyList.create 4 [3; 2; 1], result)
+    
+    [<Test>]
+    let ``sortWith works`` () =
+        let list = NonEmptyList.create 3 [1; 4; 2]
+        let result = NonEmptyList.sortWith compare list
+        Assert.AreEqual(NonEmptyList.create 1 [2; 3; 4], result)
+    
+    [<Test>]
+    let ``splitAt works`` () =
+        let list = NonEmptyList.create 1 [2; 3; 4]
+        let result1, result2 = NonEmptyList.splitAt 2 list
+        Assert.AreEqual(NonEmptyList.create 1 [2], result1)
+        Assert.AreEqual(NonEmptyList.create 3 [4], result2)
+    
+    [<Test>]
+    let ``splitInto works`` () =
+        let list = NonEmptyList.create 1 [2; 3; 4; 5]
+        let result = NonEmptyList.splitInto 2 list
+        let expected = NonEmptyList.create (NonEmptyList.create 1 [2; 3]) [NonEmptyList.create 4 [5]]
+        shoulSeqEqual expected result
+    
+    [<Test>]
+    let ``sum works`` () =
+        let list = NonEmptyList.create 1 [2; 3; 4]
+        let result = NonEmptyList.sum list
+        Assert.AreEqual(10, result)
+    
+    [<Test>]
+    let ``sumBy works`` () =
+        let list = NonEmptyList.create 1 [2; 3; 4]
+        let result = NonEmptyList.sumBy (fun x -> x * 2) list
+        Assert.AreEqual(20, result)
+    
+    [<Test>]
+    let ``tail works`` () =
+        let list = NonEmptyList.create 1 [2; 3; 4]
+        let result = NonEmptyList.tail list
+        Assert.AreEqual(NonEmptyList.create 2 [3; 4], result)
+    
+    [<Test>]
+    let ``take works`` () =
+        let list = NonEmptyList.create 1 [2; 3; 4]
+        let result = NonEmptyList.take 2 list
+        Assert.AreEqual(NonEmptyList.create 1 [2], result)
+    
+    [<Test>]
+    let ``takeWhile works`` () =
+        let list = NonEmptyList.create 1 [2; 3; 4]
+        let result = NonEmptyList.takeWhile (fun x -> x < 3) list
+        Assert.AreEqual(NonEmptyList.create 1 [2], result)
+    
+    [<Test>]
+    let ``transpose works`` () =
+        let list = NonEmptyList.create (NonEmptyList.create 1 [2]) [NonEmptyList.create 3 [4]]
+        let result = NonEmptyList.transpose list
+        Assert.AreEqual(NonEmptyList.create (NonEmptyList.create 1 [3]) [NonEmptyList.create 2 [4]], result)
+    
+    [<Test>]
+    let ``truncate works`` () =
+        let list = NonEmptyList.create 1 [2; 3; 4]
+        let result = NonEmptyList.truncate 2 list
+        Assert.AreEqual(NonEmptyList.create 1 [2], result)
+    
+    [<Test>]
+    let ``tryExactlyOne works`` () =
+        let list = NonEmptyList.create 1 []
+        let result = NonEmptyList.tryExactlyOne list
+        Assert.AreEqual(Some 1, result)
+    
+    [<Test>]
+    let ``tryFind works`` () =
+        let list = NonEmptyList.create 1 [2; 3; 4]
+        let result = NonEmptyList.tryFind (fun x -> x % 2 = 0) list
+        Assert.AreEqual(Some 2, result)
+    
+    [<Test>]
+    let ``tryFindBack works`` () =
+        let list = NonEmptyList.create 1 [2; 3; 4]
+        let result = NonEmptyList.tryFindBack (fun x -> x % 2 = 0) list
+        Assert.AreEqual(Some 4, result)
+    
+    [<Test>]
+    let ``tryFindIndex works`` () =
+        let list = NonEmptyList.create 1 [2; 3; 4]
+        let result = NonEmptyList.tryFindIndex (fun x -> x % 2 = 0) list
+        Assert.AreEqual(Some 1, result)
+    
+    [<Test>]
+    let ``tryFindIndexBack works`` () =
+        let list = NonEmptyList.create 1 [2; 3; 4]
+        let result = NonEmptyList.tryFindIndexBack (fun x -> x % 2 = 0) list
+        Assert.AreEqual(Some 3, result)
+    
+    [<Test>]
+    let ``tryItem works`` () =
+        let list = NonEmptyList.create 1 [2; 3; 4]
+        let result = NonEmptyList.tryItem 2 list
+        Assert.AreEqual(Some 3, result)
+    
+    [<Test>]
+    let ``tryLast works`` () =
+        let list = NonEmptyList.create 1 [2; 3; 4]
+        let result = NonEmptyList.tryLast list
+        Assert.AreEqual(Some 4, result)
+    
+    [<Test>]
+    let ``tryPick works`` () =
+        let list = NonEmptyList.create 1 [2; 3; 4]
+        let result = NonEmptyList.tryPick (fun x -> if x % 2 = 0 then Some (x * 2) else None) list
+        Assert.AreEqual(Some 4, result)
+    
+    [<Test>]
+    let ``unfold works`` () =
+        let result = NonEmptyList.unfold (fun state -> if state > 3 then None else Some (state, state + 1)) 0
+        Assert.AreEqual(NonEmptyList.create 0 [1; 2; 3], result)
+    
+    [<Test>]
+    let ``unzip works`` () =
+        let list = NonEmptyList.create (1, 'a') [(2, 'b'); (3, 'c')]
+        let result1, result2 = NonEmptyList.unzip list
+        Assert.AreEqual(NonEmptyList.create 1 [2; 3], result1)
+        Assert.AreEqual(NonEmptyList.create 'a' ['b'; 'c'], result2)
+    
+    [<Test>]
+    let ``unzip3 works`` () =
+        let list = NonEmptyList.create (1, 'a', true) [(2, 'b', false); (3, 'c', true)]
+        let result1, result2, result3 = NonEmptyList.unzip3 list
+        Assert.AreEqual(NonEmptyList.create 1 [2; 3], result1)
+        Assert.AreEqual(NonEmptyList.create 'a' ['b'; 'c'], result2)
+        Assert.AreEqual(NonEmptyList.create true [false; true], result3)
+    
+    [<Test>]
+    let ``updateAt works`` () =
+        let list = NonEmptyList.create 1 [2; 3; 4]
+        let result = NonEmptyList.updateAt 2 99 list
+        Assert.AreEqual(NonEmptyList.create 1 [2; 99; 4], result)
+    
+    [<Test>]
+    let ``where works`` () =
+        let list = NonEmptyList.create 1 [2; 3; 4]
+        let result = NonEmptyList.where (fun x -> x % 2 = 0) list
+        Assert.AreEqual(NonEmptyList.create 2 [4], result)
+    
+    [<Test>]
+    let ``windowed works`` () =
+        let list = NonEmptyList.create 1 [2; 3; 4]
+        let result = NonEmptyList.windowed 2 list
+        Assert.AreEqual(NonEmptyList.create (NonEmptyList.create 1 [2]) [NonEmptyList.create 2 [3]; NonEmptyList.create 3 [4]], result)
+    
+    [<Test>]
+    let ``zip works`` () =
+        let list1 = NonEmptyList.create 1 [2; 3]
+        let list2 = NonEmptyList.create 'a' ['b'; 'c']
+        let result = NonEmptyList.zip list1 list2
+        Assert.AreEqual(NonEmptyList.create (1, 'a') [(2, 'b'); (3, 'c')], result)
+    
+    [<Test>]
+    let ``zip3 works`` () =
+        let list1 = NonEmptyList.create 1 [2; 3]
+        let list2 = NonEmptyList.create 'a' ['b'; 'c']
+        let list3 = NonEmptyList.create true [false; true]
+        let result = NonEmptyList.zip3 list1 list2 list3
+        Assert.AreEqual(NonEmptyList.create (1, 'a', true) [(2, 'b', false); (3, 'c', true)], result)
+    
 module MultiMap =
     [<Test>]
     let ``monoid works`` () =
