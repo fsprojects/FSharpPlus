23b8003d54039b37107ae7f9a186ba3c98a03db8
Oskar Gewalli <wallymathieu@users.noreply.github.com>
2025-06-13 22:57:18 +0300

    HashSet net45

--- src/FSharpPlus/Extensions/HashSet.fs
+++ src/FSharpPlus/Extensions/HashSet.fs
@@ -17,7 +17,7 @@ module HashSet =
     [<CompiledName("Singleton")>]
     let singleton (value: 'T) : HashSet<'T> =
         let set =
-            #if FABLE_COMPILER
+            #if FABLE_COMPILER || NET45 || NETSTANDARD2_0
             HashSet<'T> ()
             #else
             HashSet<'T> 1
@@ -31,10 +31,12 @@ module HashSet =
     /// <returns>The union of <c>set1</c> and <c>set2</c>.</returns>
     [<CompiledName("Union")>]
     let union (source1: HashSet<'T>) (source2: HashSet<'T>) : HashSet<'T> =
+        #if !NET45
         raiseIfNull (nameof source1) source1
         raiseIfNull (nameof source2) source2
+        #endif
         let union =
-            #if FABLE_COMPILER
+            #if FABLE_COMPILER || NET45 || NETSTANDARD2_0
             HashSet<'T> ()
             #else
             HashSet<'T> (max source1.Count source2.Count)
@@ -50,7 +52,9 @@ module HashSet =
     /// <returns>A set containing the transformed elements.</returns>
     [<CompiledName("Map")>]
     let map (mapping: 'T -> 'U) (source: HashSet<'T>) : HashSet<'U> =
+        #if !NET45
         raiseIfNull (nameof source) source
+        #endif
         let result = empty<'U>
         for item in source do
             result.Add (mapping item) |> ignore
