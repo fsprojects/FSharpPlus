1fcb92b159165b2843744b63c7e155c0f06dede5
Mindu Gamese <mindgamese@gmail.com>
2025-06-15 19:42:12 +0000

    fix: use same dll loading as the other fsi files

--- docsrc/content/abstraction-arrow.fsx
+++ docsrc/content/abstraction-arrow.fsx
@@ -1,6 +1,8 @@
 (*** hide ***)
 // This block of code is omitted in the generated HTML documentation. Use 
 // it to define helpers that you do not want to show in the documentation.
+#r @"../../src/FSharpPlus/bin/Release/net8.0/FSharpPlus.dll"
+
 
 (**
 Arrow
@@ -77,9 +79,11 @@ Let's define a simple arrow type based on a function:
 
 *)
 
-#if INTERACTIVE
-#r "nuget: FSharpPlus"
-#endif
+(**
+```f#
+#r @"nuget: FSharpPlus"
+```
+*)
 
 open FSharpPlus
 open FSharpPlus.Data
